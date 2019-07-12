d2a9dade5ccf828fc4715088341ba70a63a17917
kiljoonho <kiljh@me.com>
2019-07-12 18:33:45 +0900

    수정 - JK 의 피드백에 따라 에러 문구의 저장 방식을 변경
    에러 타입의 raw 값을 localizedDescription 로 변경

--- CardGameApp/CardGameApp/InputError.swift
+++ CardGameApp/CardGameApp/InputError.swift
@@ -8,7 +8,16 @@
 
 import Foundation
 
-enum InputError: String, Error {
-    case notExistsMenu = "해당 번호의 메뉴는 존재하지 않습니다."
-    case notSupportUserCount = "참석인원은 1명부터 4명까지 가능합니다."
+enum InputError: Error {
+    case notExistsMenu
+    case notSupportUserCount
+    
+    var localizedDescription: String {
+        switch self {
+        case .notExistsMenu:
+            return "해당 번호의 메뉴는 존재하지 않습니다."
+        case .notSupportUserCount:
+            return "참석인원은 1명부터 4명까지 가능합니다."
+        }
+    }
 }
