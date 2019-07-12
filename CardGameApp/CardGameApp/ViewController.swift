4c161c176a85db24640843d0e765098d601106a5
kiljoonho <kiljh@me.com>
2019-07-12 18:36:57 +0900

    추가 - 세그멘트 구성
    카드 숫자와 참여자 숫자를 입력할 세그멘트 구성

--- CardGameApp/CardGameApp/ViewController.swift
+++ CardGameApp/CardGameApp/ViewController.swift
@@ -9,6 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var cardGame = CardGame()
+    @IBOutlet weak var menuSegemnt: UISegmentedControl!
+    @IBOutlet weak var playerCountSegent: UISegmentedControl!
+    
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return .lightContent
     }
@@ -18,8 +22,6 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         
         self.view.backgroundColor = UIColor(patternImage: UIImage(named: "bg_pattern")!)
-        
-        drawCards()
     }
     
     private func drawCards () {
@@ -31,6 +33,29 @@ class ViewController: UIViewController {
             self.view.addSubview(imageView)
             imageView.frame = CGRect(x: Double(coordinateX), y: 30.0, width: 50.0, height: 63.5)
         }
+    }
+    
+    @IBAction func startCardGame(_ sender: Any) {
+        let menuIndex = menuSegemnt.selectedSegmentIndex+1
+        let menu = Menu.init(rawValue: menuIndex)!
+        let userCountIndex = playerCountSegent.selectedSegmentIndex+2
+        let userCount = UserCount.init(rawValue: userCountIndex)!
+        
+        do {
+            let participant = try cardGame.executeMenu(menu, userCount)
+        }
+        catch let error as InputError
+        {
+            let alert = UIAlertController(title: "오류", message: error.localizedDescription, preferredStyle: .alert)
+            
+            alert.addAction(UIAlertAction(title: "닫기", style: .default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }
+        catch {
+            
+        }
+        
+        
     }
 }
 
