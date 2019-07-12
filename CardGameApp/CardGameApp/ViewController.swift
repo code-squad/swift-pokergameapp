dad593f3e833ab6901c601d5d752c710cfe2f617
kiljoonho <kiljh@me.com>
2019-07-12 11:18:50 +0900

    추가 - viewController 에서 배경과 카드덱을 화면 출력하는 소스 작성

--- CardGameApp/CardGameApp/ViewController.swift
+++ CardGameApp/CardGameApp/ViewController.swift
@@ -14,9 +14,23 @@ class ViewController: UIViewController {
     }
     
     override func viewDidLoad() {
-        
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        self.view.backgroundColor = UIColor(patternImage: UIImage(named: "bg_pattern")!)
+        
+        drawCards()
+    }
+    
+    private func drawCards () {
+        for index in 0..<7 {
+            let coordinateX = Double(10 + 57 * index)
+            let image: UIImage = UIImage(named: "card-back")!
+            let imageView = UIImageView(image: image)
+        
+            self.view.addSubview(imageView)
+            imageView.frame = CGRect(x: Double(coordinateX), y: 30.0, width: 50.0, height: 63.5)
+        }
     }
 }
 
