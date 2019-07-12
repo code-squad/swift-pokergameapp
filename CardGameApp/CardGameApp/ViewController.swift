31fe029ceee9ac3631d43122ce1e047f991cbaa5
kiljoonho <kiljh@me.com>
2019-07-12 10:44:55 +0900

    수정 - StatusBar 스타일을 LightContent 로 변경

--- CardGameApp/CardGameApp/ViewController.swift
+++ CardGameApp/CardGameApp/ViewController.swift
@@ -9,12 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
 
