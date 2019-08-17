f8ccd0ec2fbdc4743d03aa079a42c069e7ce126f
hwj0623 <hwj0623@naver.com>
2019-08-17 14:59:29 +0900

    초기 프로젝트 세팅 - 불필요 이미지 파일 제거

---
+++ PokergameApp/PokergameApp/PlayerModels/Protocols/Playable.swift
@@ -0,0 +1,13 @@
+//
+//  Playable.swift
+//  PokergameApp
+//
+//  Created by hw on 16/08/2019.
+//  Copyright © 2019 hwj. All rights reserved.
+//
+
+import Foundation
+
+protocol Playable {
+    func sortOwnDeck()
+}
---
+++ PokergameApp/PokergameApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PokergameApp
+//
+//  Created by hw on 14/08/2019.
+//  Copyright © 2019 hwj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
+
