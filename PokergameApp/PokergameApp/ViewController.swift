008c1f6710d87e9b44dfab5a16664b8a48be53d1
hwj0623 <hwj0623@naver.com>
2019-08-19 15:03:39 +0900

    fix : 이미지 하드코딩 제거 - 배경이미지, 카드 뒷면을 ImageInfo의 static 변수로 설정 - 이미지 너비 계산식에서 하드코딩을 변수로 변환

--- PokergameApp/PokergameApp/ViewController.swift
+++ PokergameApp/PokergameApp/ViewController.swift
@@ -12,13 +12,15 @@ class ViewController: UIViewController {
     private let customSpace: CGFloat = 10
     private var cardSize = 7
     private var width: CGFloat = 0
+    private let ratio: CGFloat = 1.27
+    private var margin = 10
     private var height: CGFloat {
-        return CGFloat((width-customSpace * CGFloat(cardSize)) / CGFloat(cardSize)) * 1.27
+        return CGFloat((width-customSpace * CGFloat(cardSize)) / CGFloat(cardSize)) * ratio
     }
-    private var widthProportion:CGFloat = 0.9
+    private var widthProportion: CGFloat = 0.9
     private var deckSizeRect: CGRect = CGRect.init(x: 0, y: 0, width: 1, height: 1)
     private lazy var stackview: UIStackView = UIStackView.init(frame: deckSizeRect)
-
+    private let verticalConstant: CGFloat = 20
     override func viewDidLoad() {
         super.viewDidLoad()
         width = view.frame.width * widthProportion
@@ -31,12 +33,12 @@ class ViewController: UIViewController {
     }
     
     private func addImageViewsInStackView(){
-        let imageWidth = (deckSizeRect.width-70)/7
+        let imageWidth = (deckSizeRect.width - CGFloat(margin*cardSize))/CGFloat(cardSize)
         let imageHeight = height
         for _ in 0..<cardSize  {
             let currentCardRect = CGRect.init(x: 0, y: 0, width: imageWidth, height: imageHeight)
             let uiImageView = UIImageView.init(frame: currentCardRect)
-            uiImageView.image = UIImage.init(named: "card-back.png")!
+            uiImageView.image = UIImage.init(named: "\(ImageInfo.cardBack)")!
             stackview.addArrangedSubview(uiImageView)
         }
         stackview.distribution = .fillEqually
@@ -46,7 +48,7 @@ class ViewController: UIViewController {
     private func setConstraints(){
         stackview.translatesAutoresizingMaskIntoConstraints = false
         let horizontalConstraint = NSLayoutConstraint.init(item: stackview, attribute: NSLayoutConstraint.Attribute.centerX, relatedBy: NSLayoutConstraint.Relation.equal, toItem: stackview.superview, attribute: NSLayoutConstraint.Attribute.centerX, multiplier: 1, constant: 0)
-        let verticalConstraint = NSLayoutConstraint.init(item: stackview, attribute: NSLayoutConstraint.Attribute.topMargin, relatedBy: NSLayoutConstraint.Relation.equal, toItem: stackview.superview, attribute: NSLayoutConstraint.Attribute.topMargin, multiplier: 1, constant: 20)
+        let verticalConstraint = NSLayoutConstraint.init(item: stackview, attribute: NSLayoutConstraint.Attribute.topMargin, relatedBy: NSLayoutConstraint.Relation.equal, toItem: stackview.superview, attribute: NSLayoutConstraint.Attribute.topMargin, multiplier: 1, constant: verticalConstant)
         let widthConstraint = NSLayoutConstraint.init(item: stackview, attribute: NSLayoutConstraint.Attribute.width, relatedBy: NSLayoutConstraint.Relation.equal, toItem: stackview.superview, attribute: NSLayoutConstraint.Attribute.width, multiplier: 0, constant: width)
         let heightConstraint = NSLayoutConstraint.init(item: stackview, attribute: NSLayoutConstraint.Attribute.height, relatedBy: NSLayoutConstraint.Relation.equal, toItem: stackview.superview, attribute:  NSLayoutConstraint.Attribute.height, multiplier: 0, constant: height)
         NSLayoutConstraint.activate([horizontalConstraint, verticalConstraint, widthConstraint, heightConstraint])
@@ -57,7 +59,7 @@ class ViewController: UIViewController {
     }
     
     override func viewWillAppear(_ animated: Bool) {
-        guard let backgroundPatternImage = UIImage.init(named: "bg_pattern.png") else {
+        guard let backgroundPatternImage = UIImage.init(named: "\(ImageInfo.background)") else {
             return
         }
         view.backgroundColor = UIColor.init(patternImage: backgroundPatternImage)
