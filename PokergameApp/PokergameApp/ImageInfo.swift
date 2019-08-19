7d2c8fc88c4e4069b4e3c0f00bc4c647ecebb4a2
hwj0623 <hwj0623@naver.com>
2019-08-19 15:03:39 +0900

    fix : 이미지 하드코딩 제거 - 배경이미지, 카드 뒷면을 ImageInfo의 static 변수로 설정 - 이미지 너비 계산식에서 하드코딩을 변수로 변환

---
+++ PokergameApp/PokergameApp/ImageInfo.swift
@@ -0,0 +1,14 @@
+//
+//  ImageInfo.swift
+//  PokergameApp
+//
+//  Created by hw on 19/08/2019.
+//  Copyright © 2019 hwj. All rights reserved.
+//
+
+import Foundation
+
+struct ImageInfo {
+    static var background = "bg_pattern.png"
+    static var cardBack = "card-back.png"
+}
