ba020125ba1148042165e1b616991543931e610d
hwj0623 <hwj0623@naver.com>
2019-08-19 16:07:59 +0900

    cardSizeInfo와 stackViewSizeInfo 각각의 프로퍼티들을 별도 구조체로 분리 - 단, 둘을 함께 참조하는 연산프로퍼티 height는 ViewController의 연산프로퍼티로 유지

--- PokergameApp/PokergameApp/ViewController.swift
+++ PokergameApp/PokergameApp/ViewController.swift
@@ -9,25 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-    private let customSpace: CGFloat = 10
-    private var cardSize = 7
-    private var width: CGFloat = 0
-    private let ratio: CGFloat = 1.27
-    private var margin = 10
-    private var height: CGFloat {
-        return CGFloat((width-customSpace * CGFloat(cardSize)) / CGFloat(cardSize)) * ratio
+    private struct CardSizeInfo {
+        var cardSize = 7
+        let ratio: CGFloat = 1.27
     }
-    private var widthProportion: CGFloat = 0.9
-    private var deckSizeRect: CGRect = CGRect.init(x: 0, y: 0, width: 1, height: 1)
-    private lazy var stackview: UIStackView = UIStackView.init(frame: deckSizeRect)
+    private struct StackViewSizeInfo {
+        let widthProportion: CGFloat = 0.9
+        let marginSpace: CGFloat = 10
+        var width: CGFloat = 0
+        var deckSizeRect: CGRect = CGRect.init(x: 0, y: 0, width: 1, height: 1)
+    }
+    var height: CGFloat {
+        return CGFloat((stackViewSizeInfo.width-stackViewSizeInfo.marginSpace * CGFloat(cardSizeInfo.cardSize))
+            / CGFloat(cardSizeInfo.cardSize)) * cardSizeInfo.ratio
+    }
+    private var cardSizeInfo = CardSizeInfo()
+    private var stackViewSizeInfo = StackViewSizeInfo()
+    private lazy var stackview: UIStackView = UIStackView.init(frame: stackViewSizeInfo.deckSizeRect)
     private let verticalConstant: CGFloat = 20
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setBackgroundPatternImage()
         
-        width = view.frame.width * widthProportion
-        deckSizeRect = CGRect.init(x: 0, y: 0, width: width, height: height)
-        stackview = UIStackView.init(frame: deckSizeRect)
+        stackViewSizeInfo.width = view.frame.width * stackViewSizeInfo.widthProportion
+        stackViewSizeInfo.deckSizeRect = CGRect.init(x: 0, y: 0, width: stackViewSizeInfo.width, height: height)
+        stackview = UIStackView.init(frame: stackViewSizeInfo.deckSizeRect)
         addImageViewsInStackView()
         view.addSubview(stackview)
         
@@ -42,23 +49,24 @@ class ViewController: UIViewController {
     }
     
     private func addImageViewsInStackView(){
-        let imageWidth = (deckSizeRect.width - CGFloat(margin*cardSize))/CGFloat(cardSize)
+        let imageWidth = (stackViewSizeInfo.deckSizeRect.width - stackViewSizeInfo.marginSpace
+            * CGFloat(cardSizeInfo.cardSize))/CGFloat(cardSizeInfo.cardSize)
         let imageHeight = height
-        for _ in 0..<cardSize  {
+        for _ in 0..<cardSizeInfo.cardSize  {
             let currentCardRect = CGRect.init(x: 0, y: 0, width: imageWidth, height: imageHeight)
             let uiImageView = UIImageView.init(frame: currentCardRect)
             uiImageView.image = UIImage.init(named: "\(ImageInfo.cardBack)")!
             stackview.addArrangedSubview(uiImageView)
         }
         stackview.distribution = .fillEqually
-        stackview.spacing = customSpace
+        stackview.spacing = stackViewSizeInfo.marginSpace
     }
     
     private func setConstraints(){
         stackview.translatesAutoresizingMaskIntoConstraints = false
         let horizontalConstraint = NSLayoutConstraint.init(item: stackview, attribute: NSLayoutConstraint.Attribute.centerX, relatedBy: NSLayoutConstraint.Relation.equal, toItem: stackview.superview, attribute: NSLayoutConstraint.Attribute.centerX, multiplier: 1, constant: 0)
         let verticalConstraint = NSLayoutConstraint.init(item: stackview, attribute: NSLayoutConstraint.Attribute.topMargin, relatedBy: NSLayoutConstraint.Relation.equal, toItem: stackview.superview, attribute: NSLayoutConstraint.Attribute.topMargin, multiplier: 1, constant: verticalConstant)
-        let widthConstraint = NSLayoutConstraint.init(item: stackview, attribute: NSLayoutConstraint.Attribute.width, relatedBy: NSLayoutConstraint.Relation.equal, toItem: stackview.superview, attribute: NSLayoutConstraint.Attribute.width, multiplier: 0, constant: width)
+        let widthConstraint = NSLayoutConstraint.init(item: stackview, attribute: NSLayoutConstraint.Attribute.width, relatedBy: NSLayoutConstraint.Relation.equal, toItem: stackview.superview, attribute: NSLayoutConstraint.Attribute.width, multiplier: 0, constant: stackViewSizeInfo.width)
         let heightConstraint = NSLayoutConstraint.init(item: stackview, attribute: NSLayoutConstraint.Attribute.height, relatedBy: NSLayoutConstraint.Relation.equal, toItem: stackview.superview, attribute:  NSLayoutConstraint.Attribute.height, multiplier: 0, constant: height)
         NSLayoutConstraint.activate([horizontalConstraint, verticalConstraint, widthConstraint, heightConstraint])
     }
@@ -70,6 +78,6 @@ class ViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
     }
-
+    
 }
 
