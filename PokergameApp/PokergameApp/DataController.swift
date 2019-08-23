296440bf7648bcf1b80df88759fc78e6bae89a27
hwj0623 <hwj0623@naver.com>
2019-08-23 13:04:38 +0900

    스토리보드의 더미 뷰컨트롤러에 요구사항 UI 구현 - 뷰컨트롤러와 모델 객체 분리를 위한 데이터 컨트롤러 생성

---
+++ PokergameApp/PokergameApp/DataController.swift
@@ -0,0 +1,17 @@
+//
+//  DataController.swift
+//  PokergameApp
+//
+//  Created by hw on 22/08/2019.
+//  Copyright © 2019 hwj. All rights reserved.
+//
+
+import Foundation
+
+class DataController {
+    private var cardGamePlay = CardGamePlay(number: PlayerType.three, gameType: .sevenCard)
+    
+    init(playStyle: CardGamePlay){
+        cardGamePlay = playStyle
+    }
+}
