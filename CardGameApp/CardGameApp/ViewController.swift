ade33ed7b6e9590dbd24e46d350f2d99db1d7d02
conyconydev <conyconydev@gmail.com>
2019-11-10 07:44:59 +0900

    fix: 기존 Outputview 처럼 구현하도록 playGame을 둠

--- CardGameApp/CardGameApp/ViewController.swift
+++ CardGameApp/CardGameApp/ViewController.swift
@@ -19,32 +19,30 @@ class ViewController: UIViewController {
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return .lightContent
     }
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         guard let image = UIImage(named: "bg_pattern") else { return }
         self.view.backgroundColor = UIColor(patternImage: image)
 
-        drawCards(cardCount: 7)
-    }
-    
-    //MARK: private func
-    /// 카드를 갯수를 입력받은 만큼 그려준다.
-    private func drawCards(cardCount: Int) {
-        let bounds = UIScreen.main.bounds
-        let space = 10
-        let ratio = 1.27
-        var cardX = CGFloat(space)
-        let cardY = CGFloat(50)
-        let allSpace = (cardCount + 1) * space
-        let cardWidth = (bounds.width - CGFloat(allSpace)) / CGFloat(cardCount)
-        let cardHeight = cardWidth * CGFloat(ratio)
+        playGame()
 
-        for _ in 0..<cardCount {
-            let cardFrame = CGRect(x: cardX, y: cardY, width: cardWidth, height: cardHeight)
-            let imageView = CardImageView(frame: cardFrame)
-            cardX = cardX + cardWidth + CGFloat(space)
+    }
+
+    //MARK: private func
+    /// 카드 게임
+    private func playGame() {
+        let gameMode = GameMode.init(rawValue: cardsSegment.selectedSegmentIndex + 1) ?? GameMode.sevenCard
+        let userCount = playerSegment.selectedSegmentIndex + 2
+        let cardGame = CardGame.init(gameMode: gameMode, players: userCount)
+
+        let _ = showResults(cardGame)
+    }
+    /// 결과
+    func showResults(_ cardGame: CardGame) -> Bool {
+        return cardGame.play(card(name: cardStack:), winner)
+    }
             self.view.addSubview(imageView)
         }
     }
