34e421636b931d13b9c253023943f72f7bc3d025
conyconydev <conyconydev@gmail.com>
2019-11-09 23:35:47 +0900

    add : setImage function

--- CardGameApp/CardGameApp/CardImageView.swift
+++ CardGameApp/CardGameApp/CardImageView.swift
@@ -12,15 +12,20 @@ class CardImageView: UIImageView {
 
     override func layoutSubviews() {
         super.layoutSubviews()
-        self.image = UIImage(named: "card-back")
+        self.image = UIImage(named: setImage())
     }
 
     override init(frame: CGRect) {
         super.init(frame: frame)
     }
-    
+
     required init?(coder: NSCoder) {
         super.init(coder: coder)
     }
-    
+
+    /// 카드 이미지
+    private func setImage(named name: String? = nil) -> String {
+        let name: String = name ?? "card-back"
+        return name
+    }
 }
