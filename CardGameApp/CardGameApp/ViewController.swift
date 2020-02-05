7d332bbca25cc5e60d5a7cc8d904f5a44e73ff49
TTOzzi <devttozzi@gmail.com>
2020-02-05 13:06:05 +0900

    Single View App 으로 프로젝트 생성

---
+++ CardGameApp/CardGameApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CardGameApp
+//
+//  Created by TTOzzi on 2020/02/05.
+//  Copyright © 2020 TTOzzi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
