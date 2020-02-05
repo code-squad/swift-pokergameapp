eb9969cf5fe499afd1aaa9ef6e476e3f42bf194f
seizze <cwcwc3@gmail.com>
2020-02-05 17:45:24 +0900

    feat: Display 7 upturned cards at the top

--- CardGameApp/CardGameApp/ViewController.swift
+++ CardGameApp/CardGameApp/ViewController.swift
@@ -9,6 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let numberOfUpturnedCards = 7
+    
+    lazy var cardStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.distribution = .fillEqually
+        stackView.spacing = 4
+        stackView.layoutMargins = UIEdgeInsets(top: 0, left: 8, bottom: 0, right: 8)
+        stackView.isLayoutMarginsRelativeArrangement = true
+        (0..<numberOfUpturnedCards).forEach { _ in
+            let upturnedCardImageView = UIImageView(image: UIImage(named: "card-back")!)
+            upturnedCardImageView.translatesAutoresizingMaskIntoConstraints = false
+            upturnedCardImageView.heightAnchor
+                .constraint(equalTo: upturnedCardImageView.widthAnchor, multiplier: 1.27)
+                .isActive = true
+            stackView.addArrangedSubview(upturnedCardImageView)
+        }
+        return stackView
+    }()
 
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return .lightContent
@@ -21,7 +40,13 @@ class ViewController: UIViewController {
             self.view.backgroundColor = UIColor(patternImage: backgroundPatternImage)
         }
         
+        self.view.addSubview(cardStackView)
         
+        let safeArea = self.view.safeAreaLayoutGuide
+        cardStackView.translatesAutoresizingMaskIntoConstraints = false
+        cardStackView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor).isActive = true
+        cardStackView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor).isActive = true
+        cardStackView.topAnchor.constraint(equalTo: safeArea.topAnchor).isActive = true
     }
 }
 
