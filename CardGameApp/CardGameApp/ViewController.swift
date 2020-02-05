d679cf1fc61a8c71c6c193a1bc9bfd12ab27294d
TTOzzi <devttozzi@gmail.com>
2020-02-05 13:37:28 +0900

    StatusBar 스타일 LightContent 로 변경
    LightContent 로 변경된 것을 확인하기 위해 ViewController 의 배경색을 검정색으로 변경해줌

--- CardGameApp/CardGameApp/ViewController.swift
+++ CardGameApp/CardGameApp/ViewController.swift
@@ -10,10 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
 
 
 }
