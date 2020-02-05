ab5eb2379c4a41e5a3d574ba1acd4e4f1fb3c5fc
corykim0829 <corykim0829@gmail.com>
2020-02-05 12:37:44 +0900

    Init a project

---
+++ CardGameApp/CardGameApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CardGameApp
+//
+//  Created by Cory Kim on 2020/02/05.
+//  Copyright © 2020 corykim0829. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
