adff1cdcc095a6ceedc4c66c6d874edb302ed923
kjoonk <kjoonk@gmail.com>
2020-02-05 21:26:15 +0900

    배경 이미지 추가

---
+++ CardGameApp/CardGameApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CardGameApp
+//
+//  Created by Viet on 2020/02/05.
+//  Copyright © 2020 Viet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = UIColor(patternImage: #imageLiteral(resourceName: "bg_pattern"))
+    }
+}
+
