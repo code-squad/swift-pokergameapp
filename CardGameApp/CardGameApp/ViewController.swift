c98c8330e96c1264fd5e7ed30b707a2b84de428c
TTOzzi <devttozzi@gmail.com>
2020-02-05 17:19:39 +0900

    배경을 이미지 패턴으로 지정
    UIColor 로 이미지를 패턴으로 만들어 ViewController 의 배경으로 지정
    예시와 같게 출력되도록 Assets 에서 scale 조절

--- CardGameApp/CardGameApp/ViewController.swift
+++ CardGameApp/CardGameApp/ViewController.swift
@@ -16,7 +16,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = UIColor(patternImage: #imageLiteral(resourceName: "bg_pattern"))
     }
     
 
