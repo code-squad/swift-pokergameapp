3a274440c1d6e51a5d094d6baf1d6510cdce652a
corykim0829 <corykim0829@gmail.com>
2020-02-06 21:48:15 +0900

    Feat: CardDeck class
    - Initialize with generate all kind of cards

---
+++ CardGameApp/CardGameApp/CardDeck.swift
@@ -0,0 +1,29 @@
+//
+//  CardDeck.swift
+//  CardGameApp
+//
+//  Created by Cory Kim on 2020/02/06.
+//  Copyright © 2020 corykim0829. All rights reserved.
+//
+
+import Foundation
+
+class CardDeck {
+    var cards: [Card] = []
+    
+    init() {
+        setupDeck()
+    }
+    
+    private func setupDeck() {
+        let suits: [Card.Suit] = [.club, .diamond, .heart, .spade]
+        let ranks: [Card.Rank] = [.ace, .two, .three, .four, .five, .six, .seven, .eight, .nine, .ten, .jack, .queen, .king]
+        
+        for suitIndex in 0..<suits.count {
+            for rankIndex in 0..<ranks.count {
+                let card = Card(suit: suits[suitIndex], rank: ranks[rankIndex])
+                cards.append(card)
+            }
+        }
+    }
+}
