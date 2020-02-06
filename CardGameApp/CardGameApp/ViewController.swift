45880983957a573103b631dde0fff1d5e8884228
TTOzzi <devttozzi@gmail.com>
2020-02-06 20:12:04 +0900

    카드 출력 구현
    카드들을 담을 cardStack 을 UIStackView 로 만들어 준 뒤, 정렬방법, 간격을 설정해 줌.
    카드를 UIImageView 로 7개를 만들어 cardStack 에 담아주는 makeCards() 함수를 만들어 줌.
    viewDidLoad() 에서 makeCards() 를 호출해주고 view 에 cardStack 이 출력되도록 해줌.
    view 를 기준으로 cardStack 의 위, 양쪽 위치를 잡아줌.

--- CardGameApp/CardGameApp/ViewController.swift
+++ CardGameApp/CardGameApp/ViewController.swift
@@ -10,16 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var cardStack: UIStackView = {
+        let stack = UIStackView()
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        stack.distribution = .fillEqually
+        stack.axis = .horizontal
+        stack.spacing = 5
+        return stack
+    }()
+    
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return .lightContent
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        makeCards()
         view.backgroundColor = UIColor(patternImage: #imageLiteral(resourceName: "bg_pattern"))
+        self.view.addSubview(cardStack)
+        cardStack.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 30).isActive = true
+        cardStack.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 5).isActive = true
+        cardStack.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -5).isActive = true
     }
     
-
-
+    func makeCards() {
+        for _ in 0..<7 {
+            let card = UIImageView(image: #imageLiteral(resourceName: "card-back"))
+            card.contentMode = .scaleAspectFit
+            cardStack.addArrangedSubview(card)
+        }
+    }
 }
 
