f1f49e77528bff03e93b4f5815bcd7b470ea20c2
corykim0829 <corykim0829@gmail.com>
2020-02-06 22:13:48 +0900

    Test: Initialize a cardDeck

--- CardGameApp/CardGameAppTests/CardGameAppTests.swift
+++ CardGameApp/CardGameAppTests/CardGameAppTests.swift
@@ -18,4 +18,12 @@ class CardGameAppTests: XCTestCase {
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
+    
+    func testCardDeckInitializer() {
+        print("> 카드 초기화")
+        let cardDeck = CardDeck()
+        print("카드 전체를 초기화했습니다.")
+        XCTAssertTrue(cardDeck.count == 52)
+        print("총 \(cardDeck.count)장의 카드가 있습니다.")
+    }
 }
