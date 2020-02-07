879926e87e85c7b9d9423cd79e77409822b7b367
corykim0829 <corykim0829@gmail.com>
2020-02-08 00:57:03 +0900

    Feat: Player class

---
+++ CardGameApp/CardGameApp/Player.swift
@@ -0,0 +1,13 @@
+//
+//  Player.swift
+//  CardGameApp
+//
+//  Created by Cory Kim on 2020/02/08.
+//  Copyright © 2020 corykim0829. All rights reserved.
+//
+
+import Foundation
+
+class Player {
+    var hands: [Card] = []
+}
