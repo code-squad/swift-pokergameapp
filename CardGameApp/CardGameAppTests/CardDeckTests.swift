544de9c6047ea46881245ce91a2b44ca0d0b62c9
seizze <cwcwc3@gmail.com>
2020-02-07 15:05:26 +0900

    test: Rename a test file to CardDeckTests.swift

---
+++ CardGameApp/CardGameAppTests/CardDeckTests.swift
@@ -0,0 +1,16 @@
+//
+//  CardDeckTests.swift
+//  CardGameAppTests
+//
+//  Created by Chaewan Park on 2020/02/07.
+//  Copyright © 2020 Chaewan Park. All rights reserved.
+//
+
+import XCTest
+
+class CardDeckTests: XCTestCase {
+
+    override func setUp() {
+        
+    }
+}
