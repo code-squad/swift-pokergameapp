0694c84b856db7916d5e0c353c2667fd07c3904c
TTOzzi <devttozzi@gmail.com>
2020-02-07 16:23:19 +0900

    Card 클래스 구현
    모양과 랭크를 열거형으로 만들어줌
    랭크에서 숫자일때와 영어일때를 함께 처리해주기위해 열거형을 중첩해서 사용함

--- CardGameApp/CardGameApp/ViewController.swift
+++ CardGameApp/CardGameApp/ViewController.swift
@@ -31,6 +31,8 @@ class ViewController: UIViewController {
         cardStack.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 30).isActive = true
         cardStack.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 5).isActive = true
         cardStack.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -5).isActive = true
+        let a = Card(shape: .diamond, rank: .number(.ten))
+        print(a.info)
     }
     
     func makeCards() {
