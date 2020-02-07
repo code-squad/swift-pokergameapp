8e3eb8e330b935a0cb8e54a9aa533b7fc9c6547d
TTOzzi <devttozzi@gmail.com>
2020-02-07 19:28:14 +0900

    removeOne(), shuffle() 구현, 기존 코드 수정
    배열 가장 뒤쪽 카드를 뽑아주는 removeOne() 함수 구현
    카드들을 섞어주는 shuffle() 함수 구현
    reset 할때 cards 를 초기화해주도록 수정

--- CardGameApp/CardGameApp/Deck.swift
+++ CardGameApp/CardGameApp/Deck.swift
@@ -9,17 +9,35 @@
 import Foundation
 
 struct Deck {
-    var cards = [Card]()
+    private var cards = [Card]() {
+        didSet {
+            print("총 \(count)장의 카드가 남아있습니다.")
+        }
+    }
     var count: Int {
         cards.count
     }
     
     mutating func reset() {
+        cards = [Card]()
         Card.Shape.allCases.forEach {
             let shape = $0
             Card.Rank.allCases.forEach {
                 cards.append(Card(shape: shape, rank: $0))
             }
         }
+    }
+    
+    mutating func removeOne() {
+        guard count > 0 else {
+            print("카드가 없어요")
+            return
+        }
+        let card = cards.removeLast()
+        print(card)
+    }
+    
+    mutating func shuffle() {
+        cards.shuffle()
     }
 }
