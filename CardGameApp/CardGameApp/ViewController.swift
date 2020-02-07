f954a8f6bbdc79a4227483991fdd965d3223a1e2
ehgud0670 <kimdo2297@gmail.com>
2020-02-07 16:30:24 +0900

    Feature : 화면 상단에 7개의 카드 추가하는 기능 구현

--- CardGameApp/CardGameApp/ViewController.swift
+++ CardGameApp/CardGameApp/ViewController.swift
@@ -9,6 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
+    private let cardStackView : UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.distribution = .fillEqually
+        stackView.spacing = 4
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
     
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return .lightContent
@@ -17,9 +25,47 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setupBackground()
+        setupCards()
     }
     
     private func setupBackground() {
         self.view.backgroundColor = UIColor(patternImage: #imageLiteral(resourceName: "bg_pattern"))
     }
+    
+    private func setupCards(){
+        addCards(to : cardStackView)
+        self.view.addSubview(cardStackView)
+        setConstraintCardStackView()
+    }
+    
+    private func addCards(to cardStackView : UIStackView) {
+        let maxCardNum = 7
+        for _ in 0 ..< maxCardNum {
+            cardStackView.addArrangedSubview(generateCardImageView())
+        }
+    }
+
+    private func generateCardImageView() -> UIImageView {
+        let cardImageView = UIImageView(image:  #imageLiteral(resourceName: "card-back"))
+        cardImageView.contentMode = .scaleAspectFill
+        cardImageView.translatesAutoresizingMaskIntoConstraints = false
+        cardImageView.heightAnchor.constraint(
+            equalTo: cardImageView.widthAnchor,
+            multiplier: 1.27).isActive = true
+        return cardImageView
+    }
+    
+    private func setConstraintCardStackView() {
+        let boundaryConstant : CGFloat = 8
+        cardStackView.topAnchor.constraint(
+            equalTo: view.safeAreaLayoutGuide.topAnchor,
+            constant: boundaryConstant).isActive = true
+        cardStackView.leadingAnchor.constraint(
+            equalTo: view.leadingAnchor,
+            constant: boundaryConstant).isActive = true
+        cardStackView.trailingAnchor.constraint(
+            equalTo: view.trailingAnchor,
+            constant: -boundaryConstant).isActive = true
+    }
+
 }
