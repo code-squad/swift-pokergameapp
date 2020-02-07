1765f75756e35e708022c9b6582ff9b63c6e8ead
delma <sdy2856@gmail.com>
2020-02-07 17:00:47 +0900

    card class에 CustomStringConvertible 프로토콜 적용

---
+++ CardGameApp/CardGameApp/CardDeck.swift
@@ -0,0 +1,12 @@
+//
+//  CardDeck.swift
+//  CardGameApp
+//
+//  Created by delma on 06/02/2020.
+//  Copyright © 2020 delma. All rights reserved.
+//
+
+import Foundation
+struct CardDeck {
+    
+}
