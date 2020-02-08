12f0e2aaf187b205520e360ba55c5bd27c85bb52
dev-Lena <keunnalee@gmail.com>
2020-02-09 00:07:06 +0900

    StatusBar 스타일을 LightContent로 설정

---
+++ CardGameApp/CardGameApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CardGameApp
+//
+//  Created by Keunna Lee on 2020/02/08.
+//  Copyright © 2020 Keunna Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# swift-pokergameapp
-STEP15 포커게임 앱 저장소
+# LENA의 Poker Game App
+
