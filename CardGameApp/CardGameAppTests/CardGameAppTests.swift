42a47cd6593eb54e243db899d023a7d8f3e8f150
dev-Lena <keunnalee@gmail.com>
2020-02-09 00:07:06 +0900

    StatusBar 스타일을 LightContent로 설정

---
+++ CardGameApp/CardGameAppTests/CardGameAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  CardGameAppTests.swift
+//  CardGameAppTests
+//
+//  Created by Keunna Lee on 2020/02/08.
+//  Copyright © 2020 Keunna Lee. All rights reserved.
+//
+
+import XCTest
+@testable import CardGameApp
+
+class CardGameAppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
