5d615201b77b1044f0d2fcb7a9354335adc9f2b0
TTOzzi <devttozzi@gmail.com>
2020-02-08 21:03:04 +0900

    Player, Dealer 클래스 생성
    플레이어 번호, 핸드, 딜러 핸드 추가
    아직 별도 동작을 구현해주진 않음

---
+++ CardGameApp/CardGameApp/Dealer.swift
@@ -0,0 +1,14 @@
+//
+//  Dealer.swift
+//  CardGameApp
+//
+//  Created by TTOzzi on 2020/02/08.
+//  Copyright © 2020 TTOzzi. All rights reserved.
+//
+
+import Foundation
+
+class Dealer {
+    var hand: [Card]?
+    
+}
