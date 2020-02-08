9dc48a4283ea7ad0a49ecbef2cf61a2814eb7297
TTOzzi <devttozzi@gmail.com>
2020-02-08 21:06:43 +0900

    FiveCardStud 클래스 생성
    딜러 변수와 참여한 플레이어의 수를 담을 변수 추가

---
+++ CardGameApp/CardGameApp/FiveCardStud.swift
@@ -0,0 +1,18 @@
+//
+//  FiveCardStud.swift
+//  CardGameApp
+//
+//  Created by TTOzzi on 2020/02/08.
+//  Copyright © 2020 TTOzzi. All rights reserved.
+//
+
+import Foundation
+
+class FiveCardStud {
+    var dealer = Dealer()
+    var numberOfPlayer: Int
+    
+    init(numberOfPlayer: Int) {
+        self.numberOfPlayer = numberOfPlayer
+    }
+}
