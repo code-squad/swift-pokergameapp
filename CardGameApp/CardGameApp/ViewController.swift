a32c6b19c3d35e7f136ffa624a008b861726086c
hanseop <hanseop95@gmail.com>
2020-02-08 20:24:04 +0900

    카드 보여주기 기능 추가
    UIStackView에 UIImageView를 추가하여 균일한 간격으로 카드를 보여줌
    또한 stackView에 Constraint를 설정하여 safe area와 간격을 둠

--- CardGameApp/CardGameApp/ViewController.swift
+++ CardGameApp/CardGameApp/ViewController.swift
@@ -9,15 +9,47 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let stackView=UIStackView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let backgroundColor = UIColor(patternImage: #imageLiteral(resourceName: "bg_pattern"))
-        self.view.backgroundColor = backgroundColor
+        
+        setUpBackground()
+        setUpStackView()
+        addCardIntoStackView()
     }
     
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return .lightContent
+    }
+    
+    func setUpBackground(){
+        self.view.backgroundColor = UIColor(patternImage: #imageLiteral(resourceName: "bg_pattern"))
+        self.view.addSubview(stackView)
+    }
+    
+    func setUpStackView() {
+        stackView.spacing=4
+        stackView.axis = .horizontal
+        stackView.distribution = .fillEqually
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        self.stackView.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 50).isActive=true
+        self.stackView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 5).isActive=true
+        self.stackView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -5).isActive=true
+    }
+    
+    func addCardIntoStackView(){
+        for _ in 0...6{
+            self.stackView.addArrangedSubview(createCard())
+        }
+    }
+    
+    func createCard() -> UIImageView{
+        let imageView=UIImageView(image: #imageLiteral(resourceName: "card-back"))
+        imageView.contentMode = .scaleAspectFill
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.heightAnchor.constraint(equalTo: imageView.widthAnchor, multiplier: 1.27).isActive=true
+        return imageView
     }
 }
 
