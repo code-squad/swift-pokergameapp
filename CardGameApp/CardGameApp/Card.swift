101de54b966ce62f3a070fa74de11103f0e3d676
dev-Lena <keunnalee@gmail.com>
2020-02-10 19:39:50 +0900

    Card 클래스(class) 생성 후 Suit 이넘(enum) 생성

--- CardGameApp/CardGameApp/Card.swift
+++ CardGameApp/CardGameApp/Card.swift
@@ -7,3 +7,9 @@
 //
 
 import Foundation
+
+class Card {
+    enum Suit: Character {// 한글자이기 때문에 캐릭터라고 했는데 String도 됨.
+        case spades = "♠️", hearts = "♥️", diamonds = "♦️", clubs = "♣️"
+    }
+}
