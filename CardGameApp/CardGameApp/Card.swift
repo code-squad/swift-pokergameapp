73a0153bc065b194789effdfda7442264f20cef7
dev-Lena <keunnalee@gmail.com>
2020-02-10 19:44:02 +0900

    Card 클래스에 rank와 suit를 프로퍼티로 추가하고 init 메소드 생성

--- CardGameApp/CardGameApp/Card.swift
+++ CardGameApp/CardGameApp/Card.swift
@@ -9,6 +9,15 @@
 import Foundation
 
 class Card {
+    
+    let suit : Suit
+    let rank : Rank
+    
+    init(suit: Suit, rank: Rank) {
+        self.suit = suit
+        self.rank = rank
+    }
+    
     enum Suit: Character {// 한글자이기 때문에 캐릭터라고 했는데 String도 됨.
         case spades = "♠️", hearts = "♥️", diamonds = "♦️", clubs = "♣️"
     }
