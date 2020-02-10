2750a7e1deea7734d86599a0d53fa99d727727b0
kjoonk <kjoonk@gmail.com>
2020-02-10 13:23:23 +0900

    Deck 클래스 생성
    - 카드 덱 관리용
    - count, shuffle 등 구현해야 할 기능 선언만 함

---
+++ CardGameApp/CardGameApp/Deck.swift
@@ -0,0 +1,31 @@
+//
+//  Deck.swift
+//  CardGameApp
+//
+//  Created by Viet on 2020/02/10.
+//  Copyright © 2020 Viet. All rights reserved.
+//
+
+import Foundation
+
+struct Deck {
+    /// 갖고 있는 카드 개수를 반환한다.
+    var count: Int {
+        0
+    }
+    
+    /// 전체 카드를 섞는다.
+    func shuffle() {
+        
+    }
+    
+    /// 카드를 하나 반환하고 목록에서 삭제
+    func removeOne() {
+        // 랜덤 추출?
+    }
+    
+    /// 모든 카드를 다시 채워놓는다.
+    func reset() {
+
+    }
+}
