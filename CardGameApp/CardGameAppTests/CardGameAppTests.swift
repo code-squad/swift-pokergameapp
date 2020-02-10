b145ec9a91df7702e9185b75615efd3424d692f7
dev-Lena <keunnalee@gmail.com>
2020-02-10 20:28:54 +0900

    Card 클래스의 프로퍼티와 초기화 값을 각각 Suit와 Rank(enum)에서 Suit.RawValue, Rank.RawValue로 변경

--- CardGameApp/CardGameAppTests/CardGameAppTests.swift
+++ CardGameApp/CardGameAppTests/CardGameAppTests.swift
@@ -18,26 +18,26 @@ class CardGameAppTests: XCTestCase {
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
+//
+//    func testExample() {
+//        // This is an example of a functional test case.
+//        // Use XCTAssert and related functions to verify your tests produce the correct results.
+//    }
+//
+//    func testPerformanceExample() {
+//        // This is an example of a performance test case.
+//        self.measure {
+//            // Put the code you want to measure the time of here.
+//        }
+//    }
     
     func testDescripteCard() {
         // Given
-        let card = Card(suit: "♥️", rank: three)
+        let card = Card(suit: .hearts , rank: .eight )
         // When
-        var description = card.descripteCard()
+        let description = card.descripteCard()
         // Then
-        XCTAssertEqual(description, "❤️3")
+        XCTAssertEqual(description, "♥️8", "맞습니다")
     }
 
 }
