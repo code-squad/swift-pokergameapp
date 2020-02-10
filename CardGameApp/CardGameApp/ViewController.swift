0edcf3b71931963f6add9f2ac3434877148016b9
limwin94 <amuguna94@gmail.com>
2020-02-10 13:44:25 +0900

    single view app project 생성

---
+++ CardGameApp/CardGameApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CardGameApp
+//
+//  Created by 임승혁 on 2020/02/10.
+//  Copyright © 2020 임승혁. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
