feda1be44d6e6704503e957a0d5fcff26d696d80
kjoonk <kjoonk@gmail.com>
2020-02-10 22:25:42 +0900

    CardTest 추가
    - 주요 메소드에 대해 테스트 추가: count, removeOne, reset

--- CardGameApp/CardGameAppTests/CardTests.swift
+++ CardGameApp/CardGameAppTests/CardTests.swift
@@ -9,18 +9,29 @@
 import XCTest
 
 class CardTests: XCTestCase {
-
-    override func setUp() {
-    }
-
-    override func tearDown() {
-    }
-
-    func testExample() {
+    func testNewCard() {
         let card = Card(suit: .heart, rank: .king)
         XCTAssertEqual(card.description, "♥️ K")
         
         let card2 = Card(suit: .spade, rank: .ten)
         XCTAssertEqual(card2.description, "♠️ 10")
+    }
+    
+    func testCardDeck() {
+        // count() 테스트
+        var deck = Deck()
+        let numberOfAllCards = 13 * 4
+        XCTAssertEqual(deck.count, numberOfAllCards)
+        
+        // removeOne() 테스트
+        let someCard = deck.removeOne()
+        XCTAssert(someCard is Card)
+        XCTAssertEqual(deck.count, numberOfAllCards - 1)
+        deck.removeOne()
+        XCTAssertEqual(deck.count, numberOfAllCards - 2)
+        
+        // reset() 테스트
+        deck.reset()
+        XCTAssertEqual(deck.count, numberOfAllCards)
     }
 }
