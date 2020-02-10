4375c58282356f36194bbe256a10c830d6cfcef3
delma <sdy2856@gmail.com>
2020-02-10 21:22:07 +0900

    Add: Dealer, Player, PokerGame class

---
+++ CardGameApp/CardGameApp/Model/Player.swift
@@ -0,0 +1,13 @@
+//
+//  Player.swift
+//  CardGameApp
+//
+//  Created by delma on 10/02/2020.
+//  Copyright © 2020 delma. All rights reserved.
+//
+
+import Foundation
+class Player {
+    var cards = [Card]()
+    
+}
