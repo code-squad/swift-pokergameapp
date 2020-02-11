61f62fddd80c4aef9a3b2ae66b0232bc563745ab
TTOzzi <devttozzi@gmail.com>
2020-02-11 15:04:00 +0900

    플레이어, 딜러 표시 Label 추가
    label 을 만들어 플레이어 순서에 맞게 텍스트를 넣은 뒤 playerStack 에 추가해줌
    constraint 로 스택뷰의 spacing 에 영향을 받지 않고 카드스택 위에 붙어있도록 해줌

--- CardGameApp/CardGameApp/ViewController.swift
+++ CardGameApp/CardGameApp/ViewController.swift
@@ -10,13 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    var pokerGame = PokerGame(gameType: .fiveCardStud, numberOfPlayers: .four)
+    var pokerGame = PokerGame(gameType: .fiveCardStud, numberOfPlayers: .two)
     
     var playerStack: UIStackView = {
         let stack = UIStackView()
         stack.axis = .vertical
-        stack.distribution = .fillEqually
-        stack.spacing = 10
+        stack.spacing = 30
         stack.translatesAutoresizingMaskIntoConstraints = false
         return stack
     }()
@@ -29,13 +28,22 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = UIColor(patternImage: #imageLiteral(resourceName: "bg_pattern"))
         pokerGame.play()
-        game()
+        makeGame()
         setPlayerStackLayout()
     }
     
-    func game() {
+    func makeGame() {
+        var index = 1
         pokerGame.players.forEach {
-            playerStack.addArrangedSubview(makePlayer($0))
+            let playerLabel = makePlayerLabel(playerName: "Player\(index)")
+            if index == pokerGame.players.count {
+                playerLabel.text = "Dealer"
+            }
+            let playerCard = makePlayer($0)
+            playerStack.addArrangedSubview(playerLabel)
+            playerStack.addArrangedSubview(playerCard)
+            playerLabel.bottomAnchor.constraint(equalTo: playerCard.topAnchor).isActive = true
+            index += 1
         }
         self.view.addSubview(playerStack)
     }
@@ -44,6 +52,17 @@ class ViewController: UIViewController {
         playerStack.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 50).isActive = true
         playerStack.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 10).isActive = true
         playerStack.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -10).isActive = true
+    }
+    
+    func makePlayerLabel(playerName: String) -> UILabel {
+        let playerLabel: UILabel = {
+            let label = UILabel()
+            label.text = playerName
+            label.textColor = .white
+            label.translatesAutoresizingMaskIntoConstraints = false
+            return label
+        }()
+        return playerLabel
     }
     
     func makePlayer(_ player: Player) -> UIStackView {
