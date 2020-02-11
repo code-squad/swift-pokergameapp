36cffd265be554e6e25b199d7b61230e558e70ab
idevjinie <idevjinie@gmail.com>
2020-02-11 15:21:12 +0900

    CardGameApp 프로젝트 생성

---
+++ CardGameApp/CardGameApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CardGameApp
+//
+//  Created by jinie on 2020/02/11.
+//  Copyright © 2020 jinie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
