623b83eb00b4db0441398e44f48a8369cde8e5c5
1Consumption <hanseop95@gmail.com>
2020-02-11 21:48:44 +0900

    PokerGame class 추가
    포커게임에 대한 데이터를 저장하고 로직을 처리 해주기위한 클래스

---
+++ CardGameApp/CardGameApp/model/pokerGame.swift
@@ -0,0 +1,13 @@
+//
+//  pokerGame.swift
+//  CardGameApp
+//
+//  Created by 신한섭 on 2020/02/11.
+//  Copyright © 2020 신한섭. All rights reserved.
+//
+
+import Foundation
+
+class PokerGame{
+    
+}
