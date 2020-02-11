08fedbd6106b8f3991d42aab927dabb7b662cf14
TTOzzi <devttozzi@gmail.com>
2020-02-11 14:29:29 +0900

    화면 카드 출력 구현
    pokerGame 의 게임타입, 참여자 수에 맞게 화면에 카드를 출력하도록 구현해줌
    카드를 카드스택에 담고, 카드스택을 플레이어 스택에 담은 형태

--- CardGameApp/CardGameApp/ViewController.swift
+++ CardGameApp/CardGameApp/ViewController.swift
@@ -10,12 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    var cardStack: UIStackView = {
+    var pokerGame = PokerGame(gameType: .fiveCardStud, numberOfPlayers: .four)
+    
+    var playerStack: UIStackView = {
         let stack = UIStackView()
-        stack.translatesAutoresizingMaskIntoConstraints = false
+        stack.axis = .vertical
         stack.distribution = .fillEqually
-        stack.axis = .horizontal
-        stack.spacing = 5
+        stack.spacing = 10
+        stack.translatesAutoresizingMaskIntoConstraints = false
         return stack
     }()
     
@@ -25,20 +27,55 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        makeCards()
         view.backgroundColor = UIColor(patternImage: #imageLiteral(resourceName: "bg_pattern"))
-        self.view.addSubview(cardStack)
-        cardStack.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 30).isActive = true
-        cardStack.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 5).isActive = true
-        cardStack.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -5).isActive = true
+        pokerGame.play()
+        game()
+        setPlayerStackLayout()
     }
     
-    func makeCards() {
-        for _ in 0..<7 {
-            let card = UIImageView(image: #imageLiteral(resourceName: "card-back"))
-            card.heightAnchor.constraint(equalTo: card.widthAnchor, multiplier: 1.27).isActive = true
-            cardStack.addArrangedSubview(card)
+    func game() {
+        pokerGame.players.forEach {
+            playerStack.addArrangedSubview(makePlayer($0))
         }
+        self.view.addSubview(playerStack)
+    }
+    
+    func setPlayerStackLayout() {
+        playerStack.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 50).isActive = true
+        playerStack.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 10).isActive = true
+        playerStack.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -10).isActive = true
+    }
+    
+    func makePlayer(_ player: Player) -> UIStackView {
+        let cardStack = makeCardStack()
+        player.handInfo.forEach {
+            let card = makeCard($0)
+            cardStack.addArrangedSubview(card)
+            card.heightAnchor.constraint(equalTo: card.widthAnchor, multiplier: 1.27).isActive = true
+        }
+        return cardStack
+    }
+    
+    func makeCardStack() -> UIStackView {
+        let cardStack: UIStackView = {
+            let stack = UIStackView()
+            stack.distribution = .fillEqually
+            stack.spacing = -5
+            return stack
+        }()
+        return cardStack
+    }
+    
+    func makeCard(_ cardInfo: Card) -> UIImageView {
+        let card: UIImageView = {
+            let imageView = UIImageView()
+            imageView.translatesAutoresizingMaskIntoConstraints = false
+            imageView.layer.cornerRadius = 3.0
+            imageView.clipsToBounds = true
+            return imageView
+        }()
+        card.image = UIImage(named: "\(cardInfo).png")
+        return card
     }
 }
 
