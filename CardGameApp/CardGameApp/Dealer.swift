83e90f755c6d5c3ad61a2c9b0039eac27cf81e1b
kjoonk <kjoonk@gmail.com>
2020-02-11 17:07:41 +0900

    Dealer, Playable 일부 기능 구현
    - 구현을 위해 게임 규칙은 7-stud로 고정시킴. 추후 구현할 것
    - prepare, startGame 메소드 구현
    - give 메소드 구현
    - Dealer 관련 테스트 작성

--- CardGameApp/CardGameApp/Dealer.swift
+++ CardGameApp/CardGameApp/Dealer.swift
@@ -8,16 +8,18 @@
 
 import Foundation
 class Dealer: Playable {
+
     var cards = [Card]()
     var deck: Deck
     
     init(deck: Deck) {
         self.deck = deck
     }
-    
+
     /// playerㅇ에게 한 장의 카드를 건넴
     func give(card: Card, to player: Playable) {
-        
+        var player = player
+        player.receive(card: card)
     }
     
     /// 게임 준비. 카드를 섞는다
@@ -26,12 +28,20 @@ class Dealer: Playable {
         self.deck.shuffle()
     }
     
-    /// 게임을 시작한다. 자신을 포함해 모든 플레이어에게 카드를 돌린다.
-    func startGame(players: [Playable]) {
+    fileprivate func dealCardsOfOneRound(_ players: [Playable]) {
         (players + [self]).forEach { player in
             if let card = deck.removeOne() {
                 self.give(card: card, to: player)
             }
+        }
+    }
+    
+    /// 게임을 시작한다. 자신을 포함해 모든 플레이어에게 카드를 돌린다.
+    func startGame(players: [Playable]) {
+        let numberOfCardsByRule = 7
+        
+        (1...numberOfCardsByRule).forEach { _ in
+            dealCardsOfOneRound(players)
         }
     }
 }
@@ -42,4 +52,10 @@ class Player: Playable {
 
 protocol Playable {
     var cards: [Card] { get set }
+}
+
+extension Playable {
+    mutating func receive(card: Card) {
+        self.cards.append(card)
+    }
 }
--- CardGameApp/CardGameAppTests/CardTests.swift
+++ CardGameApp/CardGameAppTests/CardTests.swift
@@ -11,10 +11,10 @@ import XCTest
 class CardTests: XCTestCase {
     func testNewCard() {
         let card = Card(suit: .heart, rank: .king)
-        XCTAssertEqual(card.description, "♥️ K")
+        XCTAssertEqual(card.description, "♥️K")
         
         let card2 = Card(suit: .spade, rank: .ten)
-        XCTAssertEqual(card2.description, "♠️ 10")
+        XCTAssertEqual(card2.description, "♠️10")
     }
     
 }
