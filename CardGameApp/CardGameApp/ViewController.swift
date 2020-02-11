e49bc09e6b194add96b3048a376187f6df191033
MagnaPax <MagnaPax@outlook.kr>
2020-02-11 19:11:32 +0900

    이미지 파일을 Asset에 추가하고 배경을 이미지 패턴으로 지정

--- CardGameApp/CardGameApp/ViewController.swift
+++ CardGameApp/CardGameApp/ViewController.swift
@@ -12,7 +12,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.view.backgroundColor = UIColor.init(patternImage: (UIImage.init(named: "bg_pattern")!))
+
     }
     
 
