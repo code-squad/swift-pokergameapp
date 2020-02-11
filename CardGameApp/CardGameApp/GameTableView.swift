a7f58638cdbadc2cfb32431904b0069541cb34f1
GangWoon <gangwoon2018@gmail.com>
2020-02-11 11:51:06 +0900

    refactoring about lazy var property

--- CardGameApp/CardGameApp/GameTableView.swift
+++ CardGameApp/CardGameApp/GameTableView.swift
@@ -9,22 +9,17 @@
 import UIKit
 import SnapKit
 
-class GameTableView: UIView {
+final class GameTableView: UIView {
     
-    lazy var stackView: UIStackView! = {
-        let stackView: UIStackView = UIStackView()
-        creatCards(stackView, amount: 6)
-        configuration(stackView)
-        addSubview(stackView)
-        return stackView
-    }()
+    private var stackView: UIStackView = UIStackView()
     private let viewBackground: String = "bg_pattern"
     private let cardBackground: String = "card-back"
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         patternBackground()
-        makeStackConstraint()
+        configuration(stackView)
+        makeStackConstraint(stackView)
     }
     
     required init?(coder: NSCoder) {
@@ -34,7 +29,7 @@ class GameTableView: UIView {
     private func patternBackground() {
         backgroundColor = UIColor(patternImage: UIImage(named: viewBackground) ?? UIImage())
     }
-    
+
     private func creatCards(_ stack: UIStackView, amount: Int) {
         (0...amount).forEach { _ in
             let card = UIImageView(image: UIImage(named: cardBackground) ?? UIImage())
@@ -46,11 +41,13 @@ class GameTableView: UIView {
     }
     
     private func configuration(_ stack: UIStackView) {
+        creatCards(stackView, amount: 6)
         stack.distribution = .equalSpacing
         stack.spacing = 4
+        addSubview(stackView)
     }
     
-    private func makeStackConstraint() {
+    private func makeStackConstraint(_ stackView: UIStackView) {
         stackView.snp.makeConstraints { (make) in
             make.top.equalTo(safeAreaLayoutGuide).offset(16)
             make.leading.trailing.equalTo(safeAreaLayoutGuide).inset(8)
