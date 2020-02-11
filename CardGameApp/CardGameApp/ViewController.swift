704f5edecc440b6d339d48b1f3cfc883a9e36a8a
MagnaPax <MagnaPax@outlook.kr>
2020-02-11 17:00:54 +0900

    iOS 프로젝트 Single View App 템플릿 생성

---
+++ CardGameApp/CardGameApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CardGameApp
+//
+//  Created by 한천희 on 2020/02/11.
+//  Copyright © 2020 한천희. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
