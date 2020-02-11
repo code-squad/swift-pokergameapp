f049d4432f328ed49c8f45e60b0e1e1d5c3cf7a3
kjoonk <kjoonk@gmail.com>
2020-02-11 14:21:26 +0900

    Dealer 클래스 생성
    - 카드를 돌리는 빈 메소드 정의

---
+++ CardGameApp/CardGameApp/Dealer.swift
@@ -0,0 +1,18 @@
+//
+//  Dealer.swift
+//  CardGameApp
+//
+//  Created by Viet on 2020/02/11.
+//  Copyright © 2020 Viet. All rights reserved.
+//
+
+import Foundation
+class Dealer {
+    func deal(_ card: Card, to player: Player) {
+        
+    }
+}
+
+class Player {
+    
+}
