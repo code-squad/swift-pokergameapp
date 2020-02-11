2dbdb933674d678a6cf82bb9648095ea7253d6f8
corykim0829 <corykim0829@gmail.com>
2020-02-11 22:00:31 +0900

    Refactor: SegmentedControls, CardStackView

--- CardGameApp/CardGameApp/Views/PokerGameStackViews.swift
+++ CardGameApp/CardGameApp/Views/PokerGameStackViews.swift
@@ -28,7 +28,6 @@ class PokerGameStackView: UIStackView {
 }
 
 class PlayerStackView: UIStackView {
-    
     private var displayName: String = ""
     let nameLabel = UILabel()
     
@@ -55,21 +54,19 @@ class PlayerStackView: UIStackView {
 }
 
 class CardStackView: UIStackView {
+    private var player: Playable?
     
     init(of player: Playable) {
+        self.player = player
         super.init(frame: .zero)
         commonInit()
-        setupCards(of: player)
-    }
-    
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        commonInit()
+        setupCards()
     }
     
     required init(coder: NSCoder) {
         super.init(coder: coder)
         commonInit()
+        setupCards()
     }
     
     private func commonInit() {
@@ -78,8 +75,8 @@ class CardStackView: UIStackView {
         translatesAutoresizingMaskIntoConstraints = false
     }
     
-    private func setupCards(of player: Playable) {
-        player.hands.map { $0.description }
+    private func setupCards() {
+        player?.hands.map { $0.description }
             .forEach({ (filename) in
                 addArrangedSubview(CardImageView(named: filename))
             })
---
+++ CardGameApp/CardGameApp/Views/SegmentedControls.swift
@@ -0,0 +1,36 @@
+//
+//  SegmentedControls.swift
+//  CardGameApp
+//
+//  Created by Cory Kim on 2020/02/11.
+//  Copyright © 2020 corykim0829. All rights reserved.
+//
+
+import UIKit
+
+class GameTypeSegmentedControl: UISegmentedControl {
+    private var items: [String]?
+    
+    init(items: [String]) {
+        self.items = items
+        super.init(frame: .zero)
+        commonInit()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        commonInit()
+    }
+    
+    private func commonInit() {
+        setupItems()
+        selectedSegmentIndex = 0
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    private func setupItems() {
+        items?.enumerated().forEach { (index, item) in
+            insertSegment(withTitle: item, at: index, animated: false)
+        }
+    }
+}
