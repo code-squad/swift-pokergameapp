09e3be105542266c29fe07e7a86a29452d427c75
idevjinie <idevjinie@gmail.com>
2020-02-12 13:00:05 +0900

    화면을 균등하게 7등분하여 7개의 카드 출력

--- CardGameApp/CardGameApp/ViewController.swift
+++ CardGameApp/CardGameApp/ViewController.swift
@@ -16,7 +16,41 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        drawUI()
+    }
+    
+    func specifyStackViewProperty(of stackView: UIStackView) {
+        stackView.axis = .horizontal
+        stackView.distribution = .fillEqually
+        stackView.alignment = .fill
+        stackView.spacing = 5
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    func addCardViews() -> UIStackView {
+        let stackView = UIStackView()
+        specifyStackViewProperty(of: stackView)
+        
+        for _ in 0..<7 {
+            let cardView = UIImageView(image: UIImage(named: "card-back"))
+            stackView.addArrangedSubview(cardView)
+            cardView.heightAnchor.constraint(equalTo: cardView.widthAnchor, multiplier: 1.27).isActive = true
+        }
+        
+        return stackView
+    }
+    
+    func drawUI() {
         self.view.backgroundColor = UIColor(patternImage: UIImage(named: "bg_pattern")!)
+        
+        let stackView = addCardViews()
+        self.view.addSubview(stackView)
+        let constraints = [
+            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10.0),
+            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10.0),
+            stackView.topAnchor.constraint(equalTo: view.topAnchor, constant: 50.0)
+        ]
+        NSLayoutConstraint.activate(constraints)
     }
 
 }
