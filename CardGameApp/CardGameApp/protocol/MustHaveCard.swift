15afcfe2f2d077f8a43cb9ace4ecdbff69ce22af
1Consumption <hanseop95@gmail.com>
2020-02-12 13:33:11 +0900

    MustHaveCard 프로토콜 추가
    Player와 Dealer가 카드를 가져야한다는 공통점을 프로토콜로 강제함

---
+++ CardGameApp/CardGameApp/protocol/MustHaveCard.swift
@@ -0,0 +1,12 @@
+//
+//  MustHaveCard.swift
+//  CardGameApp
+//
+//  Created by 신한섭 on 2020/02/12.
+//  Copyright © 2020 신한섭. All rights reserved.
+//
+
+import Foundation
+
+protocol MustHaveCard{
+}
