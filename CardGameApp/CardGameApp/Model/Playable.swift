8e249cc5b7382ea52afca8ae4cec796740581512
delma <sdy2856@gmail.com>
2020-02-12 22:11:18 +0900

    add: Playable 프로토콜 추가로  상속관계 제거

---
+++ CardGameApp/CardGameApp/Model/Playable.swift
@@ -0,0 +1,12 @@
+//
+//  Playable.swift
+//  CardGameApp
+//
+//  Created by delma on 12/02/2020.
+//  Copyright © 2020 delma. All rights reserved.
+//
+
+import Foundation
+protocol Playable {
+    func appendCard(_ card: Card)
+}
