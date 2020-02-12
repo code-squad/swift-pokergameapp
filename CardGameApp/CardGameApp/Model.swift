c93e773def7f3bba6772aa32d3f153b3c4d5ba2d
GangWoon <gangwoon2018@gmail.com>
2020-02-12 12:15:02 +0900

    New deck struct

--- CardGameApp/CardGameApp/Model.swift
+++ CardGameApp/CardGameApp/Model.swift
@@ -8,10 +8,9 @@
 
 import Foundation
 
-class Card {
-    
+final class Card {
     // class는 더 많은 기능을 사용할때 사용할 거 같았습니다. enum은 작은 범위를 나눌때 유용했습니다.
-    enum suit: String {
+    enum suit: String, CaseIterable {
         case spade = "♤"
         case hert = "❤"
         case club = "♧"
@@ -46,3 +45,40 @@ extension Card: CustomStringConvertible {
     }
 }
 
+struct Deck {
+    
+    private var cards: [Card] = []
+    
+    var count: Int {
+        return cards.count
+    }
+    
+    init() {
+        makeDeck()
+    }
+    
+    mutating func shuffle() {
+        cards.shuffle()
+    }
+    
+    mutating func removeOne() -> Card? {
+        cards.popLast()
+    }
+    
+    mutating func reset() {
+        cards.removeAll()
+        
+    }
+    
+    private func makeSuit(suit: Card.suit) -> [Card] {
+        return (1...13).map {
+            Card(suit: suit, rank: Card.rank(rawValue: $0)!)
+        }
+    }
+    
+    private mutating func makeDeck() {
+        Card.suit.allCases.forEach {
+            cards.append(contentsOf: makeSuit(suit: $0))
+        }
+    }
+}
