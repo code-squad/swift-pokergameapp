03a141b032243e7d6f0c4510f765249206f213c1
seizze <cwcwc3@gmail.com>
2020-02-13 17:51:28 +0900

    refactor: Move UIView extension to utils

--- CardGameApp/CardGameApp/Views/PlayModeSelectionView.swift
+++ CardGameApp/CardGameApp/Views/PlayModeSelectionView.swift
@@ -70,13 +70,3 @@ class PlayModeSelectionView: UIView {
         }
     }
 }
-
-extension UIView {
-    func fillSuperView() {
-        guard let superview = superview else { return }
-        topAnchor.constraint(equalTo: superview.topAnchor).isActive = true
-        leadingAnchor.constraint(equalTo: superview.leadingAnchor).isActive = true
-        trailingAnchor.constraint(equalTo: superview.trailingAnchor).isActive = true
-        bottomAnchor.constraint(equalTo: superview.bottomAnchor).isActive = true
-    }
-}
