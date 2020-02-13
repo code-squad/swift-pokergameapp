f60295bb380e4500e5e5417371c9c77e6f57ae0f
corykim0829 <corykim0829@gmail.com>
2020-02-13 15:46:51 +0900

    Refactor: Remove comments

--- CardGameApp/CardGameApp/Models/Card.swift
+++ CardGameApp/CardGameApp/Models/Card.swift
@@ -9,12 +9,6 @@
 import Foundation
 
 struct Card {
-    // 카드 객체의 모양을 저장할 suit는 Nested enum type인 Suit이다.
-    // 카드 정보를 출력할 때 바로 Character를 반환해주기 위해 raw value는 Character를 사용하였다.
-    // 카드 객체의 숫자(값)을 저장할 rank는 Nested enum type인 Rank이다.
-    // 게임을 할 때에는 상대적인 값이 중요할 것 같아서 raw value를 Int로 정하고
-    // toString()함수에서 출력 정보는 값에 따라 분류하였다.
-    
     let suit: Suit
     let rank: Rank
     
