eb477f67c117ad612bc90a7a793d28d52c003474
corykim0829 <corykim0829@gmail.com>
2020-02-13 16:07:17 +0900

    Refactor: Card CustomStringConvertible

---
+++ CardGameApp/CardGameApp/Models/Deck 2.swift
@@ -0,0 +1,40 @@
+//
+//  CardDeck.swift
+//  CardGameApp
+//
+//  Created by Cory Kim on 2020/02/06.
+//  Copyright © 2020 corykim0829. All rights reserved.
+//
+
+import Foundation
+
+struct Deck {
+    private var cards: [Card] = []
+    
+    init() {
+        setupDeck()
+    }
+    
+    private mutating func setupDeck() {
+        Card.Suit.allCases.forEach {
+            let suit = $0
+            Card.Rank.allCases.forEach {
+                cards.append(Card(suit: suit, rank: $0))
+            }
+        }
+    }
+    
+    mutating func shuffle() {
+        cards.shuffle()
+    }
+    
+    mutating func removeOne() -> Card? {
+        guard cards.count > 0 else { return nil }
+        return cards.removeLast()
+    }
+    
+    mutating func reset() {
+        cards.removeAll()
+        setupDeck()
+    }
+}
