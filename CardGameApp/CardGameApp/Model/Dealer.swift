2169800eb7e5deb6dd40ef298e2532dd910570d3
GangWoon <gangwoon2018@gmail.com>
2020-02-14 15:21:41 +0900

    딜러 클래스 생성
    기본 구조만 만들었습니다.

---
+++ CardGameApp/CardGameApp/Model/Dealer.swift
@@ -0,0 +1,27 @@
+//
+//  Dealer.swift
+//  CardGameApp
+//
+//  Created by Cloud on 2020/02/14.
+//  Copyright © 2020 Cloud. All rights reserved.
+//
+
+import Foundation
+
+class Dealer: Gamer {
+    
+    private var deck: Deck = Deck()
+    
+    init() {
+        super.init(identifier: "Dealer")
+    }
+    
+    func shuffle() {
+        deck.shuffle()
+    }
+    
+    func pushCard() -> Card? {
+        return deck.removeOne()
+    }
+}
+
