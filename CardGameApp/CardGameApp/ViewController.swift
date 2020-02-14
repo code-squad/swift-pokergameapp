fc17ad55d64c77700f13e587811893e0e91d6a5b
Anna <bluewhale0528@naver.com>
2020-02-14 18:13:18 +0900

    Dealer가 자기자신 그리고 참가자들에게 생성할때 정해진 카드의 갯수만큼 분배하는 기능 추가

--- CardGameApp/CardGameApp/ViewController.swift
+++ CardGameApp/CardGameApp/ViewController.swift
@@ -26,6 +26,7 @@ class ViewController: UIViewController {
         print(spadesOfSeven)
         
         let pokerGame = PokerGame(gameMode: .sevenCardStud, playerMode: .three)
+        pokerGame.giveCards()
     }
 
     override var preferredStatusBarStyle: UIStatusBarStyle {
