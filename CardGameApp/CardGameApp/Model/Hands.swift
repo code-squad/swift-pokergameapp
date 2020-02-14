1f695d2e134dd04f67a0869b1f088bc9bfa4b02e
GangWoon <gangwoon2018@gmail.com>
2020-02-14 15:16:43 +0900

    카드 구조체 생성
    기본으로 필요한 속성을 만들었습니다.

---
+++ CardGameApp/CardGameApp/Model/Hands.swift
@@ -0,0 +1,22 @@
+//
+//  Hands.swift
+//  CardGameApp
+//
+//  Created by Cloud on 2020/02/14.
+//  Copyright © 2020 Cloud. All rights reserved.
+//
+
+import Foundation
+
+struct Hands {
+    
+    private var cards: [Card] = []
+    var count: Int {
+        cards.count
+    }
+    
+    mutating func add(_ card: Card?) {
+        guard let card = card else { return }
+        cards.append(card)
+    }
+}
