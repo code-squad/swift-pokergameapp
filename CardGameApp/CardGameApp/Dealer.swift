8b5f2206d9718ba53906f1fc0d096b03daa75660
kjoonk <kjoonk@gmail.com>
2020-02-14 16:34:01 +0900

    Dealer.startGame 기능을 Game으로 옮김
    - Dealer에게 넘겼던 기능을 Game이 하도록 함
    - Dealer는 룰에 상관없이 한장씩 카드를 돌리도록 변경
    - DealerTests에 중복되어 있던 Game 관련 테스트 제거

--- CardGameApp/CardGameApp/Dealer.swift
+++ CardGameApp/CardGameApp/Dealer.swift
@@ -6,28 +6,18 @@ class Dealer: Playable {
         self.deck = deck
     }
 
-    /// 게임을 시작한다. 자신을 포함해 모든 플레이어에게 카드를 돌린다.
-    func startGame(rule: Game.Rule, players: [Playable]) {
-        self.prepare()
-        
-        let numberOfCards = rule.rawValue
-        (1...numberOfCards).forEach { _ in
-            dealCardsOfOneRound(players)
-        }
+    /// 게임 준비. 카드를 섞는다
+    func prepare() {
+        self.deck.reset()
+        self.deck.shuffle()
     }
     
     func receive(card: Card) {
         self.cards.append(card)
     }
 
-    /// 게임 준비. 카드를 섞는다
-    private func prepare() {
-        self.deck.reset()
-        self.deck.shuffle()
-    }
-    
     // 카드를 한 장 뽑아 플레이어들에게 돌린다.
-    private func dealCardsOfOneRound(_ players: [Playable]) {
+    func dealCardsOfOneRound(_ players: [Playable]) {
         (players + [self]).forEach { player in
             if let card = deck.removeOne() {
                 player.receive(card: card)
--- CardGameApp/CardGameApp/Game.swift
+++ CardGameApp/CardGameApp/Game.swift
@@ -4,12 +4,12 @@ struct Game {
         case fiveStud = 5
         case sevenStud = 7
     }
-
+        
     private let playerRange = 1...4
     private let rule: Rule
     private let dealer: Dealer
     private let players: [Playable]
-    
+
     init(rule: Rule, dealer: Dealer, players: [Playable]) throws {
         guard playerRange.contains(players.count) else {
             throw GameError.playersOutOfRange
@@ -20,11 +20,31 @@ struct Game {
         self.players = players
     }
     
+    /// 게임을 시작한다. 자신을 포함해 모든 플레이어에게 카드를 돌린다.
     func start() {
-        dealer.startGame(rule: rule, players: players)
+        dealer.prepare()
+        
+        let numberOfCards = rule.rawValue
+        (1...numberOfCards).forEach { _ in
+            dealer.dealCardsOfOneRound(players)
+        }
     }
 }
 
 enum GameError: Error {
     case playersOutOfRange
 }
+
+//enum Rule {
+//    case fiveStud
+//    case sevenStud
+//
+//    struct SevenStudRule {
+//        let requiredCard = 7
+//    }
+//
+//    struct FiveStudRule {
+//        let requiredCard = 5
+//    }
+//}
+//
