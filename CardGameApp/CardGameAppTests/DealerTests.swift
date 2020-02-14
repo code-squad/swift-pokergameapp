bf46e5ced5dcd40a0b72f84118cd9b29c4b3f84b
kjoonk <kjoonk@gmail.com>
2020-02-14 14:10:35 +0900

    Dealer, Player의 cards 속성을 private으로 변경
    - Playable 프로토콜 변경: isReadyToGame 메소드 추가
    - Dealer.give 메소드 제거: receive 메소드에서 처리하게 변경
    - 그에 따른 테스트 코드 수정

--- CardGameApp/CardGameAppTests/DealerTests.swift
+++ CardGameApp/CardGameAppTests/DealerTests.swift
@@ -9,28 +9,16 @@ class DealerTests: XCTestCase {
         self.dealer = Dealer(deck: deck)
     }
     
-    func testDealCard() {
-        let players = [Player(), Player(), Player(), Player()]
-        let card = deck.removeOne()!
-        let player = players.first!
-        
-        dealer.give(card: card, to: player)
-        
-        XCTAssertEqual(player.cards.count, 1)
-    }
-    
     func testStartGameOfSevenStud() {
         let players = try! CardGameFactory.createPlayers(count: 4)
         let numberOfCards = Game.Rule.sevenStud.rawValue
         
         dealer.startGame(rule: .sevenStud, players: players)
         
-        print(dealer.cards)
-        XCTAssertEqual(dealer.cards.count, numberOfCards)
+        XCTAssertTrue(dealer.isReadyToGame(requiredCards: numberOfCards))
         
         let receivedCorrectly = players.filter { player in
-            print(player.cards)
-            return player.cards.count == numberOfCards
+            player.isReadyToGame(requiredCards: numberOfCards)
         }
         
         XCTAssertEqual(receivedCorrectly.count, players.count)
@@ -42,8 +30,9 @@ class DealerTests: XCTestCase {
         
         dealer.startGame(rule: .fiveStud, players: players)
         
-        let receivedCorrectly = players.filter { $0.cards.count == numberOfCards }
+        let receivedCorrectly = players.filter { $0.isReadyToGame(requiredCards: numberOfCards)
+        }
         XCTAssertEqual(receivedCorrectly.count, players.count)
-        XCTAssertEqual(dealer.cards.count, numberOfCards)
+        XCTAssertTrue(dealer.isReadyToGame(requiredCards: numberOfCards))
     }
 }
