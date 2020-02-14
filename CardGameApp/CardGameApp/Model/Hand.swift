194ce124c84ceec694008712d21a1624cff594df
TTOzzi <devttozzi@gmail.com>
2020-02-14 13:45:28 +0900

    점수 계산을 위한 함수 구현
    set 을 활용해 스트레이트를 먼저 확인하고
    dictionary 로 같은 카드의 갯수 확인

--- CardGameApp/CardGameApp/Model/Hand.swift
+++ CardGameApp/CardGameApp/Model/Hand.swift
@@ -9,6 +9,14 @@
 import Foundation
 
 class Hand {
+    enum Score: Int {
+        case none = 0
+        case onePair
+        case twoPair
+        case triple
+        case fourCard
+        case straight
+    }
     private var cards = [Card]()
     
     func forEach(_ transform: (Card) -> ()) {
@@ -19,7 +27,7 @@ class Hand {
         cards.append(card)
     }
     
-    func checkStraight() -> Bool {
+    private func checkStraight() -> Bool {
         let cardSet = Set<Int>(cards.map { $0.number }).sorted()
         var straightCount = 1
         var index = 1
@@ -34,7 +42,7 @@ class Hand {
         return straightCount == 5
     }
     
-    func getInfo() -> [Int:Int]{
+    private func getInfo() -> [Int:Int]{
         let result = cards.reduce(into: [Int:Int]()) {
             if $0[$1.number] == nil {
                 $0[$1.number] = 1
@@ -43,5 +51,34 @@ class Hand {
             }
         }
         return result
+    }
+    
+    func getScore() -> (Score ,Int) {
+        let result = getInfo()
+        let info = result.sorted { (lhs, rhs) -> Bool in
+            if lhs.value == rhs.value {
+                return lhs.key > rhs.key
+            } else {
+                return lhs.value > rhs.value
+            }
+        }
+        
+        if checkStraight() {
+            return (.straight, info[0].key)
+        } else if info.contains(where: { $0.value == 4 }) {
+            return (.fourCard, info[0].key)
+        } else if info.contains(where: { $0.value == 3 }) {
+            return (.triple, info[0].key)
+        }
+        
+        let pairs = info.filter { $0.value == 2 }
+        
+        if pairs.count == 2 {
+            return (.twoPair, info[0].key)
+        } else if pairs.count == 1 {
+            return (.onePair, info[0].key)
+        } else {
+            return (.none, info[0].key)
+        }
     }
 }
