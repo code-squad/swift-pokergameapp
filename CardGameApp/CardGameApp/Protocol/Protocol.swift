6c90e0935046ad45b48b1df21188b99a20cba764
GangWoon <gangwoon2018@gmail.com>
2020-02-14 15:15:13 +0900

    프로토콜 파일 생성

---
+++ CardGameApp/CardGameApp/Protocol/Protocol.swift
@@ -0,0 +1,11 @@
+//
+//  Protocol.swift
+//  CardGameApp
+//
+//  Created by Cloud on 2020/02/14.
+//  Copyright © 2020 Cloud. All rights reserved.
+//
+
+protocol CustomIntegerConvertible {
+    var description: Int { get }
+}
