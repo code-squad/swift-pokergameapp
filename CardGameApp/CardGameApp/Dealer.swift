b12ce921146f39279587998ac74560d4cd2d438b
Anna <bluewhale0528@naver.com>
2020-02-14 17:33:41 +0900

    Dealer에 private 접근권한으로 카드덱 프로퍼티 추가

--- CardGameApp/CardGameApp/Dealer.swift
+++ CardGameApp/CardGameApp/Dealer.swift
@@ -3,6 +3,7 @@ import Foundation
 class Dealer: Player {
     var role = "딜러"
     var cards = [Card]()
+    private var cardDeck = CardDeck()
     
     func recieveCard(newCard: Card) {
         cards.append(newCard)
