7b42d03c855b1ee2835253a5c101a2035c1fed3a
Anna <bluewhale0528@naver.com>
2020-02-14 22:49:31 +0900

    일부 프로퍼티 접근권한 변경

--- CardGameApp/CardGameAppTests/PokerGameTests.swift
+++ CardGameApp/CardGameAppTests/PokerGameTests.swift
@@ -3,32 +3,34 @@ import XCTest
 class PokerGameTests: XCTestCase {
     
     var pokerGame: PokerGame!
+    var participants: [Participant] = []
     
     override func setUp() {
         super.setUp()
         self.pokerGame = PokerGame(gameMode: .sevenCardStud, playerMode: .four)
+        self.participants = pokerGame.showParticipants()
     }
 
     func testPokerGameInitialize() {
         let participantCount = Mode.PlayerMode.four.rawValue
         
-        XCTAssertEqual(self.pokerGame.participants.count, participantCount)
+        XCTAssertEqual(self.participants.count, participantCount)
     }
 
     func testGiveCards() {
         pokerGame.giveCards()
-        let randomIndex = Int.random(in: 0..<pokerGame.participants.count)
-        var anotherRandomIndex = Int.random(in: 0..<pokerGame.participants.count)
+        let randomIndex = Int.random(in: 0..<self.participants.count)
+        var anotherRandomIndex = Int.random(in: 0..<self.participants.count)
         while anotherRandomIndex == randomIndex {
-            anotherRandomIndex = Int.random(in: 0..<pokerGame.participants.count)
+            anotherRandomIndex = Int.random(in: 0..<self.participants.count)
         }
         
-        let participantCards = pokerGame.participants[randomIndex].cards
-        let anotherParticipantCards = pokerGame.participants[anotherRandomIndex].cards
+        let participantCards = self.participants[randomIndex].cards
+        let anotherParticipantCards = self.participants[anotherRandomIndex].cards
         
         XCTAssertNotEqual(participantCards, anotherParticipantCards)
         
-        let dealerCards = pokerGame.dealer.cards
+        let dealerCards = pokerGame.showDealer().cards
         
         XCTAssertNotEqual(participantCards, dealerCards)
     }
