4c2b75ce3b2199db032e86372ada5738c86ce323
kjoonk <kjoonk@gmail.com>
2020-02-14 16:34:01 +0900

    Dealer.startGame 기능을 Game으로 옮김
    - Dealer에게 넘겼던 기능을 Game이 하도록 함
    - Dealer는 룰에 상관없이 한장씩 카드를 돌리도록 변경
    - DealerTests에 중복되어 있던 Game 관련 테스트 제거

--- CardGameApp/CardGameAppTests/DealerTests.swift
+++ CardGameApp/CardGameAppTests/DealerTests.swift
@@ -8,31 +8,12 @@ class DealerTests: XCTestCase {
         self.deck = CardGameFactory.createDeck()
         self.dealer = Dealer(deck: deck)
     }
-    
-    func testStartGameOfSevenStud() {
-        let players = try! CardGameFactory.createPlayers(count: 4)
-        let numberOfCards = Game.Rule.sevenStud.rawValue
+
+    func testDealCardsOfOneRound() {
+        let dealer = CardGameFactory.createDealer()
+        let players = [Player(), Player()]
+        dealer.dealCardsOfOneRound(players)
         
-        dealer.startGame(rule: .sevenStud, players: players)
         
-        XCTAssertTrue(dealer.isReadyToGame(requiredCards: numberOfCards))
-        
-        let receivedCorrectly = players.filter { player in
-            player.isReadyToGame(requiredCards: numberOfCards)
-        }
-        
-        XCTAssertEqual(receivedCorrectly.count, players.count)
-    }
-    
-    func testStartGameOfFiveStud() {
-        let players = try! CardGameFactory.createPlayers(count: 2)
-        let numberOfCards = Game.Rule.fiveStud.rawValue
-        
-        dealer.startGame(rule: .fiveStud, players: players)
-        
-        let receivedCorrectly = players.filter { $0.isReadyToGame(requiredCards: numberOfCards)
-        }
-        XCTAssertEqual(receivedCorrectly.count, players.count)
-        XCTAssertTrue(dealer.isReadyToGame(requiredCards: numberOfCards))
     }
 }
