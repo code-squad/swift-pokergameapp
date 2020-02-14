caf2224b712ea00513e2b38067b47f2f76a12a85
Anna <bluewhale0528@naver.com>
2020-02-14 17:49:47 +0900

    PokerGame생성함과 동시에 참가자 배열에 참가자 수만큼 추

--- CardGameApp/CardGameApp/PokerGame.swift
+++ CardGameApp/CardGameApp/PokerGame.swift
@@ -3,6 +3,18 @@ import Foundation
 class PokerGame {
     var participants = [Participant]()
     var dealer = Dealer()
+    var cardsCount: Int
+    var participantCount: Int
+    
+    init(gameMode: Mode.GameMode, playerMode: Mode.PlayerMode) {
+        cardsCount = gameMode.rawValue
+        participantCount = playerMode.rawValue
+        
+        for count in 0..<participantCount {
+            participants.append(Participant())
+            participants[count].role += "#\(count+1)"
+        }
+    }
     
     func giveCards() {
         
