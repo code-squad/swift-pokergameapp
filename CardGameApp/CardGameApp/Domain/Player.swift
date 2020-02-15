d044183c50dc3df2e7c74ffcf730e18c3914d3f1
ehgud0670 <kimdo2297@gmail.com>
2020-02-15 17:54:46 +0900

    Feature : 게임 플레이어 객체 추가          * 프로퍼티로 옵셔널? 타입인 카드배열 객체 추가
             게임 플레이어가 class인 이유
    	    : 이후에 나타날 게임 판에서 딜러를 Player 타입으로 두고 let으로 선언해 constant로 두고 싶다.
                  따라서 이후 딜러의 속성들을 변경하려면 딜러의 타입이 struct가 아닌 class 이어야 하므로
                  Player타입을 class로 두었다.
    
             카드배열의 타입이 옵셔널?인 이유
                : 1. 플레이어의 카드들은 이후에 나타날 게임 판의 카드덱에 의존적이다.
                : 2. 이후 미션에서 앱을 shake()할때 플레어이들에게 카드들을 다시 나눠줘야 하고
                이때 플레이어의 카드 배열이 nil이 된 이후 다시 세팅될 가능성이 많으므로
                안전성을 위해 옵셔녈?로 두었다.

---
+++ CardGameApp/CardGameApp/Domain/Player.swift
@@ -0,0 +1,15 @@
+//
+//  Playerable.swift
+//  CardGameApp
+//
+//  Created by kimdo2297 on 2020/02/15.
+//  Copyright © 2020 Jason. All rights reserved.
+//
+
+import Foundation
+
+class Player {
+    private var cards : [Card]?
+}
+
+
