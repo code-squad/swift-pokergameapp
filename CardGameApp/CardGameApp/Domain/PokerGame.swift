50113021eaf5338b7a4da3d3e56f9fa0b2c38e33
ehgud0670 <kimdo2297@gmail.com>
2020-02-15 19:18:22 +0900

    Feature : * 게임 스터드 방식을 나타내는 enum 선언 	  * gameStut을 게임 클래스의 프로퍼티로 선언

--- CardGameApp/CardGameApp/Domain/PokerGame.swift
+++ CardGameApp/CardGameApp/Domain/PokerGame.swift
@@ -13,12 +13,17 @@ class PokerGame {
     private let deck = Deck()
     private let dealer = Player()
     private var players: [Player]!
+    enum GameStut : Int {
+        case five = 5, seven = 7
+    }
+    private let gameStut : GameStut
     
-    init?(playersNumber: Int) throws {
+    init?(gameStut : GameStut , playersNumber: Int) throws {
         guard playersNumber >= minPlayersNumber,
             playersNumber <= maxPlayersNumber else {
                 throw PokerGameError.invalidPlayersNumber
         }
+        self.gameStut = gameStut
         players = initPlayers(num: playersNumber)
     }
     
