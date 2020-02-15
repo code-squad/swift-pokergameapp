455ee9652a1ff95080ff9a6e6caad10976c08066
ehgud0670 <kimdo2297@gmail.com>
2020-02-15 18:40:14 +0900

    Feature : 포커게임 클래스 프로퍼티 선언 및 초기화 	  * 카드덱 프로퍼티 let으로 초기화 	  * 딜러 프로퍼티를 let, Player객체로 초기화 	  * 프로퍼티인 플레이어들을 Player 배열 옵셔널! 타입으로 선언 	  => 게임 진행할때 반드시 필요한 객체들이므로 !으로 선언 	  => 바로 접근가능하도록 !으로 선언
    	  * 포커게임을 클래스로 구현한 이유
    	   => 이후에 Controller로 올라갈건데 let으로 두고
    	  포커게임의 프로퍼티만 변경가능하도록 하고 싶기 때문.

---
+++ CardGameApp/CardGameApp/Domain/PokerGame.swift
@@ -0,0 +1,17 @@
+//
+//  PokerGame.swift
+//  CardGameApp
+//
+//  Created by kimdo2297 on 2020/02/15.
+//  Copyright © 2020 Jason. All rights reserved.
+//
+
+import Foundation
+
+class PokerGame {
+    
+    let deck = Deck()
+    let dealer = Player()
+    var players : [Player]!
+    
+}
