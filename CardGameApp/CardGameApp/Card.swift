e58a368375a2b29876b1f49c428a7efe318c87ef
idevjinie <idevjinie@gmail.com>
2020-02-15 17:17:42 +0900

    CustomStringConvertible 프로토콜 채택

--- CardGameApp/CardGameApp/Card.swift
+++ CardGameApp/CardGameApp/Card.swift
@@ -8,14 +8,23 @@
 
 import Foundation
 
-struct Card {
+struct Card: CustomStringConvertible {
     enum Suit: Character {
         case spades = "♠️", hearts = "♥️", diamonds = "♦️", clubs = "♣️"
     }
 
     enum Rank: Int {
-        case one = 1, two, three, four, five, six, seven, eight, nine, ten, eleven, twelve, thirteen
+        case A = 1, two, three, four, five, six, seven, eight, nine, ten, J, Q, K
     }
 
-    let suit: Suit?, rank: Rank?
+    let suit: Suit, rank: Rank
+    
+    var description: String {
+        switch rank {
+        case .A, .J, .Q, .K:
+            return "\(suit.rawValue)\(rank)"
+        default:
+            return "\(suit.rawValue)\(rank.rawValue)"
+        }
+    }
 }
