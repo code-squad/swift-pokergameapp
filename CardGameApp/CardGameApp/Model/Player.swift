f60b3e49f1289194b32ede4e149efde331ba5645
GangWoon <gangwoon2018@gmail.com>
2020-02-16 22:03:36 +0900

    Player 구조체 생성
    게이머를 배열로 같고 있을 필요가 없어서 player라는 구조체를 만들었습니다.

---
+++ CardGameApp/CardGameApp/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  CardGameApp
+//
+//  Created by Cloud on 2020/02/14.
+//  Copyright © 2020 Cloud. All rights reserved.
+//
+
+import Foundation
+
+class Player {
+    private var gamer: [Gamer] = []
+    
+}
