6e9e5e0bd74082e8beb51c7219c70dc280345fe7
limwin94 <amuguna94@gmail.com>
2020-02-17 18:53:27 +0900

    Players Class에서 참가자 참가 함수(participate) 작성

--- CardGameApp/CardGameApp/model/Players.swift
+++ CardGameApp/CardGameApp/model/Players.swift
@@ -11,11 +11,15 @@ import Foundation
 class Players {
     private var players = [Player]()
     
-    func participate(stud){
-        players.append(player)
-    }
-    
     // 여기서 배열탐색을 해야한다?
     // player들 세팅을 여기서 해줘야한다...
     // 그럼 참가자가 몇명인지는 어떻게 알수 있을까 게임타입?
+    
+    func participate(entryAmount: GameMode.Entry){
+        let count = Int(entryAmount.description)!
+        for _ in 1 ... count {
+            let player = Player()
+            players.append(player)
+        }
+    }
 }
