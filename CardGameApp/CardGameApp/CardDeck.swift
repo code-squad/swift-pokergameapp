52838690553939915cbabc03cc6349933204e724
idevjinie <idevjinie@gmail.com>
2020-02-17 18:04:25 +0900

    카드덱 구조체 구현

--- CardGameApp/CardGameApp/CardDeck.swift
+++ CardGameApp/CardGameApp/CardDeck.swift
@@ -7,3 +7,33 @@
 //
 
 import Foundation
+
+struct CardDeck {
+    private var cardDeck = [Card]()
+    
+    init() {
+        for suit in Card.Suit.allCases {
+            for rank in Card.Rank.allCases {
+                cardDeck.append(Card(suit: suit, rank: rank))
+            }
+        }
+    }
+    
+    func count() -> Int {
+        return cardDeck.count
+    }
+    
+    mutating func shuffle() {
+        cardDeck.shuffle()
+    }
+    
+    mutating func removeOne() -> Card {
+        let index = Int.random(in: 0..<cardDeck.count)
+        let card = cardDeck.remove(at: index)
+        return card
+    }
+    
+    mutating func reset() {
+        cardDeck = CardDeck().cardDeck
+    }
+}
