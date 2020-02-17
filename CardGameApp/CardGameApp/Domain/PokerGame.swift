bf3036662da0a95c6decfac510eb63ee7e5b45c6
ehgud0670 <kimdo2297@gmail.com>
2020-02-17 21:05:53 +0900

    Refactor : PokerGame init 코드 변경 및 nested enum 분리           * PlayersNum enum 생성           * GameStut enum과 PlayersNum enum 을 생성자의 파라미터값으로 변경함 	  * GameStur enum 의 isValid() 메서드 삭제
    	* 생성자의 파라미터 타입을 바꾼 이유
    	: Int값으로 매개변수를 받아서 최대값과 최솟값을 변경하는 것은
    	이후에 코드를 변경할 때마다 테스트 코드도 재작성해야 하므로 번거롭기 때문
    
    	* GameStut enum의 isValid()을 없앤 이유:
    	단순히 init() 하는 것이 더 깔끔하고 더 나은 방법이기 때문이다.
    	GameStut(.five) 나 GameStut(.seven) 이런 방식이 더 깔끔하고
    	개발자가 enum의 case들만 선택하면 되기 때문에 오류가 날 일이 없기 때문이다.

--- CardGameApp/CardGameApp/Domain/PokerGame.swift
+++ CardGameApp/CardGameApp/Domain/PokerGame.swift
@@ -8,53 +8,49 @@
 
 import Foundation
 
+enum GameStut: Int {
+    case five = 5, seven = 7
+    
+    func forEach(handler: () -> ()) {
+        for _ in 0 ..< self.rawValue {
+            handler()
+        }
+    }
+    
+    func forEach(handler: () throws  -> ()) throws {
+        for _ in 0 ..< self.rawValue {
+            try handler()
+        }
+    }
+}
+
+enum PlayersNum: Int {
+    case one = 1 , two, three, four
+    func forEach(handler : () -> ()) {
+        for _ in 0 ..< self.rawValue {
+            handler()
+        }
+    }
+}
+
 class PokerGame {
     
     private let deck = Deck()
     private let dealer = Player()
     private var players: [Player]!
     
-    private enum GameStut: Int {
-        case five = 5, seven = 7
-        
-        func isValid(num: Int) -> Bool {
-            return self.rawValue == num
-        }
-        
-        func forEach(handler: () -> ()) {
-            for _ in 0 ..< self.rawValue {
-                handler()
-            }
-        }
-        
-        func forEach(handler: () throws  -> ()) throws {
-            for _ in 0 ..< self.rawValue {
-                try handler()
-            }
-        }
-    }
-    private var gameStut: GameStut!
+    private var gameStut: GameStut
+    private var playersNum : PlayersNum
     
-    init?(gameStutNumber: Int , playersNumber: Int) throws {
-        guard GameStut.five.isValid(num: gameStutNumber) ||
-            GameStut.seven.isValid(num: gameStutNumber) else {
-                throw PokerGameError.invalidGameStutNumber
-        }
-        
-        let minPlayersNumber = 1
-        let maxPlayersNumber = 4
-        guard playersNumber >= minPlayersNumber,
-            playersNumber <= maxPlayersNumber else {
-                throw PokerGameError.invalidPlayersNumber
-        }
-        
-        self.gameStut = GameStut(rawValue: gameStutNumber)
-        players = initPlayers(num: playersNumber)
+    init(gameStut: GameStut , playerNum: PlayersNum){
+        self.gameStut = gameStut
+        self.playersNum = playerNum
+        players = initPlayers(num: self.playersNum)
     }
     
-    private func initPlayers(num: Int) -> [Player] {
+    private func initPlayers(num: PlayersNum) -> [Player] {
         var players = [Player]()
-        for _ in 0 ..< num {
+        num.forEach {
             players.append(Player())
         }
         return players
@@ -99,7 +95,7 @@ class PokerGame {
     
     private func sendToPlayers() throws {
         for index in 0 ..< players.count {
-            try players[index].receiveCard{
+            try players[index].receiveCard {
                 try deck.removeOne()
             }
         }
