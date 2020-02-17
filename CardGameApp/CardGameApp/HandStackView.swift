da58dec03fc73d719ab2ddcf9c477ada0bdc953e
TTOzzi <devttozzi@gmail.com>
2020-02-17 21:28:50 +0900

    HandStackView 분리

---
+++ CardGameApp/CardGameApp/HandStackView.swift
@@ -0,0 +1,27 @@
+//
+//  HandStackView.swift
+//  CardGameApp
+//
+//  Created by TTOzzi on 2020/02/17.
+//  Copyright © 2020 TTOzzi. All rights reserved.
+//
+
+import UIKit
+
+class HandStackView: UIStackView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        self.distribution = .fillEqually
+        self.spacing = -5
+    }
+    
+}
