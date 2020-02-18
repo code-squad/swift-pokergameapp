106a6e19a43cb107dceb8893a4043b3fe0e04812
dev-Lena <keunnalee@gmail.com>
2020-02-18 11:40:08 +0900

    NumbersOfPlayers enum과 GameMode enum을 PokerGame클래스 외부로 분리

--- CardGameApp/CardGameApp/PokerGame.swift
+++ CardGameApp/CardGameApp/PokerGame.swift
@@ -8,33 +8,35 @@
 
 import Foundation
 
-class PokerGame {
-    enum NumbersOfPlayers : Int {
-        case one = 1 , two, three, four
-        
-        // 플레이어의 자리가 몇 개가 필요한지 세팅
-        func setPlayerSeat(for bringPlayer: () -> ()) {
-            for _ in 1 ... self.rawValue {
-                bringPlayer()
-            }
+enum NumbersOfPlayers : Int {
+    case one = 1 , two, three, four
+    
+    // 플레이어의 자리가 몇 개가 필요한지 세팅
+    func setPlayerSeat(for bringPlayer: () -> ()) {
+        for _ in 1 ... self.rawValue {
+            bringPlayer()
         }
+    }
+}
+
+enum GameMode : Int {
+    case fiveCardStud = 5
+    case sevenCardStud = 7
+    
+    func compareStudNumber(with target : Int) -> Bool {
+        return self.rawValue == target
     }
     
-    enum GameMode : Int {
-        case fiveCardStud = 5
-        case sevenCardStud = 7
-        
-        func compareStudNumber(with target : Int) -> Bool {
-            return self.rawValue == target
-        }
-        
-        // 카드를 내려놓는 곳이 몇 개가 필요한지 세팅
-        func setCardPlacement(of giveCard: ()-> ()) {
-            for _ in 1 ... self.rawValue{
-                giveCard()
-            }
+    // 카드를 내려놓는 곳이 몇 개가 필요한지 세팅
+    func setCardPlacement(of giveCard: ()-> ()) {
+        for _ in 1 ... self.rawValue{
+            giveCard()
         }
     }
+}
+
+class PokerGame {
+    
     private let gameMode: GameMode
     private let dealer : Dealer
     private var players = Players()
