cbf54773f0b0b6e56c76482091c15410845c816b
1Consumption <hanseop95@gmail.com>
2020-02-18 16:04:07 +0900

    스트레이트, 포카드, 트리플 구분 알고리즘 추가

--- CardGameApp/CardGameApp/model/Hand.swift
+++ CardGameApp/CardGameApp/model/Hand.swift
@@ -9,12 +9,15 @@
 import Foundation
 
 class Hand{
-    enum resultPriority: Int{
-        case noPair = 0, onePair, twoPair, fourCard, straight
+    enum ResultPriority: Int{
+        case noPair = 0, onePair, twoPair, triple, fourCard, straight
+        
+        func equal(num: Int) -> Bool{
+            return self.rawValue == num
+        }
     }
     
     private var card: [Card] = [Card]()
-    private var result: resultPriority!
     
     func append(card: Card){
         self.card.append(card)
@@ -28,5 +31,49 @@ class Hand{
         card.forEach {
             transform($0)
         }
+    }
+    
+    func result() -> ResultPriority{
+        if checkStraight(card: card.sorted()){
+            return .straight
+        }
+        
+        if checkSameCard(howMany: .fourCard, card: card.sorted()){
+            return .fourCard
+        }
+        
+        if checkSameCard(howMany: .triple, card: card.sorted()){
+            return .triple
+        }
+        
+        return .noPair
+    }
+    
+    func checkStraight(card: [Card]) -> Bool{
+        var pre = card[0]
+        var straightCount = 1
+        for cardIndex in 1..<card.count{
+            if pre - card[cardIndex] == -1{
+                straightCount += 1
+            } else{
+                straightCount = 1
+            }
+            pre = card[cardIndex]
+        }
+        return ResultPriority.straight.equal(num: straightCount)
+    }
+    
+    func checkSameCard(howMany same: ResultPriority, card: [Card]) -> Bool{
+        var pre = card[0]
+        var sameCardCount = 1
+        for cardIndex in 1..<card.count{
+            if pre == card[cardIndex]{
+                sameCardCount += 1
+            } else{
+                sameCardCount = 1
+            }
+            pre = card[cardIndex]
+        }
+        return same.equal(num: sameCardCount)
     }
 }
