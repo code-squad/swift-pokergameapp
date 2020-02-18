84f285dbd2b03f1847bd85d265dcef700d369c3d
TTOzzi <devttozzi@gmail.com>
2020-02-18 16:43:31 +0900

    Ranks 클래스 생성

---
+++ CardGameApp/CardGameApp/Model/Ranks.swift
@@ -0,0 +1,49 @@
+//
+//  Ranks.swift
+//  CardGameApp
+//
+//  Created by TTOzzi on 2020/02/18.
+//  Copyright © 2020 TTOzzi. All rights reserved.
+//
+
+import Foundation
+
+class Ranks {
+    
+    private var sortedCards: [Card]
+    
+    init(cards: [Card]) {
+        let info = cards.reduce(into: [Card:Int]()) {
+            if $0[$1] == nil {
+                $0[$1] = 1
+            } else {
+                $0[$1]! += 1
+            }
+        }
+        let result = info.sorted { (lhs, rhs) -> Bool in
+            if lhs.value == rhs.value {
+                return lhs.key > rhs.key
+            } else {
+                return lhs.value > rhs.value
+            }
+        }
+        self.sortedCards = result.map { (value) -> Card in value.key }
+    }
+}
+
+extension Ranks: Equatable {
+    static func == (lhs: Ranks, rhs: Ranks) -> Bool {
+        lhs.sortedCards == rhs.sortedCards
+    }
+    
+    static func > (lhs: Ranks, rhs: Ranks) -> Bool {
+        for index in 0...lhs.sortedCards.count {
+            if lhs.sortedCards[index] == rhs.sortedCards[index] {
+                continue
+            } else {
+                return lhs.sortedCards[index] > rhs.sortedCards[index]
+            }
+        }
+        return false
+    }
+}
