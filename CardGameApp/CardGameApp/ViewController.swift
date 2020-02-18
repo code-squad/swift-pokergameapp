9141c520f8dd43147e389e8809aa06769083c058
Anna <bluewhale0528@naver.com>
2020-02-18 09:13:58 +0900

    PlayersStackView추가(vertical stackView)

--- CardGameApp/CardGameApp/ViewController.swift
+++ CardGameApp/CardGameApp/ViewController.swift
@@ -10,34 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let cardStackView: UIStackView = {
-        let stack = UIStackView()
-        stack.axis = .horizontal
+    let playersStackView: UIStackView = {
+       let stack = UIStackView()
+        stack.axis = .vertical
         stack.translatesAutoresizingMaskIntoConstraints = false
         stack.distribution = .fillEqually
-        stack.spacing = 10
-        stack.backgroundColor = .yellow
+        stack.spacing = 40
         return stack
     }()
 
+    var cardStackView: UIStackView!
+    func createCardsStackView() -> UIStackView {
+        cardStackView = UIStackView()
+        cardStackView.axis = .horizontal
+        cardStackView.translatesAutoresizingMaskIntoConstraints = false
+        cardStackView.distribution = .fillEqually
+        cardStackView.spacing = -10
+        cardStackView.backgroundColor = .yellow
+        return cardStackView
+    }
+        
+    var cardImage = UIImageView()
+
+    // MARK: ViewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor(patternImage: #imageLiteral(resourceName: "pattern"))
         
-        view.addSubview(cardStackView)
- 
-        addConstraintsToStack()
-        addCardsToStackView()
-        
-//        let pokerGame = PokerGame(gameMode: .sevenCardStud, playerMode: .three)
-//        pokerGame.giveCards()
-//        pokerGame.giveCards()
-//
-//        let pokerGame2 = PokerGame(gameMode: .fiveCardStud, playerMode: .three)
-//        pokerGame2.giveCards()
-//        pokerGame2.giveCards()
-        
-//        view.addSubview(matchImageToCardInfo(suit: .spades, rank: .ten))
+        view.addSubview(playersStackView)
+        for _ in 1...5 {
+            playersStackView.addArrangedSubview(createCardsStackView())
+            addConstraintsToStack()
+            addCardsToStackView()
+        }
     }
 
     override var preferredStatusBarStyle: UIStatusBarStyle {
@@ -45,13 +50,13 @@ class ViewController: UIViewController {
     }
     
     func addConstraintsToStack() {
-        cardStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10).isActive = true
-        cardStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10).isActive = true
-        cardStackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10).isActive = true
+        cardStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
+        cardStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40).isActive = true
+        playersStackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 180).isActive = true
     }
     
     func setCardBackImage() -> UIImageView {
-        let cardBackImage = UIImageView(image: #imageLiteral(resourceName: "card-back"))
+        let cardBackImage = matchImageToCardInfo(suit: .spades, rank: .four)
         cardBackImage.heightAnchor.constraint(equalTo: cardBackImage.widthAnchor, multiplier: 1.27).isActive = true
         return cardBackImage
     }
@@ -59,7 +64,6 @@ class ViewController: UIViewController {
     func addCardsToStackView() {
         for _ in 1...7 {
             let cardBackImage = setCardBackImage()
-            cardBackImage.contentMode = .scaleAspectFit
             cardStackView.addArrangedSubview(cardBackImage)
         }
     }
@@ -80,7 +84,11 @@ class ViewController: UIViewController {
         }
         
         rankString = rank.description
-    
-        return UIImageView(image: UIImage(named: "\(suitString)\(rankString).png")!)
+        
+        cardImage = UIImageView(image: UIImage(named: "\(suitString)\(rankString).png")!)
+        cardImage.contentMode = .scaleAspectFit
+        cardImage.layer.cornerRadius = 5
+        cardImage.clipsToBounds = true
+        return cardImage
     }
 }
