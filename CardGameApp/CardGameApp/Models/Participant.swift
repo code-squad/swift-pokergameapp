65c537dece7a80bf3ba50672f79d76e4ccbc0838
seizze <cwcwc3@gmail.com>
2020-02-20 20:15:09 +0900

    feat: Add comparable score type

--- CardGameApp/CardGameApp/Models/Participant.swift
+++ CardGameApp/CardGameApp/Models/Participant.swift
@@ -26,3 +26,31 @@ extension Participant: Equatable {
         return lhs.hand == rhs.hand
     }
 }
+
+enum Score {
+    case fourOfAKind(highestCard: Card)
+    case straight(highestCard: Card)
+    case threeOfAKind(highestCard: Card)
+    case twoPairs(highestCard: Card)
+    case pair(highestCard: Card)
+    case highcard(highestCard: Card)
+    case none
+    
+    var priority: Int {
+        switch self {
+        case .fourOfAKind: return 1
+        case .straight: return 2
+        case .threeOfAKind: return 3
+        case .twoPairs: return 4
+        case .pair: return 5
+        case .highcard: return 6
+        case .none: return 7
+        }
+    }
+}
+
+extension Score: Comparable {
+    static func < (lhs: Score, rhs: Score) -> Bool {
+        return lhs.priority > rhs.priority
+    }
+}
