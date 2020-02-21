d7a0ab2fcf41f43af712e53f13c3c4e1653c3742
delma <sdy2856@gmail.com>
2020-02-21 22:29:42 +0900

    modify: logic in judgeResult()

--- CardGameApp/CardGameApp/Model/Hands.swift
+++ CardGameApp/CardGameApp/Model/Hands.swift
@@ -40,23 +40,36 @@ class Hands {
         return hands.removeFirst()
     }
     
-    func countCardNumber(){
-        for card in hands {
-            if let value = cardCounts[card.number] {
-                cardCounts.updateValue(value + 1, forKey: card.number)
+    func countPairNumber() -> [Int] {
+        let cards = sortCards(hands)
+        var pairCount = 1
+        var pairs: [Int] = []
+        for index in 0..<cards.count {
+            if cards[index] == cards[index + 1] {
+                pairCount += 1
             } else {
-                cardCounts.updateValue(1, forKey: card.number)
+                pairCount = 1
+            }
+            
+            if pairCount != 1 {
+                pairs.append(pairCount)
             }
         }
+        return pairs
+    }
+    
+    func sortCards(_ hands: [Card] ) -> [Card] {
+        var cards = hands
+        cards.sort { (first, second) -> Bool in
+            first > second
+        }
+        return cards
     }
     
     func judgeStraight() -> Bool {
-        hands.sort { (first, second) -> Bool in
-             first > second
-        }
-        
-        for index in 0..<hands.count {
-            if hands[index] >> hands[index+1] {
+        let cards = sortCards(hands)
+        for index in 0..<cards.count {
+            if cards[index] >> cards[index+1] {
                 return false
             }
         }
@@ -64,43 +77,36 @@ class Hands {
         
     }
     
-    func judgeMuchCard() -> (cardsCount: Int, twoPairs: [Card.Number]) {
-        countCardNumber()
-        var bestValue = 0
-        var bestKey = Card.Number.one
-        var twoPairs: [Card.Number] = []
-        for (key, value) in cardCounts {
-            if value > bestValue {
-                bestValue = value
-                bestKey = key
-            }
-            if value == 2 {
-                twoPairs.append(key)
-            }
-        }
-        return (bestValue, twoPairs)
-    }
-    
     func judgeResult() -> GameResult? {
-        let cardCount = judgeMuchCard().cardsCount
-        let twoPairs = judgeMuchCard().twoPairs
-        let isStraight = judgeStraight()
+        var result = GameResult.none
+        let pairs = countPairNumber()
+        var twoPairCount = 0
         
-        guard cardCount > 2 && twoPairs.count == 0 else { return GameResult.twoPair }
-        guard !isStraight else { return GameResult.straight }
-        
-        switch cardCount {
-        case 0, 1:
-            return GameResult.none
-        case 2:
-            return GameResult.onePair
-        case 3:
-            return GameResult.tripple
-        case 4, 5:
-            return GameResult.fourCard
-        default:
-            return GameResult.none
+        if judgeStraight() {
+            return GameResult.straight
+        }else if pairs.count == 0 {
+            return result
+        }else {
+            for pair in pairs {
+                if pair == 2 {
+                    twoPairCount += 1
+                }
+            }
         }
+        
+        guard let maxValue = pairs.max() else { return result }
+        
+        if maxValue == 3 {
+            result = GameResult.tripple
+        }else if maxValue == 4 {
+            result = GameResult.fourCard
+        }else if maxValue == 2, twoPairCount == 1 {
+            result = GameResult.onePair
+        }else if maxValue == 2, twoPairCount >= 2 {
+            result = GameResult.twoPair
+        }
+
+        return result
     }
     
 }
