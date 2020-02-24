0afbca403b975847271edcb27f00a0ba268922db
GangWoon <gangwoon2018@gmail.com>
2020-02-24 21:23:00 +0900

    GameTableView 세그먼트 컨트롤러 추가
    2개의 세그먼트 컨트롤러를 추가하고,  스텍 뷰를 다른 뷰 객체에서 작업하도록 했습니다.

--- CardGameApp/CardGameApp/View/GameTableView.swift
+++ CardGameApp/CardGameApp/View/GameTableView.swift
@@ -11,20 +11,73 @@ import SnapKit
 
 final class GameTableView: UIView {
     
-    private var stackView: StackView = StackView()
+    private var gameStyleSegementedTitle: [String] = ["5 cards", "7 cards"]
+    private var numberofPlayerSegementedTitle: [String] = ["2 peoples", "3 peoples", "4 peoples"]
     private let viewBackground: String = "bg_pattern"
     
+    private var gameStyleSegementedControl: UISegmentedControl
+    private var numberofPlayerSegementedControl: UISegmentedControl
+    private var stackView: StackView = StackView()
+    
     override init(frame: CGRect) {
+        gameStyleSegementedControl = UISegmentedControl(items: gameStyleSegementedTitle)
+        numberofPlayerSegementedControl = UISegmentedControl(items: numberofPlayerSegementedTitle)
         super.init(frame: frame)
-        apply(background: viewBackground)
+        configure()
     }
     
     required init?(coder: NSCoder) {
+        gameStyleSegementedControl = UISegmentedControl(items: gameStyleSegementedTitle)
+        numberofPlayerSegementedControl = UISegmentedControl(items: numberofPlayerSegementedTitle)
         super.init(coder: coder)
+        configure()
+    }
+    
+    private func configure() {
         apply(background: viewBackground)
+        configureGameStyleSegementControl()
+        configureNumberofPlayers()
+        configureStackView()
     }
     
     private func apply(background: String) {
         backgroundColor = UIColor(patternImage: UIImage(named: background) ?? UIImage())
+    }
+    
+    private func configureGameStyleSegementControl() {
+        addSubview(gameStyleSegementedControl)
+        gameStyleSegementedControl.selectedSegmentIndex = 0
+        gameStyleSegementedControl.addTarget(self, action: #selector(some), for: .valueChanged)
+        gameStyleSegementedControl.snp.makeConstraints { (make) in
+            make.top.equalTo(safeAreaLayoutGuide).offset(16)
+            make.leading.equalTo(safeAreaLayoutGuide).offset(16)
+            make.centerX.equalTo(self)
+        }
+    }
+    
+    private func configureNumberofPlayers() {
+        addSubview(numberofPlayerSegementedControl)
+        numberofPlayerSegementedControl.selectedSegmentIndex = 0
+        numberofPlayerSegementedControl.addTarget(self, action: #selector(something), for: .valueChanged)
+        numberofPlayerSegementedControl.snp.makeConstraints { (make) in
+            make.top.equalTo(gameStyleSegementedControl.snp.bottom).offset(16)
+            make.leading.equalTo(gameStyleSegementedControl.snp.leading)
+            make.centerX.equalTo(self)
+        }
+    }
+    
+    private func configureStackView() {
+        addSubview(stackView)
+        stackView.snp.makeConstraints { (make) in
+            make.leading.equalTo(numberofPlayerSegementedControl.snp.leading)
+            make.trailing.equalTo(numberofPlayerSegementedControl.snp.trailing)
+            make.top.equalTo(numberofPlayerSegementedControl.snp.bottom).offset(16)
+        }
+    }
+    
+    @objc private func some() {
+    }
+    
+    @objc private func something() {
     }
 }
