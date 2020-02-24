5d9cf9dab4ec85793156dcfcc96d6f96d0725ec9
GangWoon <gangwoon2018@gmail.com>
2020-02-24 21:24:32 +0900

    Data manager 생성
    모델에 해당하는 객체를 생성했습니다.

---
+++ CardGameApp/CardGameApp/Model/DataManager.swift
@@ -0,0 +1,13 @@
+//
+//  DataManager.swift
+//  CardGameApp
+//
+//  Created by Cloud on 2020/02/24.
+//  Copyright © 2020 Cloud. All rights reserved.
+//
+
+import Foundation
+
+class DataManager: NSObject {
+    var game = Game.fiveCardStud(gamers: Players())
+}
