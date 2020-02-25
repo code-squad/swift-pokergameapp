8bd86ceac4f17bf14da249faffdc67b9988715cb
ehgud0670 <kimdo2297@gmail.com>
2020-02-25 17:26:26 +0900

    Feature : init(coder:)의 경우도 객체가 정상적으로 만들어지도록 기능 구현 	이유: init(coder:)는 storyboard에 있는 뷰를 쓰는 경우에 호출되는 init()인데 	이후에 storyboard로 만들수도 있는 있는 가능성 때문에 	이 경우에도 custom view가 정상적으로 초기화 및 설정되도록 	하기 위해 기능 구현함

--- CardGameApp/CardGameApp/View/CardsStackView.swift
+++ CardGameApp/CardGameApp/View/CardsStackView.swift
@@ -16,7 +16,8 @@ class CardsStackView: UIStackView {
     }
     
     required init(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        super.init(coder: coder)
+        configure()
     }
     
     private func configure() {
--- CardGameApp/CardGameApp/View/ParticipantStackView.swift
+++ CardGameApp/CardGameApp/View/ParticipantStackView.swift
@@ -22,7 +22,8 @@ class ParticipantStackView: UIStackView {
     }
     
     required init(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        super.init(coder: coder)
+        configure()
     }
     
     private func configure() {
