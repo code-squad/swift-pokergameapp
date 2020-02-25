5559083bc288b10e623b939045d6138f81164ae1
limwin94 <amuguna94@gmail.com>
2020-02-25 23:13:43 +0900

    Players 객체 작성 배열관리를 따로 하도록 변경함.

---
+++ CardGameApp/CardGameApp/model/Players.swift
@@ -0,0 +1,31 @@
+//
+//  File.swift
+//  CardGameApp
+//
+//  Created by 임승혁 on 2020/02/25.
+//  Copyright © 2020 임승혁. All rights reserved.
+//
+
+import Foundation
+
+class Players {
+    private(set) var players = [Player]()
+    
+    func joinPlayerToGame(playerEntry: PlayerEntry, dealer: Dealer) {
+        var index = 1
+        playerEntry.each {
+            players.append(Player(name: "Player\(index)"))
+            index += 1
+        }
+        players.append(dealer)
+    }
+    
+    func initDrawCard(studType: StudType, dealer: Dealer) {
+        players.forEach { (player) in
+            studType.each {
+                let card = dealer.drawCard()
+                player.bringCard(card: card)
+            }
+        }
+    }
+}
