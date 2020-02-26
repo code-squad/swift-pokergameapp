227e72e04ece135432530df9906f8beb7bd84749
delma <sdy2856@gmail.com>
2020-02-26 18:26:34 +0900

    modify: unused code in Class Hands

--- CardGameApp/CardGameApp/Model/Hands.swift
+++ CardGameApp/CardGameApp/Model/Hands.swift
@@ -8,37 +8,11 @@
 
 import Foundation
 class Hands: Equatable {
+    private var hands = [Card]()
+    
     static func == (lhs: Hands, rhs: Hands) -> Bool {
         return lhs.hands == rhs.hands
     }
-    
-    private var hands = [Card]()
-    
-//    enum GameResult: Int, Comparable, Equatable {
-//        case none = 0
-//        case onePair
-//        case twoPair
-//        case tripple
-//        case fourCard
-//        case straight
-//
-//        static func < (lhs: Hands.GameResult, rhs: Hands.GameResult) -> Bool {
-//            return lhs.rawValue < rhs.rawValue
-//        }
-//
-//        static func > (lhs: Hands.GameResult, rhs: Hands.GameResult) -> Bool {
-//            return lhs.rawValue > rhs.rawValue
-//        }
-//
-//        static func >= (lhs: Hands.GameResult, rhs: Hands.GameResult) -> Bool {
-//            return lhs.rawValue >= rhs.rawValue
-//        }
-//
-//        static func == (lhs: Hands.GameResult, rhs: Hands.GameResult) -> Bool {
-//            return lhs.rawValue == rhs.rawValue
-//        }
-//
-//    }
     
     func append(_ card: Card) {
         hands.append(card)
