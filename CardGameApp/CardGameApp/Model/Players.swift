30aaad3c4b9e881879f48b1daa61104aa5b0f19d
GangWoon <gangwoon2018@gmail.com>
2020-02-26 15:04:59 +0900

    컨밴션 정리

--- CardGameApp/CardGameApp/Model/Players.swift
+++ CardGameApp/CardGameApp/Model/Players.swift
@@ -16,7 +16,7 @@ struct Players {
         players.forEach { handler($0) }
     }
     
-    mutating func replacingPlayers(peoples: Peoples, handler: (Player) -> Void) {
+    mutating func replacingPlayers(peoples: Game.Peoples, handler: (Player) -> Void) {
         players = []
         peoples.forEach { players.append(Player(identifier: "players\($0)")) }
         players.forEach { handler($0) }
