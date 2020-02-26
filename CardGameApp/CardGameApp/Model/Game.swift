28620d8b1025b01d44f1942f413741fee53cc43c
GangWoon <gangwoon2018@gmail.com>
2020-02-26 15:08:25 +0900

    게임 클래스 수정
    명확한 의미를 전달하기 위해서 게임 클래스안에 열거형을 추가했습니다.
    의미가 애매했던 메소드 이름을 수정했습니다.

--- CardGameApp/CardGameApp/Model/Game.swift
+++ CardGameApp/CardGameApp/Model/Game.swift
@@ -10,6 +10,35 @@ import Foundation
 
 @objcMembers class Game: NSObject {
     
+    enum Style: Int {
+        case five = 5
+        case seven = 7
+        
+        func forEach(handler: () -> Void) {
+            (1...self.rawValue).forEach { _ in
+                handler()
+            }
+        }
+        
+        func isEqual(_ compare: Int) -> Bool {
+            (self.rawValue - compare) > 0 ? true : false
+        }
+    }
+
+    enum Peoples: Int, CaseIterable {
+        case two = 2
+        case three = 3
+        case four = 4
+        
+        init(index: Int) {
+            self = Peoples.allCases[safe: index] ?? .two
+        }
+        
+        func forEach(handler: (Int) -> Void) {
+            (1...self.rawValue).forEach { handler($0) }
+        }
+    }
+    
     private var players: Players
     private var dealer: Dealer
     private var style: Style
@@ -34,33 +63,33 @@ import Foundation
     func translateStyle(style: Style) {
         dealer.newGame()
         self.style = style
-        players.forEach { style.isEqual($0.count) ? popCard(player: $0) : pushCard(player: $0) }
+        players.forEach { style.isEqual($0.count) ? allowCards(player: $0) : discardCards(player: $0) }
     }
     
     func pushPlayerToView(handler: (Player) -> Void) {
         players.forEach { handler($0) }
     }
     
-    private func pushCard(player: Player) {
+    private func discardCards(player: Player) {
         (1...2).forEach { _ in
             player.removeLast()
         }
     }
     
-    private func popCard(player: Player) {
+    private func allowCards(player: Player) {
         (1...2).forEach { _ in
             player.addCard(card: dealer.pushCard())
         }
     }
     
     private func distributePlayers() {
-        players.replacingPlayers(peoples: peoples) { distribute(player: $0) }
+        players.replacingPlayers(peoples: peoples) { distribute(to: $0) }
         dealer.clearCards()
-        distribute(player: dealer)
+        distribute(to: dealer)
         players.addGamer(player: dealer)
     }
     
-    private func distribute(player: Player) {
+    private func distribute(to player: Player) {
         style.forEach {
             player.addCard(card: dealer.pushCard())
         }
