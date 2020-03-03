3ebc24dd96f6352349ba3823792f8cca46b99c13
GangWoon <gangwoon2018@gmail.com>
2020-03-03 22:56:43 +0900

    Hands 구조체 수정
    처음 생각할 때 hands에 정보를 갖고 있으면 처리하기 편해서 그렇게 했었는데 구조에 대해서 조금 더 생각해보려고 노력하겠습니다. topValue는 TexasHoldemRule이 같을때 처리하기 위해서 만들었습니다. 여기서 고민했던 점은 topValue가 hands에서 갖고 있는게 아닌 player가 hands.sorted { $0 > $1 }.first?.value로 사용하고 싶었는데 sorted() 메소드가 어떻게 구현할 지 몰라서 포기했습니다. 혹시 ios framework의 코드를 볼 수 있는 방법이 있을까요??  jump to definition에 가도 자세히 안나와 있어서 궁금합니다!!

--- CardGameApp/CardGameApp/Model/Hands.swift
+++ CardGameApp/CardGameApp/Model/Hands.swift
@@ -10,57 +10,16 @@ import Foundation
 
 struct Hands {
     
-    enum TexasHoldemRule: Int, CaseIterable {
-        case none
-        case pair
-        case triple
-        case fourCard
-        case twoPair
-        case straight
-        
-        init(index: Int) {
-            switch index {
-            case 2:
-                self = .pair
-            case 3:
-                self = .triple
-            case 4:
-                self = .fourCard
-            default:
-                self = .none
-            }
-        }
-        
-        func compare(with value: Int) -> Int {
-            value < self.rawValue ? self.rawValue : value
-        }
-        
-        func findPlayer(with value: Int) -> Bool {
-            value == self.rawValue
-        }
-    }
-    
     private var cards: [Card] = []
+    var topValue: Int {
+        cards.sorted { $0 > $1 }.first?.value ?? 0
+    }
     var count: Int {
         cards.count
-    }
-    var grade: TexasHoldemRule {
-        configureGrade()
-    }
-    var topValue: Int {
-        configureDictionary().keys.sorted { $0 > $1 }.first ?? 0
     }
     
     func forEach(handler: (Card) -> Void) {
         cards.forEach { handler($0) }
-    }
-    
-    func compare(with counting: Int) -> Int {
-        grade.compare(with: counting)
-    }
-    
-    func findPlayer(with value: Int) -> Bool {
-        grade.findPlayer(with: value)
     }
     
     mutating func add(_ card: Card?) {
@@ -74,39 +33,5 @@ struct Hands {
     
     mutating func clearCards() {
         cards = []
-    }
-    
-    private func configureGrade() -> TexasHoldemRule {
-        let dictionary = configureDictionary()
-        var grade: TexasHoldemRule = .none
-        grade = applyRules(dictionary: dictionary)
-        findTwoPair(grade: &grade, dictionary: dictionary)
-        findStraight(grade: &grade, dictionary: dictionary)
-        return grade
-    }
-    
-    private func configureDictionary() -> [Int: Int]{
-        var dictionary: [Int: Int] = [: ]
-        cards.forEach { $0.configureDictionary(dictionary: &dictionary) }
-        return dictionary
-    }
-    
-    private func applyRules(dictionary: [Int: Int]) -> TexasHoldemRule {
-        TexasHoldemRule(index: dictionary.values.sorted { $0 > $1 }.first ?? 0)
-    }
-    
-    private func findTwoPair(grade: inout TexasHoldemRule, dictionary: [Int: Int]) {
-        var count = 0
-        let twoPair = 2
-        dictionary.values.forEach { $0 == twoPair ? count += 1 : nil }
-        grade == .pair && count > 1 ? grade = .twoPair : nil
-    }
-    
-    private func findStraight(grade: inout TexasHoldemRule ,dictionary: [Int: Int]) {
-        dictionary.forEach { key, _ in
-            if dictionary[key - 2] != nil && dictionary[key - 1] != nil && dictionary[key + 1] != nil && dictionary[key + 2] != nil {
-                grade = .straight
-            }
-        }
     }
 }
