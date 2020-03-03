af62512c9b13daaace46f5d2fe5af6da92404d78
GangWoon <gangwoon2018@gmail.com>
2020-03-03 23:00:17 +0900

    Player 클래스 수정
    TexasHoldemRule을 player로 옮기고 그에 계산하는 방법을 추가했습니다.
    comparable 프로토콜을 채택해서 player들끼리 TexasHoldemRule를 바로 비교할 수 있게 만들었습니다.
    inout을 써서 처리했던 부분을 return하는 형태로 바꿔서 처리했습니다. winner 속성을 private로 바꿔서 처리했습니다.

--- CardGameApp/CardGameApp/Model/Player.swift
+++ CardGameApp/CardGameApp/Model/Player.swift
@@ -10,20 +10,32 @@ import Foundation
 
 class Player {
     
+    enum TexasHoldemRule: Int, CaseIterable {
+        case none = 1
+        case pair
+        case triple
+        case fourCard
+        case twoPair
+        case straight
+    }
+    
     private var cards: Hands = Hands()
     private var identifier: String
-    var winner: Bool = false
+    private var winner: Bool = false
+    var topValue: Int {
+        return cards.topValue
+    }
     var description: String {
         return identifier
-    }
-    var topValue: Int {
-        cards.topValue
     }
     var count: Int {
         return cards.count
     }
-    var grade: Hands.TexasHoldemRule {
-        cards.grade
+    var grade: TexasHoldemRule {
+        calculateScore()
+    }
+    var winnerImageName: String? {
+        winner ? "winner" : nil
     }
     
     init(identifier: String) {
@@ -46,15 +58,65 @@ class Player {
         cards.forEach { handler($0) }
     }
     
-    func checkWinner() {
+    func toggleWinner() {
         winner.toggle()
     }
     
-    func findPlayer(with value: Int) -> Bool {
-        cards.findPlayer(with: value)
+    func resetWinnerState() {
+        winner = false
     }
     
-    func compare(with value: Int) -> Int {
-        cards.compare(with: value)
+    private func classificateSameRankCards() -> [Card.Rank: Int] {
+        var dictionary: [Card.Rank: Int] = [: ]
+        cards.forEach { card in
+            let key: Card.Rank = Card.Rank(rawValue: card.value) ?? .one
+            dictionary.updateValue(dictionary[key] == nil ? 1 : (dictionary[key] ?? 0) + 1 , forKey: key)
+        }
+        return dictionary
+    }
+    
+    private func calculateScore() -> TexasHoldemRule {
+        let dictionary = classificateSameRankCards()
+        var grade = TexasHoldemRule(rawValue: dictionary.values.sorted { $0 > $1 }.first ?? 0) ?? .none
+        grade = findTwoPair(dictionary: dictionary, grade: grade)
+        grade = findStraight(dictionary: dictionary, grade: grade)
+        return grade
+    }
+    
+    private func findTwoPair(dictionary: [Card.Rank: Int], grade: TexasHoldemRule) -> TexasHoldemRule {
+        var count = 0
+        let twoPair = 2
+        dictionary.values.forEach { $0 == twoPair ? count += 1 : nil }
+        return grade == .pair && count > 1 ? .twoPair : grade
+    }
+    
+    private func findStraight(dictionary: [Card.Rank: Int], grade: TexasHoldemRule) -> TexasHoldemRule {
+        var grade = grade
+        dictionary.keys.forEach {
+            if dictionary[$0.neighborRank(index: -2)] != nil && dictionary[$0.neighborRank(index: -1)] != nil && dictionary[$0.neighborRank(index: 1)] != nil && dictionary[$0.neighborRank(index: 2)] != nil {
+                grade = .straight
+            }
+        }
+        return grade
+    }
+}
+
+extension Player.TexasHoldemRule: Comparable {
+    static func < (lhs: Player.TexasHoldemRule, rhs: Player.TexasHoldemRule) -> Bool {
+        lhs.rawValue < rhs.rawValue
+    }
+    
+    static func == (lhs: Player.TexasHoldemRule, rhs: Player.TexasHoldemRule) -> Bool {
+        lhs.rawValue == rhs.rawValue
+    }
+}
+
+extension Player: Comparable {
+    static func < (lhs: Player, rhs: Player) -> Bool {
+        lhs.grade < rhs.grade
+    }
+
+    static func == (lhs: Player, rhs: Player) -> Bool {
+        lhs.grade == rhs.grade
     }
 }
