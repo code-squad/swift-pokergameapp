6903b08a95f2ba73c4becfd98bb266fab02a1f7d
dev-Lena <keunnalee@gmail.com>
2020-03-08 19:34:41 +0900

    NumbersOfPlayersSegmentControl 클래스 생성

--- CardGameApp/CardGameApp/View/SegmentedControl.swift
+++ CardGameApp/CardGameApp/View/SegmentedControl.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-protocol SegmentedControlProtocol: class { // 추가
+protocol SegmentedControlProtocol: class {
     func segmentControlDidChange()
 }
 
@@ -23,7 +23,7 @@ class gameModeSegmentControl : UISegmentedControl{
     override init(frame: CGRect) {
         super.init(frame: frame)
         gameModeSegmentControl = UISegmentedControl(items: gameModes)
-        self.addTarget(self, action: // 추가
+        self.addTarget(self, action:
             #selector(valueDidChange),
                        for: .valueChanged)
     }
@@ -42,7 +42,40 @@ class gameModeSegmentControl : UISegmentedControl{
         self.tintColor = UIColor.white
     }
     
-    @objc func valueDidChange(){// 추가
+    @objc func valueDidChange(){
+        delegate?.segmentControlDidChange()
+    }
+}
+
+class NumbersOfPlayersSegmentControl: UISegmentedControl {
+    private var numbersOfPlayers = NumbersOfPlayers.four
+    let numbersOfPlayersList: [String] = ["2명", "3명", "4명"]
+    var numbersOfPlayersSegmentControl = UISegmentedControl()
+    
+    weak var delegate : SegmentedControlProtocol?
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        numbersOfPlayersSegmentControl = UISegmentedControl(items: numbersOfPlayersList)
+        numbersOfPlayersSegmentControl.addTarget(self, action: #selector(valueDidChange),
+            for: .valueChanged)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    convenience init() {
+        self.init(frame: CGRect(x: 0, y: 0, width: 0, height: 0))
+    }
+    
+    func setSegmetedControl(in view: UIView, offset: CGFloat){
+        self.center = CGPoint(x: view.frame.width/2, y: view.frame.height/offset)
+        self.backgroundColor = UIColor.gray
+        self.tintColor = UIColor.white
+    }
+    
+    @objc func valueDidChange(){
         delegate?.segmentControlDidChange()
     }
 }
