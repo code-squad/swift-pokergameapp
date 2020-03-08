6f12ca77e14e19e2db59e7b34caf014c9cccdbef
dev-Lena <keunnalee@gmail.com>
2020-03-08 19:28:16 +0900

    GameStackView에 setConstraintOfView(related:, related:) 추가

--- CardGameApp/CardGameApp/View/StackView.swift
+++ CardGameApp/CardGameApp/View/StackView.swift
@@ -26,4 +26,30 @@ class GameStackView: UIStackView {
         fatalError("init(coder:) has not been implemented")
     }
     
+    func setConstraintOfView(related superView : UIView, related numbersOfPlayersSegmentControl: UISegmentedControl ) {
+        self.topAnchor.constraint(equalTo: numbersOfPlayersSegmentControl.bottomAnchor, constant: 10).isActive = true
+        self.leadingAnchor.constraint(equalTo: superView.leadingAnchor, constant: 30).isActive = true
+        self.trailingAnchor.constraint(equalTo: superView.trailingAnchor, constant: -30).isActive = true
+    }
 }
+
+class CardsStackView: UIStackView {
+    
+    override init(frame: CGRect) { // by code
+        super.init(frame: frame)
+        self.translatesAutoresizingMaskIntoConstraints = false
+        self.axis = .horizontal
+        self.distribution = .fillEqually
+        self.spacing = -15
+    }
+    
+    convenience init() {
+        self.init(frame: CGRect(x: 0, y: 0, width: 0, height: 0))
+    }
+    
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
+
