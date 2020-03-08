d524e11bb6cc8640b088134b52c391bcc549cf44
dev-Lena <keunnalee@gmail.com>
2020-03-08 19:32:45 +0900

    SegmentedControlProtocol 생성

---
+++ CardGameApp/CardGameApp/View/SegmentedControl.swift
@@ -0,0 +1,25 @@
+//
+//  SegmentedControl.swift
+//  CardGameApp
+//
+//  Created by Keunna Lee on 2020/03/08.
+//  Copyright © 2020 Keunna Lee. All rights reserved.
+//
+
+import UIKit
+
+protocol SegmentedControlProtocol: class { // 추가
+    func segmentControlDidChange()
+}
+
+class SegmentedControl: UILabel {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
