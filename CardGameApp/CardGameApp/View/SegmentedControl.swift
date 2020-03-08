b94c660185681aa97a70ee2630c5db91b40d0b44
dev-Lena <keunnalee@gmail.com>
2020-03-08 19:33:30 +0900

    gameModeSegmentControl 클래스 생성

--- CardGameApp/CardGameApp/View/SegmentedControl.swift
+++ CardGameApp/CardGameApp/View/SegmentedControl.swift
@@ -12,14 +12,37 @@ protocol SegmentedControlProtocol: class { // 추가
     func segmentControlDidChange()
 }
 
-class SegmentedControl: UILabel {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+class gameModeSegmentControl : UISegmentedControl{
+    
+    private var gameMode = GameMode.fiveCardStud
+    let gameModes: [String] = ["5 Cards","7 Cards"]
+    var gameModeSegmentControl = UISegmentedControl()
+    
+    weak var delegate : SegmentedControlProtocol?
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        gameModeSegmentControl = UISegmentedControl(items: gameModes)
+        self.addTarget(self, action: // 추가
+            #selector(valueDidChange),
+                       for: .valueChanged)
     }
-    */
-
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    convenience init() {
+        self.init(frame: CGRect(x: 0, y: 0, width: 0, height: 0))
+    }
+    
+    func setSegmetedControl(in view: UIView, offset: CGFloat){
+        self.center = CGPoint(x: view.frame.width/2, y: view.frame.height/offset)
+        self.backgroundColor = UIColor.gray
+        self.tintColor = UIColor.white
+    }
+    
+    @objc func valueDidChange(){// 추가
+        delegate?.segmentControlDidChange()
+    }
 }
