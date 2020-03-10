eaadb7bc6f8a322917c39d2993b992b97dcf9f04
dev-Lena <keunnalee@gmail.com>
2020-03-10 23:59:55 +0900

    PokerGame 클래스 수정
    WinnerDiscriminator클래스에서 승자를 찾던 부분을 PokerGame으로 옮겼습니다.

--- CardGameApp/CardGameApp/Model/PokerGame.swift
+++ CardGameApp/CardGameApp/Model/PokerGame.swift
@@ -39,11 +39,16 @@ class PokerGame {
     private let dealer = Dealer()
     private let numbersOfPlayers : NumbersOfPlayers
     private var allParticipants : Participants
+    private var scoreBoard = [String:Int]()
     
     init(numbersOfPlayers: NumbersOfPlayers, gameMode: GameMode) {
         self.gameMode = gameMode
         self.numbersOfPlayers = numbersOfPlayers
         self.allParticipants = Participants(with: numbersOfPlayers, and: dealer)
+        // 참가자 수만큼 ScoreBoard key에 참가자 이름을 넣고 기본 점수 0을 value로 설정
+        allParticipants.forEachParticipant(behavior: ){participant in
+            scoreBoard[participant.describeSelf()] = 0
+        }
     }
     
     func start() {
@@ -64,8 +69,58 @@ class PokerGame {
     }
     
     func forEachParticipant(behavior: (PokerPlayer) -> ()) {
-        allParticipants.showParticipantsCards(behavior: ) { (participant) in
+        allParticipants.forEachParticipant(behavior: ) { (participant) in
             behavior(participant)
+        }
+    }
+    
+    func findWinner() -> String{
+        allParticipants.findCombination()
+        updateEachScore()
+        breakTie()
+        let winner = scoreBoard.max(by:){ participant, anotherParticipant in participant.value < anotherParticipant.value}?.key
+        guard let theWinner = winner else { return ""}
+        return theWinner
+    }
+    
+    func updateEachScore(){
+        allParticipants.forEachParticipant(behavior: ){ participant in
+            participant.showEachMyCombination(behavior: ){ combination in
+                let name = participant.describeSelf()
+                let score = combination.tellScore()
+                addScore(to: name, extraScore: score)
+            }
+        }
+    }
+    
+    func addScore(to participant: String,extraScore: Int){
+        guard let originalScore = scoreBoard[participant] else { return }
+          scoreBoard[participant] = originalScore + extraScore
+    }
+    
+    func breakTie(){
+            guard let highestScore = scoreBoard.max(by: { participant, anotherParticipant in participant.value < anotherParticipant.value}) else { return }
+            
+            let tiebreakers = scoreBoard.filter{score in score == highestScore}
+        
+            allParticipants.forEachParticipant(behavior: ){ participant in
+                
+                giveExtraScoreForHigherRank(to: participant)
+            }
+    }
+    
+    func giveExtraScoreForHigherRank(to participant: PokerPlayer){
+        var comparingRank = 0
+        participant.showHighestRank(behavior: ){
+            cardRank in
+            
+            if comparingRank <= cardRank {
+                comparingRank = cardRank
+            }else{
+                // 더 높은 사람에게 추가점수
+                let name = participant.describeSelf()
+                addScore(to: name, extraScore: 1)
+            }
         }
     }
 }
