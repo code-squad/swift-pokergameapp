6c2e2d2407a055735ee5e740c311016626a546dd
BMO <b38238496@gmail.com>
2021-02-15 18:04:07 +0900

    카드 7장 View에 출력
    이미지뷰를 생성해 각 카드마다 위치를 조정해주고 addSubview로 넣어줬습니다.

--- PokerGameApp/PokerGameApp/ViewController.swift
+++ PokerGameApp/PokerGameApp/ViewController.swift
@@ -16,9 +16,25 @@ class ViewController: UIViewController {
         if let bgImg = UIImage(named: "bg_pattern") {
             self.view.backgroundColor = UIColor(patternImage: bgImg)
         }
-        
+        show7CardBack()
     }
 
+    func show7CardBack() {
+        let cardWidth = self.view.frame.width / 8
+        let cardHeight = cardWidth * 1.27
+        let betweenCard = cardWidth / 8
+        let image = UIImage(named: "card-back")
+        
+        var currentPosition: CGFloat = 0.0
+        currentPosition += betweenCard
+        for _ in 1...7 {
+            let cardImage: UIImageView
+            cardImage = UIImageView(frame: CGRect(x: currentPosition, y: self.view.frame.minY + 50, width: cardWidth, height: cardHeight))
+            cardImage.image = image
+            self.view.addSubview(cardImage)
+            currentPosition += betweenCard + cardWidth
+        }
+    }
 
 }
 
