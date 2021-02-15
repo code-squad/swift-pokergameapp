c548fe717000148262f02ddd80bebc8b960f356b
BMO <b38238496@gmail.com>
2021-02-15 16:41:30 +0900

    status bar 재설정
    코드로 작성하는 방법도 있지만, 프로젝트에서 설정하는 방법도 있어 바꿔보았다.

--- PokerGameApp/PokerGameApp/ViewController.swift
+++ PokerGameApp/PokerGameApp/ViewController.swift
@@ -9,10 +9,6 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override var preferredStatusBarStyle: UIStatusBarStyle {
-        return .lightContent
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
