f2de96a2004f02b0b7d5d62e0f8031e621a1b1c3
Lia <judykim257@gmail.com>
2021-02-15 15:05:47 +0900

    step1 - 배경 설정

--- PokerGameApp/PokerGameApp/ViewController.swift
+++ PokerGameApp/PokerGameApp/ViewController.swift
@@ -11,7 +11,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        self.view.backgroundColor = UIColor(patternImage: UIImage(named: "bg_pattern.png")!)
         
         }
     
