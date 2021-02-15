2d101de58010450bdc2919d686c5903db3de8ffb
zeke <yangsin1031@naver.com>
2021-02-15 15:36:07 +0900

    카드의 뒷면 View 추가
    카드의 뒷면 View 7개를 추가하고 나란히 가로로 배치

--- PokerGameApp/PokerGameApp/ViewController.swift
+++ PokerGameApp/PokerGameApp/ViewController.swift
@@ -12,8 +12,79 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor(patternImage: UIImage(named: "bg_pattern")!)
-    }
 
+        self.view.addSubview(imageView1)
+        self.view.addSubview(imageView2)
+        self.view.addSubview(imageView3)
+        self.view.addSubview(imageView4)
+        self.view.addSubview(imageView5)
+        self.view.addSubview(imageView6)
+        self.view.addSubview(imageView7)
+        
+    }
+    lazy var imageView1: UIImageView = {
+        let width:CGFloat = self.view.bounds.width/7
+        let height:CGFloat = width*1.27
+        
+        let imageView = UIImageView(frame: CGRect(x: (self.view.bounds.width/7) * 0, y: 80, width: width, height: height))
+        let image = UIImage(named: "card-back")
+        imageView.image = image
+        return imageView
+    }()
+    lazy var imageView2: UIImageView = {
+        let width:CGFloat = self.view.bounds.width/7
+        let height:CGFloat = width*1.27
+        
+        let imageView = UIImageView(frame: CGRect(x: (self.view.bounds.width/7) * 1, y: 80, width: width, height: height))
+        let image = UIImage(named: "card-back")
+        imageView.image = image
+        return imageView
+    }()
+    lazy var imageView3: UIImageView = {
+        let width:CGFloat = self.view.bounds.width/7
+        let height:CGFloat = width*1.27
+        
+        let imageView = UIImageView(frame: CGRect(x: (self.view.bounds.width/7) * 2, y: 80, width: width, height: height))
+        let image = UIImage(named: "card-back")
+        imageView.image = image
+        return imageView
+    }()
+    lazy var imageView4: UIImageView = {
+        let width:CGFloat = self.view.bounds.width/7
+        let height:CGFloat = width*1.27
+        
+        let imageView = UIImageView(frame: CGRect(x: (self.view.bounds.width/7) * 3, y: 80, width: width, height: height))
+        let image = UIImage(named: "card-back")
+        imageView.image = image
+        return imageView
+    }()
+    lazy var imageView5: UIImageView = {
+        let width:CGFloat = self.view.bounds.width/7
+        let height:CGFloat = width*1.27
+        
+        let imageView = UIImageView(frame: CGRect(x: (self.view.bounds.width/7) * 4, y: 80, width: width, height: height))
+        let image = UIImage(named: "card-back")
+        imageView.image = image
+        return imageView
+    }()
+    lazy var imageView6: UIImageView = {
+        let width:CGFloat = self.view.bounds.width/7
+        let height:CGFloat = width*1.27
+        
+        let imageView = UIImageView(frame: CGRect(x: (self.view.bounds.width/7) * 5, y: 80, width: width, height: height))
+        let image = UIImage(named: "card-back")
+        imageView.image = image
+        return imageView
+    }()
+    lazy var imageView7: UIImageView = {
+        let width:CGFloat = self.view.bounds.width/7
+        let height:CGFloat = width*1.27
+        
+        let imageView = UIImageView(frame: CGRect(x: (self.view.bounds.width/7) * 6, y: 80, width: width, height: height))
+        let image = UIImage(named: "card-back")
+        imageView.image = image
+        return imageView
+    }()
 
 }
 
