69bb5672cdc70de8ff2b3d400feea123de6f8995
hoonha choi <sosqsz2@naver.com>
2021-02-15 16:53:10 +0900

    stackView 이용한 ImageView 추가 및 설정

--- PokerGameApp/PokerGameApp/ViewController.swift
+++ PokerGameApp/PokerGameApp/ViewController.swift
@@ -8,12 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let horizontalStackView : UIStackView  = {
+       let stack = UIStackView()
+        stack.axis = .horizontal
+        stack.distribution = .fillEqually
+        stack.alignment = .fill
+        stack.spacing = 5
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        return stack
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor(patternImage:
                                                 backgroundImageChange(imageName: "bg_pattern"))
         
+        viewAdd()
+        viewConstraint()
+        cardsCreate()
     }
 
 
@@ -22,6 +35,33 @@ class ViewController: UIViewController {
             return UIImage()
         }
         return backgroundimage
+    }
+    
+    func viewAdd() {
+        self.view.addSubview(horizontalStackView)
+    }
+    
+    func viewConstraint() {
+        NSLayoutConstraint.activate([
+            horizontalStackView.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor, constant: 5),
+            horizontalStackView.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor, constant: -5),
+            horizontalStackView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 40)
+        ])
+    }
+    
+    func cardsCreate()  {
+        (0...6).forEach { _ in
+            horizontalStackView.addArrangedSubview(imageViewCreate())
+        }
+    }
+
+    func imageViewCreate() -> UIImageView {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFit
+        imageView.image = UIImage(named: "card-back")
+        imageView.widthAnchor.constraint(equalTo: imageView.heightAnchor, multiplier: 1.0/1.27).isActive = true
+        return imageView
     }
 }
 
