7820f2e0763e9d29d4280a726376129591272b3a
Lia <judykim257@gmail.com>
2021-02-15 16:03:01 +0900

    step1 - 카드 뒷면 view 추가 및 크기 조절
    7등분한 view 를 적절한 위치에 넣음
    view 의 모서리를 둥글게 만듦

--- PokerGameApp/PokerGameApp/ViewController.swift
+++ PokerGameApp/PokerGameApp/ViewController.swift
@@ -12,12 +12,25 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor(patternImage: UIImage(named: "bg_pattern.png")!)
-        
+        for i in 0..<7 {
+            showCardBack(x: i)
         }
+    }
     
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return .lightContent
     }
 
+    func showCardBack(x: Int) {
+        let cardBackImage = UIImage(named: "card-back.png")
+        let cardBackImageView = UIImageView(image: cardBackImage!)
+        let width = self.view.bounds.width/7
+        
+        cardBackImageView.frame = CGRect(x: width*CGFloat(x)+3, y: 50, width: width-5, height: (width-5)*1.27)
+        cardBackImageView.layer.cornerRadius = 5
+        cardBackImageView.clipsToBounds = true
+        view.addSubview(cardBackImageView)
+    }
+    
 }
 
