bdaef2500461c31252de9493abba5b6d20773c17
zombietux <>
2021-02-15 22:50:12 +0900

    CaardImage Assets 위치 수정

--- PokerGameApp/PokerGameApp/CardImage.swift
+++ PokerGameApp/PokerGameApp/CardImage.swift
@@ -7,15 +7,17 @@
 
 import UIKit
 
+enum CardImageString: String {
+    case background = "bg_pattern"
+    case cardBack = "card-back"
+}
+
 struct CardImage {
-    private (set) var backgroundImg = "bg_pattern"
-    
     private (set) var cardBack: UIImageView = {
-        let cardBackImg = "card-back"
         let imageView = UIImageView()
         imageView.layer.masksToBounds = true
         imageView.layer.cornerRadius = 6
-        imageView.image = UIImage(named: cardBackImg)
+        imageView.image = UIImage(named: CardImageString.cardBack.rawValue)
         
         let ratio = NSLayoutConstraint(item: imageView, attribute: .height, relatedBy: .equal, toItem: imageView, attribute: .width, multiplier: (1.27 / 1.0), constant: 0)
         imageView.addConstraint(ratio)
