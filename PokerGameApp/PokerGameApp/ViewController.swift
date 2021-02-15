5b76abb142c29599204aaf2990b82a7545f0b592
1song2 <1song2918@gmail.com>
2021-02-15 22:37:14 +0900

    Set a background image using UIColor(patternImage:)
    Use 'guard let' to unwrap optional.

--- PokerGameApp/PokerGameApp/ViewController.swift
+++ PokerGameApp/PokerGameApp/ViewController.swift
@@ -9,11 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let backgroundImage = UIImage(named: "bg_pattern")
     let pokerCardImage = UIImage(named: "card-back")
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        guard let backgroundImage = UIImage(named: "bg_pattern") else { return }
+        self.view.backgroundColor = UIColor(patternImage: backgroundImage)
     }
 }
-
