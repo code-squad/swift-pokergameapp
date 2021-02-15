f6240be337f6b116627e33125aae2f147f974303
zombietux <>
2021-02-15 18:50:57 +0900

    사용하지 않는 함수와 주석 삭제

--- PokerGameApp/PokerGameApp/AppDelegate.swift
+++ PokerGameApp/PokerGameApp/AppDelegate.swift
@@ -9,9 +9,6 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         return true
     }
@@ -22,11 +19,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        
-    }
-
-
 }
 
