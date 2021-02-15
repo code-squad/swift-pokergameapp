01d9316716723723489bec3fdfe3a1bffd8bfba6
zeke <yangsin1031@naver.com>
2021-02-15 14:52:28 +0900

    배경화면 이미지패턴으로 지정
    ViewController 클래스에서 self.view 배경을 다음 이미지 패턴으로 지정

---
+++ PokerGameApp/PokerGameApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PokerGameApp
+//
+//  Created by 양준혁 on 2021/02/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor(patternImage: UIImage(named: "bg_pattern")!)
+    }
+
+
+}
+
