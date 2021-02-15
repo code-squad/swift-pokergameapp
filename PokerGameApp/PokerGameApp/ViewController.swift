cc1870a86454423c43422f85a5f28f2a4cb37bbd
Jisoo Kim <ghis22130@naver.com>
2021-02-15 16:58:02 +0900

    Step01 구현
    배경화면과 카드 이미지를 Assets에 추가하였습니다.
    카드를 7분할하여 화면에 출력될 수 있도록하였습니다.

---
+++ PokerGameApp/PokerGameApp/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  PokerGameApp
+//
+//  Created by 지북 on 2021/02/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setBackGround()
+        drawCard()
+    }
+}
+
+extension ViewController {
+    
+    func setBackGround () {
+        self.view.backgroundColor = UIColor(patternImage: UIImage(named: "bg_pattern.png")!)
+    }
+    
+    func drawCard() {
+        let cardImage : UIImage = UIImage(named: "card-back.png")!
+        
+        let padding : CGFloat = 5
+        let width = self.view.bounds.width / 7.8
+        let height = width * 1.27
+        
+        for i in 0...6{
+            let posX = width * CGFloat(i) + (padding * CGFloat(i)) + padding
+            let posY : CGFloat = 44
+            let imageView = UIImageView(frame: CGRect(x: posX, y: posY, width: width, height: height))
+
+            imageView.image = cardImage
+            self.view.addSubview(imageView)
+        }
+    }
+    
+}
