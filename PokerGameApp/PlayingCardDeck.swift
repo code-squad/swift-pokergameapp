051b6a5a3a95c42a8abcb1d248ded5c8057f39ba
ellyheetov <yaaajcm@naver.com>
2021-02-15 21:27:28 +0900

    Update PlayingCardDeck
    add shuffle, removeOne, reset funtion
    refactor init function

--- PokerGameApp/PlayingCardDeck.swift
+++ PokerGameApp/PlayingCardDeck.swift
@@ -8,18 +8,45 @@
 import Foundation
 
 struct PlayingCardDeck {
-    private var count : Int = 0
+    
+    private var count : Int {
+        get {
+            return cards.count
+        }
+    }
     private var cards  = [PlayingCard]()
     
     init(){
+        initialize()
+    }
+    
+    mutating func initialize(){
         for suit in PlayingCard.Suit.all {
             for rank in PlayingCard.Rank.all {
                 cards.append(PlayingCard(suit: suit, rank: rank))
-                count += 1
             }
         }
         print("카드 전체를 초기화했습니다.")
         print("총 \(count)장의 카드가 있습니다.")
     }
     
+    mutating func suffle(){
+        cards.shuffle()
+        print("전체 \(count)장의 카드를 섞었습니다.")
+    }
+    
+    mutating func removeOne(){
+        
+        guard let removed = cards.popLast() else {
+            print("남아있는 카드가 없습니다.")
+            return
+        }
+        print(removed.description)
+        print("총 \(count)장의 카드가 남아있습니다.")
+    }
+    
+    mutating func reset(){
+        cards.removeAll()
+        initialize()
+    }
 }
