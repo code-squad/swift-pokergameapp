d95f9e1ffdfcde1365de0a6760537d9625507b95
zombietux <>
2021-02-15 13:55:53 +0900

    ViewController self.view에 backgroundImg 적용

---
+++ PokerGameApp/PokerGameApp/StateController.swift
@@ -0,0 +1,12 @@
+//
+//  StateController.swift
+//  PokerGameApp
+//
+//  Created by zombietux on 2021/02/15.
+//
+
+import Foundation
+
+class StateController {
+    static private (set) var backgroundImg = "bg_pattern"
+}
