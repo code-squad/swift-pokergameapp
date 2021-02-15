57c0f124470b711c214ccc050f04e60e844a24f0
zombietux <unknown>
2021-02-15 18:02:33 +0900

    VC에서 hand 인스턴스 선언 후 rootVC에 뷰 추가 후 동작

--- PokerGameApp/PokerGameApp/ViewController.swift
+++ PokerGameApp/PokerGameApp/ViewController.swift
@@ -8,11 +8,16 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let hand = Hand()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor(patternImage: UIImage(named: StateController.backgroundImg) ?? UIImage())
+        self.view.addSubview(hand)
+        hand.setConstraint(superView: self.view)
     }
+    
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return .lightContent
     }
