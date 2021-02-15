962538dbba87dc9e74dbe737a3e4b9dbdafd6221
zombietux <unknown>
2021-02-15 13:42:41 +0900

    PokerGameApp프로젝트 생성 후 ViewController StatusBarStyle 수정

---
+++ PokerGameApp/PokerGameApp/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  PokerGameApp
+//
+//  Created by zombietux on 2021/02/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+}
+
