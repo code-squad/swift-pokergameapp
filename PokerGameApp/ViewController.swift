859f6a2f33c380661a257eb184c60ca98611e8ff
ellyheetov <yaaajcm@naver.com>
2021-02-15 16:19:06 +0900

    Update ViewController
    set view background pattern
    add  7 card subView

--- PokerGameApp/ViewController.swift
+++ PokerGameApp/ViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var cardImage : UIImage = {
+        return UIImage(named: "cardback.png")!
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.view.backgroundColor = UIColor(patternImage: UIImage(named: "bg_pattern")!)
+        
+        let cardWidth = (self.view.bounds.maxX - self.view.bounds.minX) / 8
+        let cardHeight = cardWidth * 1.27
+        let offset = cardWidth / 6
+        
+        for index in 0..<7 {
+            let imageView = UIImageView(frame: CGRect(x: self.view.frame.minX + (cardWidth + offset) * CGFloat(index),
+                                                      y: self.view.frame.minY + cardHeight,
+                                                      width: cardWidth, height: cardHeight))
+            imageView.image = cardImage
+            self.view.addSubview(imageView)
+        }
+        
     }
-
-
 }
 
