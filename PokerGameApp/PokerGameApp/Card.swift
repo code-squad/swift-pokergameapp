446e7d13e8d30951d20c884bca1430a251ddc1eb
hoonha choi <sosqsz2@naver.com>
2021-02-15 17:58:45 +0900

    Card class 생성

---
+++ PokerGameApp/PokerGameApp/Card.swift
@@ -0,0 +1,20 @@
+//
+//  Card.swift
+//  PokerGameApp
+//
+//  Created by HOONHA CHOI on 2021/02/15.
+//
+
+import Foundation
+
+class Card {
+    let pattern : String
+    let number : String
+    
+    init(pattern : String, number : String) {
+        self.pattern = pattern
+        self.number = number
+    }
+    
+    
+}
