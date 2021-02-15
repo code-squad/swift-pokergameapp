1f5fb0b1b65841ebddd9d80a59e0aa37270ccc05
JUNGYUN-Daegu/JUNGYUN-Daegu/jungyun <tgms021@gmail.com>
2021-02-15 21:34:20 +0900

    Add card images on first scene
    Programmatically implemented UIStackView
    Used a function to generate UIImageView for each of 7 cards

--- PokerGameApp/PokerGameApp/ViewController.swift
+++ PokerGameApp/PokerGameApp/ViewController.swift
@@ -9,9 +9,40 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    let imageStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.spacing = 10
+        stackView.distribution = .fillEqually
+        stackView.alignment = .center
+        stackView.axis = .horizontal
+        return stackView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor(patternImage: UIImage(named: "bg_pattern")!)
+        self.view.addSubview(imageStackView)
+        imageStackView.topAnchor.constraint(equalTo: self.view.topAnchor, constant: self.view.frame.height/10).isActive = true
+        imageStackView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        imageStackView.heightAnchor.constraint(equalTo: self.view.heightAnchor, multiplier: 0.07).isActive = true
+        
+        let numberOfCards = 7
+        for _ in 0..<numberOfCards {
+            let newCardImageView = cardIn()
+            imageStackView.addArrangedSubview(newCardImageView)
+            newCardImageView.widthAnchor.constraint(equalTo: imageStackView.heightAnchor, multiplier: 1/1.27).isActive = true
+        }
+    }
+    
+    func cardIn() -> UIImageView {
+//        let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: newWidth, height: newHeight))
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "card-back")
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+//        imageView.frame.size = CGSize(width: newWidth, height: newHeight)
+//        imageView.contentMode = .scaleToFill
+        return imageView
     }
 }
 
