07d89b2663c6a3764bb40c2e366dba3b58111702
hoonha choi <sosqsz2@naver.com>
2021-02-15 18:57:59 +0900

    enum Rank, enum Suit 구현

--- PokerGameApp/PokerGameApp/Card.swift
+++ PokerGameApp/PokerGameApp/Card.swift
@@ -7,14 +7,49 @@
 
 import Foundation
 
-class Card {
-    let pattern : String
-    let number : String
+enum Suit : String, CustomStringConvertible {
+    case spade = "♠️"
+    case heart = "♥️"
+    case club = "♣️"
+    case diamond = "♦️"
     
-    init(pattern : String, number : String) {
-        self.pattern = pattern
-        self.number = number
+    var description: String {
+        return "\(self.rawValue)"
+    }
+}
+
+enum Rank : Int, CustomStringConvertible {
+    case A = 1,two,three,four,five,six,seven,eight,nine,ten,J,Q,K
+    
+    var description: String {
+        return "\(self.reName())"
     }
     
+    func reName() -> String {
+        switch self {
+        case .A:
+            return "A"
+        case .J:
+            return "J"
+        case .Q:
+            return "Q"
+        case .K:
+            return "K"
+        default:
+            return "\(self.rawValue)"
+        }
+    }
+}
+class Card : CustomStringConvertible {
+    let suit : Suit
+    let rank : Rank
     
+    init(suit : Suit, rank : Rank) {
+        self.suit = suit
+        self.rank = rank
+    }
+    
+    var description: String {
+        return "\(suit)\(rank)"
+    }
 }
