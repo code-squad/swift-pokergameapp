119e0a73a3075b6323461dbb6b5d1a98d6e1abd2
zombietux <unknown>
2021-02-16 17:12:19 +0900

    Rank enum 수정

--- PokerGameApp/PokerGameApp/Rank.swift
+++ PokerGameApp/PokerGameApp/Rank.swift
@@ -8,35 +8,16 @@
 import Foundation
 
 enum Rank: Int, CustomStringConvertible, CaseIterable {
-    case A = 1
-    case two
-    case three
-    case four
-    case five
-    case six
-    case seven
-    case eight
-    case nine
-    case ten
-    case J
-    case Q
-    case K
+    case two = 2, three, four, five, six, seven, eight, nine, ten
+    case A, J, Q, K
     
     var description: String {
         switch self {
         case .A: return "A"
-        case .two: return "2"
-        case .three: return "3"
-        case .four: return "4"
-        case .five: return "5"
-        case .six: return "6"
-        case .seven: return "7"
-        case .eight: return "8"
-        case .nine: return "9"
-        case .ten: return "10"
         case .J: return "J"
         case .Q: return "Q"
         case .K: return "K"
+        default: return String(self.rawValue)
         }
     }
 }
