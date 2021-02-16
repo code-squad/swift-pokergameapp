4c4758545a3d551e066c5e7ab1b08f21a01a7567
BMO <b38238496@gmail.com>
2021-02-16 22:22:17 +0900

    PokerCard 내부 enum에 CaseIterable 추가
    카드덱을 만들 때 열거형 항목 순회를 하기위해

--- PokerGameApp/PokerGameApp/PokerCard.swift
+++ PokerGameApp/PokerGameApp/PokerCard.swift
@@ -11,11 +11,11 @@ class PokerCard : CustomStringConvertible {
 
     //  PokerCard 클래스 내부에 Suit, Rank를 표현하는 코드가 있으면
     //  보기에 더 직관적일것 같아서 nested enum으로 구현했습니다.
-    enum Suit: Character {
+    enum Suit: Character, CaseIterable {
         case spades = "♠", hearts = "♥", diamonds = "♦", clubs = "♣"
     }
     
-    enum Rank: Int {
+    enum Rank: Int, CaseIterable {
         case one = 1, two, three, four, five, six, seven, eight, nine, ten
         case jack, queen, king
     }
@@ -25,6 +25,8 @@ class PokerCard : CustomStringConvertible {
     var description: String {
         var over10: Character
         switch self.rank {
+        case .one:
+            over10 = "A"
         case .jack:
             over10 = "J"
         case .queen:
