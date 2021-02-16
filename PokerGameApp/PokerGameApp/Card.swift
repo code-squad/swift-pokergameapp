4d3f9bce46c39fcc7953435e71a5828b40880664
zeke <yangsin1031@naver.com>
2021-02-16 16:33:35 +0900

    Card 클래스 속성 추가 및 CardDeck 구조체 생성
    suit과 rank 속성을 상수로 추가하고 init메소드 추가
    CardDeck구조체 생성

--- PokerGameApp/PokerGameApp/Card.swift
+++ PokerGameApp/PokerGameApp/Card.swift
@@ -10,6 +10,14 @@ import Foundation
 // 번호와 모양은 카드의 속성이므로 Card라는 클래스안에 중첩된 타입으로 Suit과 Rank를 생성하였으며
 // 다양한 케이스들을 갖는 카드를 모델링하기때문에 Nested Type을 열거형으로 사용하였다.
 class Card {
+    var suit: Suit
+    var rank: Rank
+    
+    init(suit: Suit, rank: Rank) {
+        self.suit = suit
+        self.rank = rank
+    }
+    
     enum Suit: Character, CaseIterable {
         case spades = "♠"
         case hearts = "♡"
---
+++ PokerGameApp/PokerGameApp/CardDeck.swift
@@ -0,0 +1,15 @@
+//
+//  CardDeck.swift
+//  PokerGameApp
+//
+//  Created by 양준혁 on 2021/02/16.
+//
+
+import Foundation
+
+struct CardDeck {
+    
+    var cards: [Card] = []
+    
+
+}
