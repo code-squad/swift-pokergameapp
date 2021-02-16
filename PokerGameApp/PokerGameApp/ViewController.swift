56446076468d95e2ea25434ccf2e0945d4f8f421
Lia <judykim257@gmail.com>
2021-02-16 15:26:41 +0900

    step1 - stackView 코드 구현
    Array(repeating: count:) 쓰려다 시간을 많이 씀
    뷰와 스택뷰에 제약조건 거는 법을 학습함

--- PokerGameApp/PokerGameApp/ViewController.swift
+++ PokerGameApp/PokerGameApp/ViewController.swift
@@ -15,9 +15,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor(patternImage: (UIImage(named: "bg_pattern.png") ?? UIImage()))
-        for i in 0..<7 {
-            showCardBack(x: i)
-        }
+        showCards(cardNum: 7)
         testCard()
     }
     
@@ -25,13 +23,41 @@ class ViewController: UIViewController {
         return .lightContent
     }
 
-    func showCardBack(x: Int) {
+    func makeCard() -> UIView {
         let cardBackImageView = UIImageView(image: cardBackImage)
-        
-        cardBackImageView.frame = CGRect(x: width*CGFloat(x)+3, y: 50, width: width-5, height: (width-5)*1.27)
-        cardBackImageView.layer.cornerRadius = 5
+        let cardConstraint = NSLayoutConstraint(item: cardBackImageView,
+                                                attribute: .height,
+                                                relatedBy: .equal,
+                                                toItem: cardBackImageView,
+                                                attribute: .width,
+                                                multiplier: 1.27,
+                                                constant: 0)
+        cardBackImageView.addConstraint(cardConstraint)
         cardBackImageView.clipsToBounds = true
-        view.addSubview(cardBackImageView)
+        cardBackImageView.layer.cornerRadius = 5
+        
+        return cardBackImageView
+    }
+    
+    func showCards(cardNum: Int) {
+        var cards = [UIView]()
+        (0..<cardNum).forEach{ _ in
+            cards.append(makeCard())
+        }
+
+        let cardStackView = UIStackView(arrangedSubviews: cards)
+        cardStackView.axis = .horizontal
+        cardStackView.alignment = .fill
+        cardStackView.distribution = .fillEqually
+        cardStackView.spacing = 5
+        
+        self.view.addSubview(cardStackView)
+        cardStackView.translatesAutoresizingMaskIntoConstraints = false
+
+        cardStackView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 15).isActive = true
+        cardStackView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -15).isActive = true
+        cardStackView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        cardStackView.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 40).isActive = true
     }
     
     func testCard(){
