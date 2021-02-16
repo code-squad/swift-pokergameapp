e81c5891c8cd96cc08a683e7088bce7c85a92c43
zombietux <>
2021-02-16 14:38:29 +0900

    CardDeck 구조체 추가 후 reset메서드, shuffle메서드 추가

--- PokerGameApp/PokerGameApp/CardDeck.swift
+++ PokerGameApp/PokerGameApp/CardDeck.swift
@@ -16,4 +16,45 @@ struct CardDeck {
     init(cards: [Card]) {
         self.cards = cards
     }
+    
+    mutating func reset(with cards: [Card]) {
+        self.cards = cards
+    }
+    
+    mutating func shuffle() -> [Card] {
+        var swappedIndexes: [Int] = []
+        var shuffledCards = cards
+        
+        func swapCardWithRandomFollowingCard(index: Int) {
+            let randomIndex = index +
+                Int(arc4random_uniform(UInt32(maxCardsCount - index)))
+            shuffledCards.swapAt(index, randomIndex)
+            swappedIndexes.append(randomIndex)
+        }
+        
+        let maxCardsCount = 52
+        for index in 0..<maxCardsCount {
+            let cardWasNotSwappedYet = !swappedIndexes.contains(index)
+            guard cardWasNotSwappedYet else {
+                continue
+            }
+            swapCardWithRandomFollowingCard(index: index)
+        }
+        
+        return shuffledCards
+    }
+}
+
+struct CardsGenerator {
+    static func generateCards() -> [Card] {
+        var cards: [Card] = []
+        
+        for suit in Card.Suit.allCases {
+            for rank in Rank.allCases {
+                cards.append(Card(rank: rank, suit: suit))
+            }
+        }
+        
+        return cards
+    }
 }
