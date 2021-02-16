8036a87fd8795d71bc29186d7bf94b44fd3b7e73
zombietux <>
2021-02-16 16:44:22 +0900

    테스트 코드 수정

--- PokerGameApp/PokerGameAppTests/PokerGameAppTests.swift
+++ PokerGameApp/PokerGameAppTests/PokerGameAppTests.swift
@@ -13,34 +13,21 @@ class PokerGameAppTests: XCTestCase {
     var cardDeck = CardDeck()
     
     func testGenerateCardDeck() {
-        print("> 카드 초기화")
         let resetCardDeck = cardDeck.reset()
-        print("카드 전체를 초기화했습니다")
         
         XCTAssertEqual(52, resetCardDeck.count)
-        print("총 \(resetCardDeck.count)장의 카드가 있습니다.")
     }
     
     func testShuffled() {
-        print("> 카드 섞기")
         let shuffledCardDeck = cardDeck.shuffle()
     
         XCTAssertNotEqual(cardDeck.reset(), shuffledCardDeck)
-        print("> 전체 \(shuffledCardDeck.count)장의 카드를 섞었습니다.")
     }
     
     func testRemoveOne() {
-        removeOne()
+        cardDeck.removeOne()
         XCTAssertEqual(51, cardDeck.count())
-        print("총 \(cardDeck.count())장의 카드가 남아있습니다.")
-        removeOne()
+        cardDeck.removeOne()
         XCTAssertEqual(50, cardDeck.count())
-        print("총 \(cardDeck.count())장의 카드가 남아있습니다.")
-    }
-    
-    private func removeOne() {
-        print("> 카드 하나 뽑기")
-        let cardToRemoved = cardDeck.removeOne()
-        print(cardToRemoved)
     }
 }
