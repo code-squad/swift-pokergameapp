b926714fef63f11b3c6ced14d0cd6ced22d3a8f2
zombietux <unknown>
2021-02-16 11:03:47 +0900

    Suit 구조체를 Card의 중첩시킴, Suit 열거형 코드도 수정

--- PokerGameApp/PokerGameApp/Card.swift
+++ PokerGameApp/PokerGameApp/Card.swift
@@ -8,6 +8,17 @@
 import Foundation
 
 struct Card: CustomStringConvertible {
+    /*
+     Suit 타입은 enum으로 생성.
+     CardValue와 달리 다양한 초기화가 불필요하고 String에 맞게 유니코드만 반환해주면 된다고 판단.
+     */
+    enum Suit: Character {
+        case hearts = "❤️"
+        case spades = "♠️"
+        case diamonds = "♦️"
+        case clubs = "♣️"
+    }
+    
     let value: CardValue
     let suit: Suit
     
