4b383f3ce7af854f8b21c37762f4699be8eeba9d
1song2 <1song2918@gmail.com>
2021-02-16 15:10:43 +0900

    Add nested enum 'Rank' to 'Card' class
    Add comments about why I chose the nested enum type for 'Suit' and 'Rank'.

--- PokerGameApp/PokerGameApp/Card.swift
+++ PokerGameApp/PokerGameApp/Card.swift
@@ -8,10 +8,22 @@
 import Foundation
 
 class Card {
+    
+    /*
+     Suit와 Rank를 Nested Enum으로 구현한 이유:
+     Suit와 Rank 모두 제한된 범위 내에서 값을 가지므로 Enum으로 구현하는 것이 안전하고 편리할 것이라 생각했습니다.
+     또한 추후 카드 정보를 문자열로 반환시 1 → A, 11 → J 등 Rank의 몇몇 값을 변환해야 하는데,
+     이때 switch statement를 사용하기에도 Enum이 좋을 것이라 판단했습니다.
+     */
+    
     enum Suit: Character {
         case spades = "\u{2660}"
         case hearts = "\u{2665}"
         case diamonds = "\u{2666}"
         case clubs = "\u{2663}"
+    }
+    
+    enum Rank: Int {
+        case one = 1, two, three, four, five, six, seven, eight, nine, ten, eleven, twelve, thirteen
     }
 }
