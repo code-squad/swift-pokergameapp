94a9024c9bf814accf3763350186b00a4b0a5deb
dahun-lee-daji <ssss6596@gmail.com>
2021-02-16 17:34:41 +0900

    카드 뒷면 7개 구현
    view에 subview를 다수 추가하여 구현함.
    stackView로 수정 예정
    Optional Binding 실패 시 modal present 하여 앱 suspend. -> 구현을 위해 viewDidAppear를 override함.

--- PokerGameApp/PokerGameApp/ViewController.swift
+++ PokerGameApp/PokerGameApp/ViewController.swift
@@ -8,16 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let cardBacksideImage = UIImage(named: "card-back")
+    let trouble = TroubleShooter()
     
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = UIColor(patternImage: optionalBindingBGImage())
+        
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(false)
+        
+        do{
+            self.view.backgroundColor = UIColor(patternImage: try optionalBindingBGImage())
+        }
+        catch {
+            print(error)
+            present(trouble.fatalError(), animated: true, completion: nil)
+            //UIControl().sendAction(#selector(URLSessionTask.suspend), to: UIApplication.shared, for: nil)
+        }
+        
+        for _ in 0..<7 {
+            self.view.addSubview(backsideOfCard())
+        }
     }
     
-    func optionalBindingBGImage() -> UIImage {
+    private func optionalBindingBGImage() throws -> UIImage {
         guard let image = UIImage(named: "bg_pattern") else {
-            return UIImage()
+            throw TroubleShooter.errors.optionalBindingError
         }
         return image
     }
@@ -26,5 +43,18 @@ class ViewController: UIViewController {
         return .lightContent
     }
 
+    func backsideOfCard() -> UIImageView {
+        
+        let cardBackSideWidth : CGFloat = (view.bounds.width - 80)/7  //214
+        let cardBackSideHeight : CGFloat = cardBackSideWidth * 1.27
+        
+        let downPosX: CGFloat = 10 + CGFloat(self.view.subviews.count ) * (cardBackSideWidth + 10)
+        let downPosY: CGFloat = 50
+        
+        let imageView: UIImageView = UIImageView(frame: CGRect(x: downPosX, y: downPosY, width: cardBackSideWidth, height: cardBackSideHeight))
+        imageView.image = self.cardBacksideImage
+
+        return imageView
+    }
 }
 
---
+++ PokerGameApp/PokerGameApp/troubleShooter.swift
@@ -0,0 +1,28 @@
+//
+//  troubleShooter.swift
+//  PokerGameApp
+//
+//  Created by 이다훈 on 2021/02/16.
+//
+
+import Foundation
+import UIKit
+
+class TroubleShooter {
+    enum errors : Error {
+        case optionalBindingError
+    }
+    func fatalError() -> UIAlertController {
+        
+        let alert = UIAlertController(title: "치명적 오류 발생", message: nil, preferredStyle: .actionSheet)
+        let stop = UIAlertAction(title: "앱 정지", style: .destructive, handler: {(action) in self.suspendApp()})
+        
+        alert.addAction(stop)
+        return alert
+    }
+    
+    private func suspendApp(){
+        UIControl().sendAction(#selector(URLSessionTask.suspend), to: UIApplication.shared, for: nil)
+
+    }
+}
