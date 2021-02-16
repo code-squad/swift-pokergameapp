626d0f070900dcb0f21a1d83fda5e95fb7361eab
zombietux <unknown>
2021-02-17 08:51:55 +0900

    Hand Struct 생성

---
+++ PokerGameApp/PokerGameApp/Hand.swift
@@ -0,0 +1,16 @@
+//
+//  Hand.swift
+//  PokerGameApp
+//
+//  Created by zombietux on 2021/02/17.
+//
+
+import Foundation
+
+struct Hand {
+    private let cards: [Card]
+    
+    init(cards: [Card]) {
+        self.cards = cards
+    }
+}
