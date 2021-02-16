3b446d79fca0849bafb696663690355d25883a79
zombietux <>
2021-02-16 11:27:06 +0900

    rank 구조체 수정

--- PokerGameApp/PokerGameApp/Rank.swift
+++ PokerGameApp/PokerGameApp/Rank.swift
@@ -7,45 +7,17 @@
 
 import Foundation
 
-/*
- CardValue는 구조체를 사용
- 1. Int로 초기화시키고, String으로 display하기에 편한 구조라 생각.
- 2. 프로그램 사용 중에 클래스 참조가 필요없어서 class는 생각하지 않음.
- */
-struct Rank: ExpressibleByIntegerLiteral {
-    typealias IntegerLiteralType = Int
-    
-    let display: String
-    let value: Int
-    
+struct Rank {
+    private var rank = String()
+
     init(integerLiteral value: Int) {
-        self.value = value
         switch value {
-        case 2...10: display = String(value)
-        case 1: display = "A"
-        case 11: display = "J"
-        case 12: display = "Q"
-        case 13: display = "K"
-        default:
-            fatalError("value \(value) out of bounds for cardValue")
-        }
-    }
-    
-    init?(display: String) {
-        guard display.count == 1 else { return nil }
-        self.display = display
-        
-        switch display {
-        case "A": value = 1
-        case "J": value = 11
-        case "Q": value = 12
-        case "K": value = 13
-        default:
-            if let digit = Int(display) {
-                value = digit
-            } else {
-                return nil
-            }
+        case 2...10: rank = String(value)
+        case 1: rank = "A"
+        case 11: rank = "J"
+        case 12: rank = "Q"
+        case 13: rank = "K"
+        default: break
         }
     }
 }
