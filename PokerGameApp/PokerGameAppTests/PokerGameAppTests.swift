525894f5ffabbfa3dbe65283514151026d4d1640
zombietux <>
2021-02-16 15:21:07 +0900

    카드 초기화 테스트 코드 작성

--- PokerGameApp/PokerGameAppTests/PokerGameAppTests.swift
+++ PokerGameApp/PokerGameAppTests/PokerGameAppTests.swift
@@ -9,4 +9,13 @@ import XCTest
 @testable import PokerGameApp
 
 class PokerGameAppTests: XCTestCase {
+   
+    func testGenerateCardDeck() {
+        print("> 카드 초기화")
+        var cardDeck = CardDeck()
+        print("카드 전체를 초기화했습니다")
+        
+        XCTAssertEqual(52, cardDeck.count())
+        print("총 \(cardDeck.count())장의 카드가 있습니다.")
+    }
 }
