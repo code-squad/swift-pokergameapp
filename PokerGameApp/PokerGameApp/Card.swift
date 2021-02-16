d82b938e1d07c3ac72bb972c795151a30c8511b2
Song Lee <songlish233@gmail.com>
2021-02-16 11:58:14 +0900

    CardDeck Struct 생성 및 기능 구현
    - Card: CardDeck 기능 구현 위한 프로토콜 채택

--- PokerGameApp/PokerGameApp/Card.swift
+++ PokerGameApp/PokerGameApp/Card.swift
@@ -13,7 +13,7 @@ class Card {
     /// init 시 가독성을 위해 영문으로 case 이름 생성, 출력을 위한 유니코드를 rawValue로 설정
     let suit: Suit
     
-    enum Suit: String {
+    enum Suit: String, CaseIterable {
         case spade = "\u{2660}"
         case club = "\u{2663}"
         case heart = "\u{2665}"
@@ -24,7 +24,7 @@ class Card {
     /// 내부 rank는 계산 편의성 위해 Int로 저장, 프린트용 변환은 개별 메소드로 구현
     let rank: Int
     
-    enum Rank: Int {
+    enum Rank: Int, CaseIterable {
         case rank1 = 1,
              rank2, rank3, rank4, rank5, rank6, rank7,
              rank8, rank9, rank10, rank11, rank12, rank13
