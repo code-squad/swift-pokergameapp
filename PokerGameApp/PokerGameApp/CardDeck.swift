5add634d27831a033eee9b11d6bc60042452264a
shimyoungmin <dudals6965@gmail.com>
2021-02-16 19:31:15 +0900

    shuffle 함수 구현

--- PokerGameApp/PokerGameApp/CardDeck.swift
+++ PokerGameApp/PokerGameApp/CardDeck.swift
@@ -11,13 +11,23 @@ struct CardDeck {
     var cards: [Card] = []
     init() {
         for i in 1...13 {
-            cards.append(Card(Card.Nums(rawValue: i)!, Card.Shapes.clobers))
-            cards.append(Card(Card.Nums(rawValue: i)!, Card.Shapes.diamonds))
-            cards.append(Card(Card.Nums(rawValue: i)!, Card.Shapes.hearts))
-            cards.append(Card(Card.Nums(rawValue: i)!, Card.Shapes.spades))
+            self.cards.append(Card(Card.Nums(rawValue: i)!, Card.Shapes.clobers))
+            self.cards.append(Card(Card.Nums(rawValue: i)!, Card.Shapes.diamonds))
+            self.cards.append(Card(Card.Nums(rawValue: i)!, Card.Shapes.hearts))
+            self.cards.append(Card(Card.Nums(rawValue: i)!, Card.Shapes.spades))
         }
     }
     func count() -> Int {
-        return cards.count
+        return self.cards.count
+    }
+    mutating func shuffle() {
+        for i in 0..<self.cards.count-1 {
+            let randomIndex = Int.random(in: 1..<self.cards.count)
+            let temp = self.cards[i]
+            
+            self.cards[i] = self.cards[randomIndex]
+            self.cards[randomIndex] = temp
+        }
+        print(self.cards)
     }
 }
