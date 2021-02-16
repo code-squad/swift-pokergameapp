14fe809848f320680faae62ab73f909fa8ed93e6
HongzCloud <jihong21511@gmail.com>
2021-02-16 14:16:55 +0900

    💄 Add card-back image stackView
    스택뷰를 생성해서 간격에 맞게 7개의 카드 뒷모습을 추가했다.

--- PokerGameApp/PokerGameApp/ViewController.swift
+++ PokerGameApp/PokerGameApp/ViewController.swift
@@ -16,12 +16,42 @@ class ViewController: UIViewController {
     
     func initialize() {
         setBackground()
+        createCardStackView()
     }
     
     func setBackground() {
         if let image = UIImage(named: "bg_pattern") {
             self.view.backgroundColor = UIColor(patternImage: image)
         }
+    }
+    
+    func createCardImageView() -> UIImageView {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "card-back")
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.heightAnchor.constraint(equalTo: imageView.widthAnchor, multiplier: 1.27).isActive = true
+        imageView.clipsToBounds = true
+        imageView.layer.cornerRadius = 5
+        
+        return imageView
+    }
+    
+    func createCardStackView() {
+        let stackView = UIStackView()
+        
+        for _ in 0..<7 {
+             stackView.addArrangedSubview(createCardImageView())
+        }
+
+        stackView.axis = .horizontal
+        stackView.distribution = .fillEqually
+        stackView.spacing = 5
+        self.view.addSubview(stackView)
+        
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.topAnchor.constraint(equalTo: self.view.topAnchor, constant: self.view.frame.height/10).isActive = true
+        stackView.trailingAnchor.constraint(equalTo: self.view.layoutMarginsGuide.trailingAnchor).isActive = true
+        stackView.leadingAnchor.constraint(equalTo: self.view.layoutMarginsGuide.leadingAnchor).isActive = true
     }
     
     //상태바 글씨를 흰색으로 변경
