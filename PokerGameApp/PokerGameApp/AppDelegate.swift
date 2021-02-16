80b8f12019e973298401fc1341e2244bffe0a6f9
1song2 <1song2918@gmail.com>
2021-02-16 11:38:34 +0900

    Remove unused functions

--- PokerGameApp/PokerGameApp/AppDelegate.swift
+++ PokerGameApp/PokerGameApp/AppDelegate.swift
@@ -24,9 +24,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-
-    }
 
 
 }
