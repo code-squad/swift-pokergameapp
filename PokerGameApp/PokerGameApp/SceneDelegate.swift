755473ccc3893ffb7fdccb683243d064cb461725
1song2 <1song2918@gmail.com>
2021-02-16 11:38:34 +0900

    Remove unused functions

--- PokerGameApp/PokerGameApp/SceneDelegate.swift
+++ PokerGameApp/PokerGameApp/SceneDelegate.swift
@@ -17,25 +17,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let _ = (scene as? UIWindowScene) else { return }
     }
 
-    func sceneDidDisconnect(_ scene: UIScene) {
-
-    }
-
-    func sceneDidBecomeActive(_ scene: UIScene) {
-
-    }
-
-    func sceneWillResignActive(_ scene: UIScene) {
-
-    }
-
-    func sceneWillEnterForeground(_ scene: UIScene) {
-
-    }
-
-    func sceneDidEnterBackground(_ scene: UIScene) {
-	
-    }
 
 
 }
