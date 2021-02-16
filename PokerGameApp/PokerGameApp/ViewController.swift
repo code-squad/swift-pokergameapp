92949c4073f3a9061a03a46e8427e2dcdbecf09e
HongzCloud <jihong21511@gmail.com>
2021-02-16 11:50:03 +0900

    💄 Add background pattern
    배경 패턴을 추가하고 패턴 간격을 촘촘하게 했다.

--- PokerGameApp/PokerGameApp/ViewController.swift
+++ PokerGameApp/PokerGameApp/ViewController.swift
@@ -11,9 +11,19 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        initialize()
     }
-
+    
+    func initialize() {
+        setBackground()
+    }
+    
+    func setBackground() {
+        if let image = UIImage(named: "bg_pattern") {
+            self.view.backgroundColor = UIColor(patternImage: image)
+        }
+    }
+    
     //상태바 글씨를 흰색으로 변경
     override var preferredStatusBarStyle: UIStatusBarStyle {
            return .lightContent
