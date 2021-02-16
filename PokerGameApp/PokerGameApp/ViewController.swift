2159e23f7a77d10852302595838837f34ff83eaa
jhpark-steve <park9368@naver.com>
2021-02-16 18:26:47 +0900

    readme에 결과 이미지 추가하고 인스턴스 생성
    class 뷰 로드할 때 생성해서 print 했습니다.

--- PokerGameApp/PokerGameApp/ViewController.swift
+++ PokerGameApp/PokerGameApp/ViewController.swift
@@ -16,16 +16,23 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         initGameplay()
+        let card = Card()
+        print(card.returnValue())
     }
+    
     func initGameplay(){
         let pattern : UIImage = UIImage(named: "bg_pattern")!
         self.view.backgroundColor = UIColor(patternImage: pattern)
         var CardViewer : [UIImageView] = []
+        let cardXpos : CGFloat = self.view.frame.size.width/7
         let offset : CGFloat = CGFloat(self.view.frame.size.width/2) - CGFloat(self.view.frame.width/7 * 4) + CGFloat((self.view.frame.width/8)/2)
+        let cardWidth : CGFloat = self.view.frame.size.width/8
+        let cardHeight : CGFloat = cardWidth * 1.27
+        
         for i in 0..<7{
             let j : CGFloat = CGFloat(i)
             CardViewer.append(UIImageView(image: UIImage(named: "card-back")))
-            CardViewer[i].frame = CGRect(x: self.view.frame.size.width/7 * j - offset, y: 50, width: self.view.frame.size.width/8 , height: self.view.frame.size.width/8 * 1.27)
+            CardViewer[i].frame = CGRect(x: cardXpos * j - offset, y: 50, width: cardWidth , height: cardHeight)
             self.view.addSubview(CardViewer[i])
         }
     }
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # swift-pokergameapp
 STEP15 포커게임 앱 저장소
+
+![cardImageCardIns](/images/step1.png)
