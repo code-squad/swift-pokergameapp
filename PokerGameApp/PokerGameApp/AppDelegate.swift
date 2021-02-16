d859adf68ad99d1811e7848f03bbf674d876f174
Jun Ho JANG <junhojang@Junui-MacBookPro.local>
2021-02-16 20:01:44 +0900

    커밋이 나눠지고 있는지 모르겠습니다.ㅠㅠ

--- PokerGameApp/PokerGameApp/AppDelegate.swift
+++ PokerGameApp/PokerGameApp/AppDelegate.swift
@@ -20,9 +20,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-    }
-
-
+    
 }
 
