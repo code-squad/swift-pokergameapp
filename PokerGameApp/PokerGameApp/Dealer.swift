f25fa368dafd82c8689130bf6bdc1e7f257a0132
Ray <readingod@naver.com>
2021-02-17 15:31:10 +0900

    딜러 클래스에 프로퍼티와 메소드 추가
    cardDeck은 전체 카드를 저장하는 프로퍼티이고, handDeck은 딜러가 들고있을 카드를 저장하는 프로퍼티입니다. 딜러는 셔플과 카드분배(deal)를 해야하기 때문에 초기화 코드에 shuffle()을 추가하고 deal 메서드를 구현했습니다.

--- PokerGameApp/PokerGameApp/Dealer.swift
+++ PokerGameApp/PokerGameApp/Dealer.swift
@@ -1,5 +1,17 @@
 import Foundation
 
 class Dealer {
+    private var cardDeck:CardDeck
+    private var handDeck:[Card] // 딜러가 들고 있을 카드덱
     
+    init() {
+        self.cardDeck = CardDeck()
+        self.handDeck = [Card]()
+        self.cardDeck.shuffle() // 셔플은 딜러의 역할
+    }
+    
+    //각 참가자(player)에게 카드를 한 장씩 돌림
+    func deal() -> Card? {
+        return self.cardDeck.removeOne()
+    }
 }
