763952d3bd0943812d941c9b02ad144c9144d12c
ellyheetov <yaaajcm@naver.com>
2021-02-17 13:06:17 +0900

    Update PlayingCardDeck
    description variable
    removeOneCard, removeCards
    takeOneCard, takeCards

--- PokerGameApp/PlayingCardDeck.swift
+++ PokerGameApp/PlayingCardDeck.swift
@@ -7,8 +7,8 @@
 
 import Foundation
 
-class PlayingCardDeck {
-    
+class PlayingCardDeck : CustomStringConvertible{
+
     public var count : Int {
         get {
             return cards.count
@@ -16,8 +16,8 @@ class PlayingCardDeck {
     }
     private var cards  = [PlayingCard]()
     
-    init(){
-        reset()
+    var description: String {
+        return cards.description
     }
     
     private func initialize(){
@@ -33,10 +33,28 @@ class PlayingCardDeck {
     }
     
     @discardableResult
-    func removeOne() -> PlayingCard?{
+    func removeOneCard() -> PlayingCard?{
         return cards.popLast()
     }
     
+    func removeCards(count : Int) -> [PlayingCard] {
+        var removedCards = [PlayingCard]()
+        for _ in 0..<count {
+            guard let removedCard = removeOneCard() else {
+                return [PlayingCard]()
+            }
+            removedCards.append(removedCard)
+        }
+        return removedCards
+    }
+    
+    func takeOneCard(card : PlayingCard){
+        cards.append(card)
+    }
+    
+    func takeCards(cards : [PlayingCard]){
+        self.cards.append(contentsOf: cards)
+    }
     func reset(){
         cards.removeAll()
         initialize()
