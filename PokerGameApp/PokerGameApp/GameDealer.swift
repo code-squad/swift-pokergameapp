548299733aa7788df046daf17e59271bdc38c412
Sonjh1306 <yale1306@gmail.com>
2021-02-17 23:21:57 +0900

    PokerGame 클래스 내부의 handoutCard, makePlayer, start 함수 구현

--- PokerGameApp/PokerGameApp/GameDealer.swift
+++ PokerGameApp/PokerGameApp/GameDealer.swift
@@ -9,6 +9,7 @@ import Foundation
 
 class PokerGame {
     private var cardDeck = CardDeck()
+    private var players: [[Card]] = [[]]
     
     enum Player: Int {
         case one = 1, two, three, four
@@ -26,4 +27,29 @@ class PokerGame {
         cardDeck.shuffle()
     }
     
+    func handoutCards() {
+        for player in 0..<players.count {
+            for _ in 0..<rule.rawValue {
+                players[player].append(cardDeck.removeOne()!)
+            }
+        }
+    }
+    
+    func makePlayer() {
+        for _ in 0..<player.rawValue {
+            let player: [Card] = []
+            players.append(player)
+        }
+    }
+    
+    func start() {
+        makePlayer()
+        handoutCards()
+        print("\(rule.rawValue)카드 기준, 참가자 \(player.rawValue)일 때")
+        for i in 0..<players.count - 1 {
+            print("player\(i+1): \(players[i])")
+        }
+        print("dealer: \(players[players.count - 1])")
+    }
 }
+
