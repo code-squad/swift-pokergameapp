d148d8ff9af36826fdbe5ed40271c9135f0788b4
Lia <judykim257@gmail.com>
2021-02-17 14:18:38 +0900

    step2 - CustomStringConvertible 적용

--- PokerGameApp/PokerGameApp/Card.swift
+++ PokerGameApp/PokerGameApp/Card.swift
@@ -8,7 +8,9 @@
 import Foundation
 
 // 4가지 모양만 존재 가능하다고 명시하는 효과가 있는 것 같아서 선택
-enum Shape: Character {
+enum Shape: Character, CustomStringConvertible {
+    var description: String { return "\(self.rawValue)" }
+    
     case Spade = "♠️"
     case Clover = "♣️"
     case Diamond = "♦️"
@@ -16,12 +18,14 @@ enum Shape: Character {
 }
 
 // 1-13 범위만 받을 수 있고, 값에 해당하는 String 값을 원시값으로 가지고 있다
-enum Rank: String {
+enum Rank: String, CustomStringConvertible {
+    var description: String { return "\(self.rawValue)" }
+    
     case ace = "A", two = "2", three = "3", four = "4", five = "5", six = "6", seven = "7", eight = "8", nine = "9", ten = "10", jack = "J", queen = "Q", king = "K"
 }
 
 
-class Card {
+class Card: CustomStringConvertible {
     private let shape: Shape
     private let rank: Rank
     
@@ -29,10 +33,8 @@ class Card {
         self.shape = shape
         self.rank = rank
     }
-}
-
-extension Card: CustomStringConvertible {
+    
     var description: String {
-        return "\(self.shape.rawValue)\(self.rank.rawValue)"
+        return "\(self.shape)\(self.rank)"
     }
 }
