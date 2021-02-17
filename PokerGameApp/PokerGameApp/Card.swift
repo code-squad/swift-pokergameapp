8cfd34c488f3b1e7d292ddd35fcc5b4acd2a4692
shimyoungmin <dudals6965@gmail.com>
2021-02-17 17:55:32 +0900

    Enum Type extension 으로 CustomStringConverible 구현했던 것 다시 원래대로 enum type 선언안으로 이동 & 코드스타일 수정

--- PokerGameApp/PokerGameApp/Card.swift
+++ PokerGameApp/PokerGameApp/Card.swift
@@ -9,25 +9,31 @@ import Foundation
 
 class Card {
     
-    enum Shapes: String, CaseIterable{
-//        ♣️
-//        클로버 카드
-//        유니코드: U+2663 U+FE0F, UTF-8: E2 99 A3 EF B8 8F
-//        ♠️
-//        스페이드 카드
-//        유니코드: U+2660 U+FE0F, UTF-8: E2 99 A0 EF B8 8F
-//        ❤️
-//        빨간색 하트
-//        유니코드: U+2764 U+FE0F, UTF-8: E2 9D A4 EF B8 8F
-//        ♦️
-//        다이아몬드 카드
-//        유니코드: U+2666 U+FE0F, UTF-8: E2 99 A6 EF B8 8F
+    enum Shapes: String, CaseIterable, CustomStringConvertible{
         case hearts = "❤️", spades = "♠️", clobers = "♣️", diamonds = "♦️"
-
+        var description: String {
+            return self.rawValue
+        }
     }
 
-    enum Nums: Int, CaseIterable{
+    enum Nums: Int, CaseIterable, CustomStringConvertible{
         case one = 1, two, three, four, five, six, seven, eight, nine, ten, jack, queen, king
+        var description: String {
+            var resultStr = ""
+            switch self{
+            case .one:
+                resultStr = "A"
+            case .jack:
+                resultStr = "J"
+            case .queen:
+                resultStr = "Q"
+            case .king:
+                resultStr = "K"
+            default:
+                resultStr = "\(self.rawValue)"
+            }
+            return resultStr
+        }
     }
     
     private var num: Nums
@@ -42,30 +48,5 @@ class Card {
 extension Card: CustomStringConvertible {
     var description: String {
         return "\(self.shape)\(self.num)"
-    }
-}
-
-extension Card.Nums: CustomStringConvertible {
-    var description: String {
-        var resultStr = ""
-        switch self{
-        case .one:
-            resultStr = "A"
-        case .jack:
-            resultStr = "J"
-        case .queen:
-            resultStr = "Q"
-        case .king:
-            resultStr = "K"
-        default:
-            resultStr = "\(self.rawValue)"
-        }
-        return resultStr
-    }
-}
-
-extension Card.Shapes: CustomStringConvertible {
-    var description: String {
-        return self.rawValue
     }
 }
