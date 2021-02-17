0ba8cb2ee216bfd13a9f6d90108d9038bbb61cd2
ellyheetov <yaaajcm@naver.com>
2021-02-17 17:52:55 +0900

    Test cardDeck : move some function to cards test

--- PokerGameAppTests/CardDeckTest.swift
+++ PokerGameAppTests/CardDeckTest.swift
@@ -10,11 +10,11 @@ import XCTest
 
 class CardDeckTest: XCTestCase {
 
-    var cardDeck : PlayingCardDeck!
+    var cardDeck : CardDeck!
     
     override func setUp() {
         super.setUp()
-        cardDeck = PlayingCardDeck()
+        cardDeck = CardDeck()
         cardDeck.initialize()
     }
 
@@ -24,42 +24,5 @@ class CardDeckTest: XCTestCase {
     
     func testShuffle(){
         XCTAssertNoThrow(cardDeck.shuffle(), "카드를 섞지 못했습니다")
-    }
-    
-    func testRemoveOneCard(){
-        let removed = cardDeck.removeOneCard()
-        XCTAssertTrue(removed != nil, "남아있는 카드가 없습니다")
-    }
-    
-    func testRemoveCards(){
-        let count = 10
-        let removed = cardDeck.removeCards(count: count)
-        
-        XCTAssertEqual(removed.count, count)
-    }
-    
-    func testRemoveAll(){
-        XCTAssertNoThrow(cardDeck.removeAll(), "카드를 초기화 할 수 없습니다.")
-    }
-    
-    func testTakeOneCard(){
-        let guessCount = 53
-        let card = PlayingCard(suit: .clubs, rank: .eight)
-        cardDeck.takeOneCard(card: card)
-        
-        XCTAssertEqual(guessCount, cardDeck.count, "카드가 정상적으로 추가되지 않았습니다.")
-    }
-    
-    func testTakeCards(){
-        let guessCount = 57
-        let cards = [PlayingCard(suit: .clubs, rank: .ace),
-                     PlayingCard(suit: .clubs, rank: .two),
-                     PlayingCard(suit: .clubs, rank: .three),
-                     PlayingCard(suit: .clubs, rank: .four),
-                     PlayingCard(suit: .clubs, rank: .five)]
-                     
-        cardDeck.takeCards(cards: cards)
-        
-        XCTAssertEqual(guessCount, cardDeck.count, "카드가 정상적으로 추가되지 않았습니다.")
     }
 }
