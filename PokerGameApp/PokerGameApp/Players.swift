95986373487dd8312ce4bb8d7e5cf2ef486512e1
hoonha choi <sosqsz2@naver.com>
2021-02-17 10:31:11 +0900

    Players 객체 생성

---
+++ PokerGameApp/PokerGameApp/Players.swift
@@ -0,0 +1,17 @@
+//
+//  Players.swift
+//  PokerGameApp
+//
+//  Created by HOONHA CHOI on 2021/02/17.
+//
+
+import Foundation
+
+
+class Players {
+    private var players = [Player]()
+    
+    func addPlayer(player : Player) {
+        players.append(player)
+    }
+}
