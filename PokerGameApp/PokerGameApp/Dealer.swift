a1d50fceebe02a7c8fd0c603c3146495bc2d80a0
shimyoungmin <dudals6965@gmail.com>
2021-02-17 18:24:22 +0900

    Dealer Class를 포함한 Dealer.swift 파일생성

---
+++ PokerGameApp/PokerGameApp/Dealer.swift
@@ -0,0 +1,20 @@
+//
+//  Dealer.swift
+//  PokerGameApp
+//
+//  Created by 심영민 on 2021/02/17.
+//
+
+import Foundation
+
+class Dealer {
+    private var cards: [Card] = []
+    
+    func receiveCard(_ card: Card) {
+        cards.append(card)
+    }
+    
+    var getCard: [Card] {
+        return cards
+    }
+}
