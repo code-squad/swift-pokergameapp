932118e4e062ada88aed54e05d3a5958a520bec4
ellyheetov <yaaajcm@naver.com>
2021-02-17 17:48:32 +0900

    Update CardDeck : divide two class CardDeck and Cards

--- PokerGameApp/CardDeck.swift
+++ PokerGameApp/CardDeck.swift
@@ -7,56 +7,23 @@
 
 import Foundation
 
-class CardDeck : CustomStringConvertible{
-
+class CardDeck : Cards{
+    
     public var count : Int {
         get {
             return cards.count
         }
     }
-    private var cards  = [PlayingCard]()
-    
-    var description: String {
-        return cards.description
-    }
-    
+
     public func initialize(){
         removeAll()
-        for suit in PlayingCard.Suit.allCases {
-            for rank in PlayingCard.Rank.allCases {
-                cards.append(PlayingCard(suit: suit, rank: rank))
+        for suit in Card.Suit.allCases {
+            for rank in Card.Rank.allCases {
+                cards.append(Card(suit: suit, rank: rank))
             }
         }
     }
-    
     public func shuffle(){
         cards.shuffle()
-    }
-    
-    @discardableResult
-    public func removeOneCard() -> PlayingCard?{
-        return cards.popLast()
-    }
-    
-    public func removeCards(count : Int) -> [PlayingCard] {
-        var removedCards = [PlayingCard]()
-        for _ in 0..<count {
-            guard let removedCard = removeOneCard() else {
-                return [PlayingCard]()
-            }
-            removedCards.append(removedCard)
-        }
-        return removedCards
-    }
-    
-    public func takeOneCard(card : PlayingCard){
-        cards.append(card)
-    }
-    
-    public func takeCards(cards : [PlayingCard]){
-        self.cards.append(contentsOf: cards)
-    }
-    public func removeAll(){
-        cards.removeAll()
     }
 }
