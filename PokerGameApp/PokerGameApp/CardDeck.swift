8e98f230087ef5a1b6e079d9bd75885e6471dd11
HongzCloud <jihong21511@gmail.com>
2021-02-17 23:00:30 +0900

    ✨ Add CardDeck class
    카드덱 클래스를 만들고 카드 갯수, 카드 뽑기, 카드 섞기, 카드 리셋 기능을 추가했다.

---
+++ PokerGameApp/PokerGameApp/CardDeck.swift
@@ -0,0 +1,43 @@
+//
+//  CardDeck.swift
+//  PokerGameApp
+//
+//  Created by 오킹 on 2021/02/17.
+//
+
+import Foundation
+
+struct CardDeck {
+    private var cardDeck = [Card]()
+
+    init() {
+        reset()
+    }
+    
+    public var count: Int {
+        return cardDeck.count
+    }
+    
+    //Knuth Shuffle 알고리즘
+    public mutating func shuffle() {
+        for index in 0..<cardDeck.count-1 {
+            let randomIndex = Int.random(in: index..<cardDeck.count)
+            let tempCard = cardDeck[index]
+            cardDeck[index] = cardDeck[randomIndex]
+            cardDeck[randomIndex] = tempCard
+        }
+    }
+    
+    public mutating func popCard() -> Card? {
+        return cardDeck.popLast()
+    }
+ 
+    public mutating func reset() {
+        cardDeck.removeAll()
+        Shape.allCases.forEach{ shape in
+            Number.allCases.forEach{ number in
+                cardDeck.append(Card(shape: shape, number: number))
+            }
+        }
+    }
+}
