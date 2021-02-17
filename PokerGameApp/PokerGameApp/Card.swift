8df5c9d07243254d7107e56e815b0b80f098f890
Jisoo Kim <ghis22130@naver.com>
2021-02-17 09:35:49 +0900

    Rank, Suit 전역화

--- PokerGameApp/PokerGameApp/Card.swift
+++ PokerGameApp/PokerGameApp/Card.swift
@@ -7,39 +7,40 @@
 
 import Foundation
 
+enum Suit: Character, CustomStringConvertible {
+    var description: String { return "\(self.rawValue)"}
+    
+    case spades = "♠️", hearts = "❤️", diamonds = "♦️", clubs = "♣️"
+}
+
+enum Rank: Int, CustomStringConvertible{
+    var description: String { return "\(self.values)"}
+    
+    case A = 1, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, J, Q, K
+    
+    var values: String {
+        let rankNumber = self.rawValue
+        
+        switch rankNumber {
+        case 1:
+            return "A"
+        case 11:
+            return "J"
+        case 12:
+            return "Q"
+        case 13:
+            return "K"
+        default:
+            return "\(rankNumber)"
+        }
+    }
+}
+
 struct Card: CustomStringConvertible {
     var description: String {
         return "suit is \(suit), rank is \(rank)"
     }
     
-    enum Suit: Character, CustomStringConvertible {
-        var description: String { return "\(self.rawValue)"}
-        
-        case spades = "♠️", hearts = "❤️", diamonds = "♦️", clubs = "♣️"
-    }
-    
-    enum Rank: Int, CustomStringConvertible{
-        var description: String { return "\(self.values)"}
-        
-        case A = 1, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, J, Q, K
-        
-        var values: String {
-            let rankNumber = self.rawValue
-            
-            switch rankNumber {
-            case 1:
-                return "A"
-            case 11:
-                return "J"
-            case 12:
-                return "Q"
-            case 13:
-                return "K"
-            default:
-                return "\(rankNumber)"
-            }
-        }
-    }
     
     let rank: Rank
     let suit: Suit
---
+++ PokerGameApp/PokerGameApp/CardDeck.swift
@@ -0,0 +1,8 @@
+//
+//  CardDeck.swift
+//  PokerGameApp
+//
+//  Created by 지북 on 2021/02/16.
+//
+
+import Foundation
