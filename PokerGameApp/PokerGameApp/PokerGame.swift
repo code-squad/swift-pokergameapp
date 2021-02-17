acf51afc6faf36c1e444a87df6da562dfd0508e3
JUNGYUN-Daegu/JUNGYUN-Daegu/jungyun <tgms021@gmail.com>
2021-02-17 17:21:58 +0900

    Refactor code2

--- PokerGameApp/PokerGameApp/PokerGame.swift
+++ PokerGameApp/PokerGameApp/PokerGame.swift
@@ -8,12 +8,25 @@
 import Foundation
 
 class PockerGame {
+    public enum StudVariant {
+        case fiveCardStud
+        case sevenCardStud
+    }
+
     private var cardDeck = CardDeck()
     private var dealer = Dealer()
     private var players: Array<Player> = []
+    private var numberOfPlayers: Int
+    private var numberOfCards: Int
     private var lackOfCard: Bool = false
     
-    private func welcomePlayer(numberOfPlayers: Int) {
+    
+    public init(withPlayersOf: Int, stud: StudVariant) {
+        numberOfPlayers = withPlayersOf
+        numberOfCards = stud == .fiveCardStud ? 5 : 7
+    }
+    
+    private func welcomePlayer() {
         for i in 1...numberOfPlayers {
             players.append(Player(id: i))
         }
@@ -25,7 +38,7 @@ class PockerGame {
         print(cardDeck.count())
     }
     
-    private func distribute(numberOfCards: Int) {
+    private func distribute() {
         for _ in 1...numberOfCards {
             for player in players {
                 if let drawnCard = cardDeck.removeOne() {
@@ -43,9 +56,9 @@ class PockerGame {
     }
     
     public func gamePlay() {
-        welcomePlayer(numberOfPlayers: 4)
+        welcomePlayer()
         resetCardDeck()
-        distribute(numberOfCards: 7)
+        distribute()
         if lackOfCard == true {
             print("카드 수 부족")
             return
