6c5789ec744961315e5519e5d799e6b2b891600c
okstring <milkoi7801@gmail.com>
2021-02-17 23:28:14 +0900

    start, registerPlayer 메소드 위치 수정

--- PokerGameApp/PokerGameApp/Dealer.swift
+++ PokerGameApp/PokerGameApp/Dealer.swift
@@ -23,4 +23,22 @@ class Dealer: PokerGame, Playable {
     func appendCard(_ card: Card) {
         cards.append(card)
     }
+    
+    public func start(numberOfPlayer: Participant, stud: CardStud) {
+        players = registerPlayer(of: numberOfPlayer)
+        shareCards(players: players, cardStud: stud)
+    }
+    
+    private func registerPlayer(of number: Participant) -> [Playable] {
+        var players = [Playable]()
+        
+        let playerCount = number.rawValue
+        for _ in 1...playerCount {
+            let player = Player()
+            players.append(player)
+        }
+        players.append(self)
+        
+        return players
+    }
 }
