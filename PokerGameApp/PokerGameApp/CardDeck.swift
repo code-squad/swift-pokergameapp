01819dfe340ac99272a0b4d1f2ae23671458c035
1song2 <1song2918@gmail.com>
2021-02-17 10:53:59 +0900

    Create method 'removeOne()'

--- PokerGameApp/PokerGameApp/CardDeck.swift
+++ PokerGameApp/PokerGameApp/CardDeck.swift
@@ -23,6 +23,16 @@ struct CardDeck {
         cards.shuffle()
     }
     
+    mutating func removeOne() -> Card? {
+        let numberOfCards = count()
+        if numberOfCards == 0 {
+            return nil
+        } else {
+            let randomNumber = Int.random(in: 0..<numberOfCards)
+            return cards.remove(at: randomNumber)
+        }
+    }
+    
     private func getAllCards(for symbol: Card.Suit) -> [Card] {
         return Card.Rank.allCases.map { Card(suit: symbol, rank: $0) }
     }
