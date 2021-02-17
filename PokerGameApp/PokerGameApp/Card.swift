2cbecd4c4c8b4b45a4cfb177299897e76718b3db
Song Lee <songlish233@gmail.com>
2021-02-17 10:34:52 +0900

    Step 3 내용 리팩토링
    - Card: Suit, Rank enum에 CustomStringConvertible 적용 / Rank case 명 변경
    - CardDeck: 메소드 위치, 줄바꿈 수정

--- PokerGameApp/PokerGameApp/Card.swift
+++ PokerGameApp/PokerGameApp/Card.swift
@@ -7,53 +7,53 @@
 
 import Foundation
 
-class Card {
+class Card: CustomStringConvertible {
     
     /// 4가지 경우 중 한가지를 선택한다는 개념을 나타내기에 적절한 enum 타입으로 생성
-    /// init 시 가독성을 위해 영문으로 case 이름 생성, 출력을 위한 유니코드를 rawValue로 설정
     let suit: Suit
     
-    enum Suit: String, CaseIterable {
-        case spade = "\u{2660}\u{FE0F}"
-        case club = "\u{2663}\u{FE0F}"
-        case heart = "\u{2665}\u{FE0F}"
-        case diamond = "\u{2666}\u{FE0F}"
+    enum Suit: String, CaseIterable, CustomStringConvertible {
+        case spade = "♠️"
+        case club = "♣️"
+        case heart = "♥️"
+        case diamond = "♦️"
+        
+        var description: String {
+            return rawValue
+        }
     }
     
-    /// 1~13까지만 설정 가능하도록 enum 통해 초기화하되,
-    /// 내부 rank는 계산 편의성 위해 Int로 저장, 프린트용 변환은 개별 메소드로 구현
-    let rank: Int
+    /// 1~13까지만 설정 가능하도록 enum 타입으로 생성
+    /// 추후 연산 편리성 위해 rawValue 타입은 Int로 설정
+    let rank: Rank
     
-    enum Rank: Int, CaseIterable {
-        case rank1 = 1,
-             rank2, rank3, rank4, rank5, rank6, rank7,
-             rank8, rank9, rank10, rank11, rank12, rank13
+    enum Rank: Int, CaseIterable, CustomStringConvertible {
+        case one = 1,
+             two, three, four, five, six, seven,
+             eight, nine, ten, eleven, twelve, thirteen
+        
+        var description: String {
+            switch rawValue {
+            case 1:
+                return "A"
+            case 11:
+                return "J"
+            case 12:
+                return "Q"
+            case 13:
+                return "K"
+            default:
+                return String(rawValue)
+            }
+        }
     }
     
     init(suit: Suit, rank: Rank) {
         self.suit = suit
-        self.rank = rank.rawValue
-    }
-}
-
-//MARK: - 출력 관련
-extension Card: CustomStringConvertible {
-    var description: String {
-        return "\(suit.rawValue)\(printableRank(from: rank))"
+        self.rank = rank
     }
     
-    private func printableRank(from rank: Int) -> String {
-        switch rank {
-        case 1:
-            return "A"
-        case 11:
-            return "J"
-        case 12:
-            return "Q"
-        case 13:
-            return "K"
-        default:
-            return String(rank)
-        }
+    var description: String {
+        return "\(suit)\(rank)"
     }
 }
