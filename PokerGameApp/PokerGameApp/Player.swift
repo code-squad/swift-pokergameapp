59f1e8a1d7475166a663b81892e07d375f1508c7
Ray <readingod@naver.com>
2021-02-17 15:43:05 +0900

    플레이어 클래스 프로퍼티 및 메서드 추가
    딜러와 마찬가지로 플레이어가 들고있을 카드를 저장하는 프로퍼티 생성 후에 딜러가 나눠주는 카드를 받는 메서드 구현하였습니다.

---
+++ PokerGameApp/PokerGameApp/Player.swift
@@ -0,0 +1,24 @@
+import Foundation
+
+class Player {
+    private var handDeck:[Card]
+    
+    init() {
+        self.handDeck = [Card]()
+    }
+    
+    private func recieveCard(card: Card) {
+        self.handDeck.append(card)
+    }
+}
+
+/*
+ extension을 사용하면 class 내부에 cumtomstringconvertible 프로토콜 쓰는 것보다 가독성에서 이점이 있을 것 같았는데
+ 가독성 측면에서 크게 이점이 있는지 모르겠음 => 여러 프로토콜을 채택할 경우에는 다를 수도 있을 것 같은데...
+*/
+
+extension Player:CustomStringConvertible {
+    var description: String {
+        return "\(self.handDeck)"
+    }
+}
