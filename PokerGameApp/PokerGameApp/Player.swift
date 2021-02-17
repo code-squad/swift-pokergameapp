458b25979e0f04a54b10e35955597edce9d46c8e
Jisoo Kim <ghis22130@naver.com>
2021-02-17 15:29:24 +0900

    Player Class 구현

--- PokerGameApp/PokerGameApp/Player.swift
+++ PokerGameApp/PokerGameApp/Player.swift
@@ -5,4 +5,23 @@
 //  Created by 지북 on 2021/02/17.
 //
 
+
 import Foundation
+
+//protocol ReceiveCardDelegate {
+//    mutating func receiveCards(with rCards:[Card])
+//}
+
+class Player : CustomStringConvertible {
+
+    var description: String { return "\(cards)" }
+    
+    
+    private var cards: [Card] = []
+    
+    func receiveCards(with rCards: [Card]) {
+        self.cards = rCards
+    }
+    
+ 
+}
