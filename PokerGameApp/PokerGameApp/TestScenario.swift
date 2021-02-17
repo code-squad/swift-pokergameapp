065eaf92ec0d7843f685315b504d5c6570097f8e
shimyoungmin <dudals6965@gmail.com>
2021-02-17 21:21:06 +0900

    Dealer, Participant, PockerGame 모두 CustomStringConvertible 채택하여 줌 & 테스트 시나리오 구현

--- PokerGameApp/PokerGameApp/TestScenario.swift
+++ PokerGameApp/PokerGameApp/TestScenario.swift
@@ -40,4 +40,14 @@ func testScenario() {
     } else {
         print("남아있는 카드가 없습니다.")
     }
+    
+    var game = PockerGame(numOfParticipant: 3, numOfCard: 5)
+    game.distributeCard()
+    print("5카드 기준, 참가자 3명 일 때\n")
+    print(game)
+    
+    game = PockerGame(numOfParticipant: 4, numOfCard: 7)
+    game.distributeCard()
+    print("7카드 기준, 참가자 4명 일 때\n")
+    print(game)
 }
