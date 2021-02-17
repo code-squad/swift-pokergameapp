b39c498dcca5f02a848fea3f4321303b21ddebf0
Song Lee <songlish233@gmail.com>
2021-02-18 01:25:58 +0900

    프린트 방식 개선을 위한 수정
    - InfoBoard class를 따로 생성하여 현 상황을 업데이트 받도록 함
    - String 외의 출력 방식도 가능하도록 설계

---
+++ PokerGameApp/PokerGameApp/InfoBoard.swift
@@ -0,0 +1,34 @@
+//
+//  InfoBoard.swift
+//  PokerGameApp
+//
+//  Created by Song on 2021/02/18.
+//
+
+import Foundation
+
+class InfoBoard: CustomStringConvertible {
+    var isGameFinished = false
+    var cardCount: Int?
+    var participantsList: [String]?
+    var cardStackList: [[Card]]?
+    
+    var description: String {
+        guard !isGameFinished else {
+            return "덱에 카드가 모자라서 게임을 종료합니다.\n"
+        }
+        
+        var gameStatus = ""
+        
+        if let pList = participantsList, let cList = cardStackList,
+           pList.count == cList.count, let count = cardCount {
+            for i in 0..<pList.count {
+                gameStatus += "\(pList[i]): \(cList[i])\n"
+            }
+            gameStatus += "남은 카드는 \(count)장입니다.\n"
+        } else {
+            gameStatus = "게임 세팅이 잘못되었습니다!\n"
+        }
+        return gameStatus
+    }
+}
