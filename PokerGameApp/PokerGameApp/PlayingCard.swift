a0e5f9c658960b98e08410cb594c11fb77c775e9
dahun-lee-daji <ssss6596@gmail.com>
2021-02-17 16:02:11 +0900

    step2 요구사항 구현
    class PlayingCard 구현
    console에 카드 내용 표시 -> random 1, heartQ, spade7

---
+++ PokerGameApp/PokerGameApp/PlayingCard.swift
@@ -0,0 +1,39 @@
+//
+//  PlayingCard.swift
+//  PokerGameApp
+//
+//  Created by 이다훈 on 2021/02/17.
+//
+
+import Foundation
+
+class PlayingCard {
+    
+    ///enum을 사용한 이유는, 사용되는 문양의 case가 적으며, 해당 case외에는 없고, CaseIterable을 이용하여 random한 값을 가져올 수 있기 때문입니다.
+    enum Suits : String, CaseIterable {
+        case spade = "\u{2660}"
+        case heart = "\u{2665}"
+        case diamond = "\u{2666}"
+        case club = "\u{2663}"
+    }
+    
+    /// 카드의 숫자 또한 enum : CaseIterable 을 사용하려고 했지만, 각 case를 명명하는 것은 효율성이 떨어져 보였기 때문에, Dictionary를 택하였습니다. 범위 밖의 숫자를 입력 시  optional chaning을 이용하여 해결 할 수 있으므로 좋은 방법이라 보았습니다.
+    private let rank : Dictionary<Int,String> = [1 : "A", 2 : "2", 3 : "3", 4 : "4", 5 : "5", 6 : "6", 7 : "7", 8 : "8", 9 : "9", 10 : "10", 11 : "J", 12 : "Q", 13 : "K"]
+    
+    let suit : Suits
+    let number : Int
+    
+    init() {
+        self.suit = Suits.allCases.randomElement()!
+        self.number = Int.random(in: 1...13)
+    }
+    
+    init(suit : Suits, number : Int) {
+        self.suit = suit
+        self.number = number
+    }
+    
+    func showCard() {
+        print(suit.rawValue, rank[number] ?? "nil", separator : "" )
+    }
+}
