f047398b5ce8c0f6d09d51d8399ab465180ae972
zombietux <unknown>
2021-02-18 00:03:01 +0900

    suit enum 수정

--- PokerGameApp/PokerGameApp/Card.swift
+++ PokerGameApp/PokerGameApp/Card.swift
@@ -12,11 +12,20 @@ struct Card: CustomStringConvertible, Equatable {
      Suit 타입은 enum으로 생성.
      CardValue와 달리 다양한 초기화가 불필요하고 String에 맞게 유니코드만 반환해주면 된다고 판단.
      */
-    enum Suit: Character, CaseIterable {
-        case hearts = "❤️"
-        case spades = "♠️"
-        case diamonds = "♦️"
-        case clubs = "♣️"
+    enum Suit: Character, CustomStringConvertible, CaseIterable {
+        case hearts = "h"
+        case spades = "s"
+        case diamonds = "d"
+        case clubs = "c"
+        
+        var description: String {
+            switch self {
+            case .hearts: return "❤️"
+            case .spades: return "♠️"
+            case .diamonds: return "♦️"
+            case .clubs: return "♣️"
+            }
+        }
     }
     
     let rank: Rank
