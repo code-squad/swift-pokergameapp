0417f4b5717f8134daadf54d52d601740deaa25c
Ray <readingod@naver.com>
2021-02-17 15:20:13 +0900

    PokerGame 클래스 생성 딜러, 플레이어, 스터드, 참가자를 변수로 생성하고 enum을 활용하여 스터드와 참가자 열거형으로 만들었습니다.

---
+++ PokerGameApp/PokerGameApp/PokerGame.swift
@@ -0,0 +1,24 @@
+import Foundation
+
+class PokerGame {
+    private var dealer: Dealer
+    private var player: Players
+    private var stud: CardStud
+    private var participants: Participants
+    
+    init(stud: CardStud, participants: Participants) {
+        self.dealer = Dealer()
+        self.player = Players()
+        self.stud = .seven
+        self.participants = .four
+    }
+    
+    enum CardStud:Int {
+        case seven = 7
+        case five = 5
+    }
+    
+    enum Participants: Int {
+        case one = 1, two, three, four
+    }
+}
