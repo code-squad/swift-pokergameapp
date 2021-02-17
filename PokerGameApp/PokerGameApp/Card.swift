6464616258bf9e42492a80ea62fabe332bf55c9d
HongzCloud <jihong21511@gmail.com>
2021-02-17 17:23:00 +0900

    ♻️  Adopt protocol for enum
    enum에도 CustomStringConvertible 프로토콜을 채택하였습니다.

--- PokerGameApp/PokerGameApp/Card.swift
+++ PokerGameApp/PokerGameApp/Card.swift
@@ -8,16 +8,18 @@
 import Foundation
 
 //지정된 범위 안의 값만을 사용해야 하기 때문에 enum을 사용, 예를들면 다른 ☞모양은 사용X
-enum Shape: Character {
+enum Shape: Character, CustomStringConvertible{
     case spade = "♠️"
     case clover = "♣️"
     case diamond = "♦️"
     case heart = "♥️"
+    
+    var description: String { "\(rawValue)" }
 }
 
 //지정된 범위 안의 값만을 사용해야 하기 때문에 enum을 사용, 예를들면 14라는 숫자는 사용X
-enum Number: Int {
-    case A = 1
+enum Number: Int, CustomStringConvertible {
+    case ace = 1
     case two
     case three
     case four
@@ -27,28 +29,33 @@ enum Number: Int {
     case eight
     case nine
     case ten
-    case J
-    case Q
-    case K
+    case jack
+    case queen
+    case king
+    
+    var description: String {
+        switch self {
+        case .ace:
+            return "A"
+        case .jack:
+            return "J"
+        case .king:
+            return "K"
+        case .queen:
+            return "Q"
+        default:
+            return "\(rawValue)"
+        }
+    }
 }
 
-class Card {
+class Card: CustomStringConvertible{
     let shape: Shape
     let number: Number
+    var description: String { "\(shape)\(number)" }
     
     init(shape: Shape, number: Number) {
         self.shape = shape
         self.number = number
-    }
-}
-
-extension Card: CustomStringConvertible {
-    var description: String {
-        switch number.rawValue {
-        case 1,11,12,13:
-            return "\(shape.rawValue)\(number)"
-        default:
-            return "\(shape.rawValue)\(number.rawValue)"
-        }
     }
 }
