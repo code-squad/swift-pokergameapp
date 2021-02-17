bdbd1bf4de3bbd0503604c05beb8d1bf39c256c5
1song2 <1song2918@gmail.com>
2021-02-17 22:05:29 +0900

    Add nested enum 'GameRule'

--- PokerGameApp/PokerGameApp/PokerGame.swift
+++ PokerGameApp/PokerGameApp/PokerGame.swift
@@ -9,4 +9,17 @@ import Foundation
 
 class PokerGame {
     
+    enum GameRule {
+        case minPlayer
+        case maxPlayer
+        
+        func getNumber() -> Int {
+            switch self {
+            case .minPlayer:
+                return 1
+            case .maxPlayer:
+                return 4
+            }
+        }
+    }
 }
