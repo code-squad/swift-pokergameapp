a1bba481bdbd01b7f117b6595fdd7311b315dfda
shimyoungmin <dudals6965@gmail.com>
2021-02-17 15:08:21 +0900

    CardDeck 클래스 removeOn 함수 수정 popLast의 경우 배열이 비어있으면 nil 을 return 하므로 기존코드는 불필요하다 생각하여 바로 return 해주는 함수로 수정

--- PokerGameApp/PokerGameApp/CardDeck.swift
+++ PokerGameApp/PokerGameApp/CardDeck.swift
@@ -29,7 +29,8 @@ struct CardDeck {
     }
     
     mutating func removeOn() -> Card?{
-        return cards.count == 0 ? nil : self.cards.popLast()
+        //return cards.count == 0 ? nil : self.cards.popLast()
+        return self.cards.popLast()
     }
     
     mutating func reset() {
