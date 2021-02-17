d608a50b004494f00c218b85a5251d586f8dbfa8
ellyheetov <yaaajcm@naver.com>
2021-02-17 13:30:08 +0900

    Refector PokerGame
    use higher order function
    change variable scope

--- PokerGameApp/PokerGame.swift
+++ PokerGameApp/PokerGame.swift
@@ -11,31 +11,28 @@ class PokerGame {
     
     enum Constant {
         static let numberOfPlayers = 3
-        static let cardstud = 5
+        static let cardstud = 7
     }
     
     var gameCardDeck = PlayingCardDeck()
+    var players = Players(totalPlayers: Constant.numberOfPlayers)
+    var dealer = Dealer()
 
     public func start(){
-        gameCardDeck.reset()
-        gameCardDeck.shuffle()
         printRule()
         
-        var players = Players(totalPlayers: Constant.numberOfPlayers)
-        var dealer = Dealer()
+        gameCardDeck.reset()
+        gameCardDeck.shuffle()
+    
+        players.player.forEach{spreadCardsToPerson(person:$0)}
+        spreadCardsToPerson(person: dealer)
         
-        spreadCardsToPlayers(players: &players, dealer: &dealer)
-        printResult(players: &players, dealer: &dealer)
+        printResult()
     }
 
-    private func spreadCardsToPlayers(players : inout Players, dealer : inout Dealer){
-    
-        for player in players.player {
-            let cards = gameCardDeck.removeCards(count: Constant.cardstud)
-            player.cards.takeCards(cards: cards)
-        }
+    private func spreadCardsToPerson(person : Dealer){
         let cards = gameCardDeck.removeCards(count: Constant.cardstud)
-        dealer.cards.takeCards(cards: cards)
+        person.cards.takeCards(cards: cards)
     }
     
     private func printRule(){
@@ -43,10 +40,10 @@ class PokerGame {
         print("\(Constant.cardstud)카드 기준")
     }
     
-    private func printResult(players : inout Players, dealer : inout Dealer){
-        players.player.map{print("참가자#\($0.id) \($0.cards)")}
-        print("딜러 \(dealer.cards)")
+    private func printResult(){
         
+        players.player.forEach{print("참가자#\($0.id) \($0.cards)")}
+        print("딜러 \(dealer.cards)")
         print("남아있는 카드의 수 \(gameCardDeck.count)")
     }
 }
