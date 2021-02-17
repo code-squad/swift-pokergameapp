ae1e66010b57c931658567059fb23192e58cf9ae
jhpark-steve <park9368@naver.com>
2021-02-17 15:42:22 +0900

    CustomString, 옵셔널 바인딩 등을 활용하여 수정 사항 반영
    주석 지우기, 유니코드 표현, customstringconvertible 활용, 옵셔널 바인딩, 상수 선언, 반복 생성 문제 개선했습니다

--- PokerGameApp/PokerGameApp/Card.swift
+++ PokerGameApp/PokerGameApp/Card.swift
@@ -1,34 +1,33 @@
-//
-//  Card.swift
-//  PokerGameApp
-//
-//  Created by 박정하 on 2021/02/16.
-//
-
 import Foundation
 
 
 class Card{
-    enum suit : String { //모양과 숫자에 enum을 선택한 이유는 유니코드, 숫자(문자열) 등이 다른 값과 혼동하기 쉬울 것 같아서 enum으로 선언하면 좀 더 명확한 표현으로 접근할 수 있으니까 입니다.
-        case spade = "\u{2660}", club = "\u{2663}", heart = "\u{2665}", diamond = "\u{2666}"
+    let suit_Value : Suit? = .spade
+    let num_Value : Number? = .Jack
+    
+    enum Suit : String { //모양과 숫자에 enum을 선택한 이유는 유니코드, 숫자(문자열) 등이 다른 값과 혼동하기 쉬울 것 같아서 enum으로 선언하면 좀 더 명확한 표현으로 접근할 수 있으니까 입니다.
+        case spade = "♠︎", club = "♣︎", heart = "♥︎", diamond = "♦︎"
         var suitValue : String {
             return self.rawValue
         }
     }
     
-    enum number : String{ //Nested enum이 중첩 enum문으로 알고 있는데, 10미만인 숫자 enum 안에 10 이상의 숫자 enum을 중첩해서 처리하려고 하니까 접근 할때 오히려 enum.enum으로 접근할때 따로따로 해야 해서 한 enum에 모두 선언했습니다.
+    enum Number : String{ //Nested enum이 중첩 enum문으로 알고 있는데, 10미만인 숫자 enum 안에 10 이상의 숫자 enum을 중첩해서 처리하려고 하니까 접근 할때 오히려 enum.enum으로 접근할때 따로따로 해야 해서 한 enum에 모두 선언했습니다.
         case two = "2", three = "3", four = "4", five = "5", six = "6", seven = "7", eight = "8", nine = "9" // 출력할 때 += 으로 간단히 연산하기 위하여 타입을 맞추다 보니까 rawValue를 하나하나 선언 했는데 상당히 비효율적인것 같습니다.
         case Jack = "J", Queen = "Q", King = "K", Ace = "A"
-        var numvalue: number {
-            return Card.number(rawValue: self.rawValue)!
+        var numvalue: Number {
+            return Card.Number(rawValue: self.rawValue)!
         }
     }
-    
-    func returnValue() -> String{
-        let suit : suit = .spade, number : number = .Queen
-        var returnString : String = ""
-        returnString += suit.suitValue
-        returnString += number.numvalue.rawValue
-        return returnString
+}
+
+extension Card : CustomStringConvertible{
+    var description: String {
+        if let suitTemp = suit_Value, let numTemp = num_Value {
+            return "\(suitTemp.rawValue)\(numTemp.rawValue)"
+        }
+        else{
+            return "Wrong Data"
+        }
     }
 }
--- PokerGameApp/PokerGameApp/ViewController.swift
+++ PokerGameApp/PokerGameApp/ViewController.swift
@@ -17,21 +17,29 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         initGameplay()
         let card = Card()
-        print(card.returnValue())
+        print(card.description)
     }
     
     func initGameplay(){
         let pattern : UIImage = UIImage(named: "bg_pattern")!
         self.view.backgroundColor = UIColor(patternImage: pattern)
         var CardViewer : [UIImageView] = []
-        let cardXpos : CGFloat = self.view.frame.size.width/7
-        let offset : CGFloat = CGFloat(self.view.frame.size.width/2) - CGFloat(self.view.frame.width/7 * 4) + CGFloat((self.view.frame.width/8)/2)
-        let cardWidth : CGFloat = self.view.frame.size.width/8
-        let cardHeight : CGFloat = cardWidth * 1.27
+        let CARDNUMBER : CGFloat = 7
+        let CARDHEIGHTRATIO : CGFloat = 1.27
+        let cardXpos : CGFloat = self.view.frame.size.width / CARDNUMBER
+        let offset : CGFloat = CGFloat(self.view.frame.size.width/2) - CGFloat(self.view.frame.width/7 * 4) + CGFloat((self.view.frame.width/(CARDNUMBER+1))/2)
+        let cardWidth : CGFloat = self.view.frame.size.width / (CARDNUMBER + 1)
+        let cardHeight : CGFloat = cardWidth * CARDHEIGHTRATIO
+        let cardBack : String? = "card-back"
+        var cardBackImage : UIImage? = nil
+        
+        if let cardname = cardBack{
+            cardBackImage = UIImage(named: cardname)
+        }
         
         for i in 0..<7{
             let j : CGFloat = CGFloat(i)
-            CardViewer.append(UIImageView(image: UIImage(named: "card-back")))
+            CardViewer.append(UIImageView(image: cardBackImage))
             CardViewer[i].frame = CGRect(x: cardXpos * j - offset, y: 50, width: cardWidth , height: cardHeight)
             self.view.addSubview(CardViewer[i])
         }
