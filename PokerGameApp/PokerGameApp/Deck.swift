cd5c6a6b2e5a122a7ab2e33e446e7b4e35058ddb
jhpark-steve <park9368@naver.com>
2021-02-18 22:41:54 +0900

    딜러의 역할 수정
    카드를 나눠주는 것이 PokerGame내에서가 아닌 딜러가 해주는 것이 자연스럽다 판단하여 카드를 나눠주는 함수를 딜러 쪽으로 모두 변경함.

--- PokerGameApp/PokerGameApp/Deck.swift
+++ PokerGameApp/PokerGameApp/Deck.swift
@@ -58,10 +58,18 @@ class Deck{
         }
     } // 이 부분 shuffle 시작 하는 부분에 옵셔널 바인딩으로 if var deckTemp = deck {} 으로 진행하려 했으나 덱을 옵셔널로 두고 nil을 부여하면 createDeck을 실행해도 덱이 초기화 되질 않습니다. 이런 경우 옵셔널 바인딩 보단 그냥 덱의 count가 0일 경우로 처리하는 것이 더 권장되는 방향일까요?
     
-    func takeOnecard(){
+    func takeRandomcard(){
         let randomIndex = Int.random(in: 0..<deck.count)
         
         print(deck[randomIndex].description)
         deck.remove(at: randomIndex)
+    }
+    
+    func takeTopcard() -> Card{
+        return deck[0]
+    }
+    
+    func removeTopCard(){
+        deck.removeFirst()
     }
 }
