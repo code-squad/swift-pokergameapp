c130690fdedebc19b2ab3a7d7546a69cd5a01314
shimyoungmin <dudals6965@gmail.com>
2021-02-18 15:27:09 +0900

    Dealer 클래스에 distributeCard() 함수구현 & Participants 클래스에 receiveCard 함수 구현

--- PokerGameApp/PokerGameApp/Dealer.swift
+++ PokerGameApp/PokerGameApp/Dealer.swift
@@ -8,11 +8,33 @@
 import Foundation
 
 class Dealer: Player {
+    private var cardDeck = CardDeck()
 
     override init(name: PlayerType) {
         super.init(name: name)
     }
-
+    // 카드를 나눠주는 함수 -> 그럼 매개변수로 participants 를 받아야겠지 ?
+    func distributeCard(participants: Participants, numOfCard: Int) {
+        cardDeck.reset()
+        cardDeck.shuffle()
+        
+        for _ in 1...numOfCard {
+            if let selectedCard = cardDeck.removeOn() {
+                self.receiveCard(selectedCard)
+            } else {
+                return
+            }
+            
+            for _ in 0..<participants.count {
+                if let selectedCard = cardDeck.removeOn() {
+                    participants.receiveCard(selectedCard)
+                } else {
+                    return
+                }
+            }
+        }
+        
+    }
 }
 //extension Dealer: CustomStringConvertible {
 //    var description: String {
