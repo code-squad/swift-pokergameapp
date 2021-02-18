63db76aae8b2ae00fad63821fb94102a339bfe84
zzisun <jisunkim98@gmail.com>
2021-02-18 12:50:01 +0900

    step3. 카드덱 구조체를 구현

--- PokerGameApp/PokerGameApp/CardDeck.swift
+++ PokerGameApp/PokerGameApp/CardDeck.swift
@@ -4,11 +4,34 @@
 //
 //  Created by 김지선 on 2021/02/17.
 //
-
 struct CardDeck {
     private var cards = [Card]()
     
+    mutating func setCard() {
+        for suit in Card.Suit.allCases {
+            for rank in Card.Rank.allCases {
+                self.cards.append(Card(suit: suit, rank: rank))
+            }
+        }
+    }
+    
     mutating func count() -> Int {
         return self.cards.count
+    }
+    
+    mutating func shuffle() {
+        for i in 0..<self.count() {
+            let randomIndex = Int.random(in: i..<self.count())
+            self.cards.swapAt(i, randomIndex)
+        }
+    }
+    
+    mutating func reset() {
+        self.cards = []
+        setCard()
+    }
+    
+    mutating func removeOne() -> Card? {
+        return self.cards.popLast()
     }
 }
