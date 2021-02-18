2723ec63d708a8a61f8ad9777c821749c20052c8
Jun Ho JANG <junhojang@Junui-MacBookPro.local>
2021-02-18 16:53:16 +0900

    덱의 생성과 기타 기능만으로 구성되도록 했습니다.

---
+++ PokerGameApp/PokerGameApp/Players.swift
@@ -0,0 +1,20 @@
+//
+//  Participants.swift
+//  PokerGameApp
+//
+//  Created by Jun Ho JANG on 2021/02/18.
+//
+
+import Foundation
+
+class Participants {
+    
+    private var name: String
+    private var hand: [String]
+    
+    init(name: String, hand: [String]) {
+        self.name = name
+        self.hand = hand
+    }
+    
+}
