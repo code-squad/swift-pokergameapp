9fd465775afc6039f5d5109b4d6e0e41e2db0400
shimyoungmin <dudals6965@gmail.com>
2021-02-18 15:52:24 +0900

    포커게임 클래스 수정 (dealer 클래스로 distributeCard 함수 위임, participant 배열을 프로퍼티로 선언했던 것을 participants 타입(클래스) 를 만들어 변경:

--- PokerGameApp/PokerGameApp/PockerGame.swift
+++ PokerGameApp/PokerGameApp/PockerGame.swift
@@ -10,52 +10,17 @@ import Foundation
 class PockerGame {
     private var deck: CardDeck
     private var dealer: Dealer
-    private var participants: [Participant] = []
-    private var numOfParticipant: Int
+    private var participants: Participants
     private var numOfCard: Int
     
     init(numOfParticipant: Int, numOfCard: Int) {
-        self.numOfParticipant = numOfParticipant
         self.numOfCard = numOfCard
         deck = CardDeck()
-        dealer = Dealer()
-        //participants = Array(repeating: Participant(), count: self.numOfParticipant)
-        for _ in 1...numOfParticipant {
-            let participant = Participant()
-            self.participants.append(participant)
-        }
+        dealer = Dealer(name: PlayerType.Dealer)
+        participants = Participants(numOfParticipant: numOfParticipant)
     }
     
-    func distributeCard() {
-        deck.shuffle()
-        for _ in 1...numOfCard {
-            if let selectedCard = deck.removeOn() {
-                // 딜러에게 한장나눠준다.
-                dealer.receiveCard(selectedCard)
-            } else {
-                print("카드가 다 소모되었습니다.\n 게임을 종료합니다.")
-                return
-            }
-
-            participants.forEach({ participant in
-                if let selectedCard = deck.removeOn() {
-                    participant.receiveCard(selectedCard)
-                } else {
-                    print("카드가 다 소모되었습니다.\n 게임을 종료합니다.")
-                    return
-                }
-            })
-        }
-    }
-}
-extension PockerGame: CustomStringConvertible {
-    var description: String {
-        var str = ""
-        for num in 1...participants.count {
-            str.append("참가자#\(num) \(participants[num-1])")
-        }
-
-        str.append("딜러 \(dealer)")
-        return str
+    func gameStart() {
+        dealer.distributeCard(participants: participants, numOfCard: self.numOfCard)
     }
 }
