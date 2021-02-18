cc5647e099d09e53cae30c0f3627902d1823fc5e
Jun Ho JANG <junhojang@Junui-MacBookPro.local>
2021-02-18 22:02:26 +0900

    willJoinGame 배열을 public에서 private으로 변경했습니다. Dealer 클래스에 핸드를 제출하기 위한 함수를 추가했습니다.

--- PokerGameApp/PokerGameApp/Player.swift
+++ PokerGameApp/PokerGameApp/Player.swift
@@ -20,16 +20,26 @@ class Player {
 
 class Players {
     
+<<<<<<< HEAD
     private var willJoinGame: Array<Player> = []
     public var hands = [[String]]()
     
     public func participatePlayer(num: Int) {
         for i in 1...num {
+=======
+    public var playerNum = 1
+    
+    public var willJoinGame: Array<Player> = []
+    
+    public func participatePlayer() {
+        for i in 1...playerNum {
+>>>>>>> 1983ea1b... dealer에 있었던 recruit를 participate로 받았습니다. 이니셜라이저에서 핸드를 제외했습니다.
             willJoinGame.append(Player(name: "Player\(i)"))
         }
         willJoinGame.append(Player(name: "Dealer"))
     }
     
+<<<<<<< HEAD
     public func dealerIndex() -> Int {
         return willJoinGame.endIndex-1
     }
@@ -50,4 +60,6 @@ class Players {
         }
     }
  
+=======
+>>>>>>> 1983ea1b... dealer에 있었던 recruit를 participate로 받았습니다. 이니셜라이저에서 핸드를 제외했습니다.
 }
