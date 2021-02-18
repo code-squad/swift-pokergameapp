f70cb8ec882aa6d782e0e56d17629156d45c8467
shimyoungmin <dudals6965@gmail.com>
2021-02-18 15:27:09 +0900

    Dealer 클래스에 distributeCard() 함수구현 & Participants 클래스에 receiveCard 함수 구현

--- PokerGameApp/PokerGameApp/Participants.swift
+++ PokerGameApp/PokerGameApp/Participants.swift
@@ -9,11 +9,23 @@ import Foundation
 
 class Participants {
     private var participants: [Participant] = []
-    
+    // 현재 카드를 받을 participant 를 나타낸다. (카드를 받을 차례)
+    private var currentIndex: Int
+
     init(numOfParticipant: Int) {
+        currentIndex = 0
         for _ in 1...numOfParticipant {
             let participant = Participant(name: PlayerType.Pariticipant)
             participants.append(participant)
         }
+        
+    }
+    var count: Int {
+        return self.participants.count
+    }
+    func receiveCard(_ card: Card) {
+        participants[currentIndex].receiveCard(card)
+        // 카드를 받고난 후 다음차례에 선수가 받을 수 있도록 index를 증가시켜준다. (단, 마지막 차례였을 경우 다시 처음 플레이어에게 순서를 넘겨준다.)
+        currentIndex = currentIndex == participants.count-1 ? 0 : currentIndex+1
     }
 }
