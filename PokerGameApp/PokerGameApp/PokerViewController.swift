7ee0399b454cbb2fbb0f65486b3a4ab2052e1cbb
MagnaPax <chunhee.han@outlook.com>
2021-02-18 16:35:12 +0900

    feat : 이미지 나오게 하기

--- PokerGameApp/PokerGameApp/PokerViewController.swift
+++ PokerGameApp/PokerGameApp/PokerViewController.swift
@@ -8,10 +8,44 @@
 import UIKit
 
 class PokerViewController: UIViewController {
+    
+    let cardBackName = "card-back.png"
+
+    
+    let cardBackView: UIImageView = {
+//        let theImageView = UIImageView()
+//        theImageView.image =
+//        let image = UIImage(named: "card-back.png")
+        let cardBackView = UIImageView(image: UIImage(named: "card-back.png"))
+//        cardBackView.translatesAutoresizingMaskIntoConstraints = false
+        return cardBackView
+    }()
+    
+    
+    
+    func createCardView(fileName: String) -> UIImageView {
+        
+        // UIImage
+        let image = UIImage(named: fileName)
+        let imageView = UIImageView(image: image)
+        
+        //
+//        imageView.frame = CGRect(x: 0, y: 0, width: 100, height: 200)
+//        view.addSubview(imageView)
+        
+        return imageView
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor(patternImage: UIImage(named: "bg_pattern.png")!)
+        
+//        let imageView = createCardView(fileName: cardBackName)
+//        imageView.frame = CGRect(x: 0, y: 0, width: 100, height: 200)
+//        view.addSubview(imageView)
+        
+        view.addSubview(cardBackView)
     }
     
     override var preferredStatusBarStyle: UIStatusBarStyle {
@@ -21,7 +55,5 @@ class PokerViewController: UIViewController {
     override var prefersStatusBarHidden: Bool {
         return false
     }
-
-
 }
 
