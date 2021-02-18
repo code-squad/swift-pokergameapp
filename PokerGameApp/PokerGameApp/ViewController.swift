05e27d5b79ea0a5a4e33dcfc682d84ee9e38cf3f
hoonha choi <sosqsz2@naver.com>
2021-02-18 16:48:29 +0900

    게임 UI stackview, label 생성 및 cardStackView로이름변경

--- PokerGameApp/PokerGameApp/ViewController.swift
+++ PokerGameApp/PokerGameApp/ViewController.swift
@@ -9,7 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let horizontalStackView : UIStackView  = {
+    let pockerGameStackView : UIStackView = {
+        let stack = UIStackView()
+        stack.axis = .vertical
+        stack.distribution = .fill
+        stack.alignment = .fill
+        stack.spacing = 10
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        return stack
+    }()
+    
+    let playerStackView : UIStackView = {
+        let stack = UIStackView()
+        stack.axis = .vertical
+        stack.distribution = .fill
+        stack.alignment = .fill
+        stack.spacing = 10
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        return stack
+    }()
+    
+    let playerName : UILabel = {
+        let label = UILabel()
+        label.textColor = .white
+        label.font = .boldSystemFont(ofSize: 20)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let cardStackView : UIStackView  = {
        let stack = UIStackView()
         stack.axis = .horizontal
         stack.distribution = .fillEqually
@@ -26,14 +54,11 @@ class ViewController: UIViewController {
         return image
     }()
     
-    private var cardDeck = CardDeck()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor(patternImage:
                                                 backgroundImageChange(imageName: "bg_pattern"))
-        
-        cardDeck.shuffle()
         constrainUI()
         makeCard()
     
@@ -41,9 +66,10 @@ class ViewController: UIViewController {
         //while pokerGame.countCardDeck() {
         pokerGame.startGame(particpatin: .two, gameType: .five)
         pokerGame.showPlayersCard().forEach { (card) in
-            card.hasPlayerCardInfo().values.forEach({ (d) in
-                print(d.showCards())
-            })
+//            card.hasPlayerCardInfo().forEach({ (d) in
+//                print(d.key, separator: "", terminator: " ")
+//                print(d.value.showCards(), separator: "", terminator: "")
+//            })
         }
         //pokerGame.resetCard()
         //}
@@ -57,26 +83,25 @@ class ViewController: UIViewController {
     }
     
     private func constrainUI() {
-        self.view.addSubview(horizontalStackView)
+        self.view.addSubview(cardStackView)
         NSLayoutConstraint.activate([
-            horizontalStackView.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor, constant: 5),
-            horizontalStackView.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor, constant: -5),
-            horizontalStackView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 100)
+            cardStackView.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor, constant: 5),
+            cardStackView.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor, constant: -5),
+            cardStackView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 100)
         ])
     }
     
     private func makeCard()  {
         (0...6).forEach { _ in
-            guard let card = cardDeck.removeOne() else { return }
-            horizontalStackView.addArrangedSubview(createImageView(card : card))
+            cardStackView.addArrangedSubview(createImageView())
         }
     }
 	
-    private func createImageView(card : Card) -> UIImageView {
+    private func createImageView() -> UIImageView {
         let imageView = UIImageView()
         imageView.translatesAutoresizingMaskIntoConstraints = false
         imageView.contentMode = .scaleAspectFit
-        imageView.image = UIImage(named:card.description)
+        imageView.image = cardBackgroundImage
         imageView.widthAnchor.constraint(equalTo: imageView.heightAnchor, multiplier: 1.0/1.27).isActive = true
         return imageView
     }
