b9f0b9f11db5b094ef86fb7b75a67c4d90b8b6f4
Jun Ho JANG <junhojang@Junui-MacBookPro.local>
2021-02-18 17:55:06 +0900

    상속 관계를 없앴습니다.

--- PokerGameApp/PokerGameApp/Dealer.swift
+++ PokerGameApp/PokerGameApp/Dealer.swift
@@ -7,50 +7,46 @@
 
 import Foundation
 
-import Foundation
-
- // 참가자 모집
- // 덱 준비
- // distribute
-
- class Dealer: CardDeck {
+class Dealer {
     
-    public var player: Array<Players> = []
-
+    var cardDeck = CardDeck()
+    var players = Players()
+    
     public var playerNum = 1
-         public var cardStud = 5
+    public var cardStud = 5
 
-         public func recruitPlayer() {
-             for i in 1...playerNum {
-                 player.append(Players(name: "Player\(i)", hand: []))
-             }
-             player.append(Players(name: "Dealer", hand: []))
-         }
+    public func recruitPlayer() {
+        for i in 1...playerNum {
+            players.willJoinGame.append(Player(name: "Player\(i)", hand: []))
+        }
+        players.willJoinGame.append(Player(name: "Dealer", hand: []))
+    }
 
-         public func makeDeckForGame() {
-             make()
-         }
+    public func makeDeckForGame() {
+        cardDeck.make()
+    }
 
-         public func distributeCard() {
-             for _ in 1...cardStud {
-                 for i in 0..<player.count-1 {
-                     player[i].hand.append(deck[0])
-                     removeOne()
-                 }
-                 player[player.endIndex-1].hand.append(deck[0])
-                 removeOne()
-             }
+    public func distributeCard() {
+        cardDeck.shuffle()
+        for _ in 1...cardStud {
+            for i in 0..<players.willJoinGame.count-1 {
+                players.willJoinGame[i].hand.append(cardDeck.deck[0])
+                cardDeck.removeOne()
+            }
+            players.willJoinGame[players.willJoinGame.endIndex-1].hand.append(cardDeck.deck[0])
+            cardDeck.removeOne()
+        }
 
-             for j in 0..<player.count {
-                 print(player[j].hand)
-             }
+        for j in 0..<players.willJoinGame.count {
+            print(players.willJoinGame[j].hand)
+        }
 
-         }
+    }
 
-         public func startGame() {
-             recruitPlayer()
-             makeDeckForGame()
-             distributeCard()
-         }
+    public func startGame() {
+        recruitPlayer()
+        makeDeckForGame()
+        distributeCard()
+    }
 
-     }
+}
