b445d11eee19ff6ff9eefdd9620b461c116b7dc4
lena <sue822330@gmail.com>
2021-02-19 15:12:43 +0900

    Separate Players, HandCard class from Player file
    (cherry picked from commit 3ca26155665871b015f7b9ebf444a896dea1a108)

--- PokerGameApp/PokerGameApp/Player.swift
+++ PokerGameApp/PokerGameApp/Player.swift
@@ -7,48 +7,6 @@
 
 import Foundation
 
-class Players {
-    private var cardDeck = CardDeck()
-    private var players: [Player] = []
-    lazy var count = players.count
-    var description: String {
-        var description = ""
-        players.forEach { player in
-            description += "\(player.description)\n"
-        }
-        return description
-    }
-    
-    init(players: [Player]) {
-        self.players = players
-        cardDeck.shuffle()
-    }
-    
-    func setHandCard(rule: PokerGame.Rule) {
-        (1...rule.rawValue).forEach { _ in
-            handout()
-        }
-    }
-    
-    func reset() {
-        cardDeck.reset()
-        players.forEach { player in
-            player.reset()
-        }
-    }
-    
-    private func handout() {
-        players.forEach { player in
-            guard let card = cardDeck.removeOne() else {
-                print("카드가 부족해 게임을 종료합니다.")
-                return
-            }
-            player.receive(card: card)
-        }
-    }
-
-}
-
 class Player {
     enum PlayerType {
         case dealer
@@ -74,21 +32,3 @@ class Player {
     }
 }
 
-class HandCard {
-    private var cards: [Card] = []
-    var description: String {
-        return cards.description
-    }
-    
-    func append(card: Card) {
-        cards.append(card)
-    }
-    
-    func reset() {
-        cards.removeAll()
-    }
-    
-    func printCard() {
-        print(cards)
-    }
-}
---
+++ PokerGameApp/PokerGameApp/Players.swift
@@ -0,0 +1,49 @@
+//
+//  Players.swift
+//  PokerGameApp
+//
+//  Created by Ador on 2021/02/19.
+//
+
+import Foundation
+
+class Players {
+    private var cardDeck = CardDeck()
+    private var players: [Player] = []
+    lazy var count = players.count
+    var description: String {
+        var description = ""
+        players.forEach { player in
+            description += "\(player.description)\n"
+        }
+        return description
+    }
+    
+    init(players: [Player]) {
+        self.players = players
+        cardDeck.shuffle()
+    }
+    
+    func setHandCard(rule: PokerGame.Rule) {
+        (1...rule.rawValue).forEach { _ in
+            handout()
+        }
+    }
+    
+    func reset() {
+        cardDeck.reset()
+        players.forEach { player in
+            player.reset()
+        }
+    }
+    
+    private func handout() {
+        players.forEach { player in
+            guard let card = cardDeck.removeOne() else {
+                print("카드 부족")
+                return
+            }
+            player.receive(card: card)
+        }
+    }
+}
