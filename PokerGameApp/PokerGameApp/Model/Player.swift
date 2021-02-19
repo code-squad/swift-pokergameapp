a3510cc322ea91a7b0beaea62daed9345d3f8efe
Lia <judykim257@gmail.com>
2021-02-19 16:47:18 +0900

    step4 - 피드백 반영
    Int 대신 구체적 타입 enum 선언
    Player와 Dealer 공통 -> Playable
    [Player] 포함 Players 클래스 선언

--- PokerGameApp/PokerGameApp/Model/Player.swift
+++ PokerGameApp/PokerGameApp/Model/Player.swift
@@ -7,28 +7,16 @@
 
 import Foundation
 
-class Player {
-    private(set) var cards: [Card]
+class Player: Playable {
     private(set) var betting: Int
     let index: Int
     
     init(index: Int) {
-        self.cards = []
         self.betting = 0
         self.index = index
     }
     
-    func getCard(from cards: [Card]) {
-        self.cards += cards
-    }
-    
     func bet(money: Int) {
         betting += money
-    }
-    
-    func openCard() {
-        for i in 0..<cards.count {
-            if cards[i].face == .down {cards[i].face = Face.up}
-        }
     }
 }
