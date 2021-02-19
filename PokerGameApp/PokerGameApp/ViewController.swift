a08b10b1d25c02173b0670f426c0299925da5d17
okstring <milkoi7801@gmail.com>
2021-02-19 18:25:24 +0900

    makeButton 메소드 삭제

--- PokerGameApp/PokerGameApp/ViewController.swift
+++ PokerGameApp/PokerGameApp/ViewController.swift
@@ -16,11 +16,11 @@ class ViewController: UIViewController {
         if let pattern = UIImage(named: "bg_pattern.png") {
             self.view.backgroundColor = UIColor(patternImage: pattern)
         }
+        
         makeSegmentedControl(typeof: .cards)
         makeSegmentedControl(typeof: .players)
         pokerGame = PokerGame(numberOfPlayer: .twoPlayer, stud: .sevenCardStud)
-        
-//        pokerGame.start()
+        resetPokerGame(numberOfPlayer: .twoPlayer, stud: .sevenCardStud)
     }
     
     private func resetPokerGame(numberOfPlayer: Participant, stud: CardStud) {
@@ -38,15 +38,12 @@ class ViewController: UIViewController {
             addPlayersStackViewIntoPokerPlate(imageStackView: imageStackView, nameLabel: playerNameLabel, numberOfPlayer: numberOfPlayer)
         }
         
-        makeStartButton()
         constraintSettingPokerPlate()
     }
 
     
     private func constraintSettingPokerPlate() {
         let margin = view.layoutMarginsGuide
-        guard let lastElement = self.view.subviews.last else { return }
-        let lastElementMargin = lastElement.layoutMarginsGuide
         
         guard let lastSegmentedControlIndex = self.view.subviews.lastIndex(where: { $0 is UISegmentedControl }) else { return }
         let lastSegmentedControl = self.view.subviews[lastSegmentedControlIndex]
@@ -60,7 +57,7 @@ class ViewController: UIViewController {
         pokerPlate.leadingAnchor.constraint(equalTo: margin.leadingAnchor, constant: 5).isActive = true
         pokerPlate.trailingAnchor.constraint(equalTo: margin.trailingAnchor, constant: -20).isActive = true
         pokerPlate.topAnchor.constraint(equalTo: lastSegmentedControlMargin.bottomAnchor, constant: 0).isActive = true
-        pokerPlate.bottomAnchor.constraint(equalTo: lastElementMargin.topAnchor, constant: -50).isActive = true
+        pokerPlate.bottomAnchor.constraint(equalTo: margin.bottomAnchor, constant: -50).isActive = true
     }
     
     
@@ -69,7 +66,6 @@ class ViewController: UIViewController {
             let stackView = UIStackView()
             stackView.axis = .vertical
             stackView.distribution = .equalSpacing
-            stackView.alignment = .top
             stackView.spacing = 10
             return stackView
         }()
@@ -87,27 +83,6 @@ class ViewController: UIViewController {
             
             pokerPlate.addArrangedSubview(stackView)
         }
-        
-    }
-    
-    private func makeStartButton() {
-        let startButton = UIButton()
-        startButton.titleLabel?.font = .systemFont(ofSize: 20, weight: .bold)
-        startButton.backgroundColor = .clear
-        startButton.layer.cornerRadius = 5
-        startButton.layer.borderWidth = 2
-        startButton.layer.borderColor = UIColor.white.cgColor
-        
-        startButton.setTitle("게임 시작", for: .normal)
-        startButton.setTitleColor(.white, for: .normal)
-        
-        self.view.addSubview(startButton)
-        
-        startButton.translatesAutoresizingMaskIntoConstraints = false
-
-        startButton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor, constant: 0).isActive = true
-        startButton.bottomAnchor.constraint(equalTo: self.view.layoutMarginsGuide.bottomAnchor, constant: 0).isActive = true
-        startButton.widthAnchor.constraint(equalToConstant: 100).isActive = true
         
     }
     
