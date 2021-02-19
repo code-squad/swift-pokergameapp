7c6cc0a77d2389cd6caafa0df8a697a7ab087d4a
ellyheetov <yaaajcm@naver.com>
2021-02-19 18:23:29 +0900

    Update InputView : add two segmentcontrol selected value funtion

--- PokerGameApp/InputView.swift
+++ PokerGameApp/InputView.swift
@@ -6,14 +6,35 @@
 //
 
 import Foundation
+import UIKit
 
 struct InputView {
     enum PlayersCount : Int {
-        case one = 1, two, three, four
+        case none, one, two, three, four
     }
 
     enum CardStud : Int {
         case five = 5
         case seven = 7
+        case none = 0
+    }
+    
+    static func segmentToCardSqud(_ segmentControl : UISegmentedControl) -> InputView.CardStud {
+        let value = segmentControl.selectedSegmentIndex
+        switch value {
+        case 0 : return InputView.CardStud.five
+        case 1 : return InputView.CardStud.seven
+        default:
+            return InputView.CardStud.none
+        }
+    }
+    static func segmentToPlayersCount(_ segmentControl : UISegmentedControl) -> InputView.PlayersCount {
+        let value = segmentControl.selectedSegmentIndex
+        switch value {
+        case 0 : return InputView.PlayersCount.two
+        case 1 : return InputView.PlayersCount.three
+        case 2 : return InputView.PlayersCount.four
+        default : return InputView.PlayersCount.none
+        }
     }
 }
