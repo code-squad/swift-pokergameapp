c5b3e04964349a418fcea41a1a37a54dacf092f8
1song2 <1song2918@gmail.com>
2021-02-19 17:00:54 +0900

    Add a new swift file for 'ShowdownCards' struct

---
+++ PokerGameApp/PokerGameApp/Models/ShowdownCards.swift
@@ -0,0 +1,14 @@
+//
+//  ShowdownCards.swift
+//  PokerGameApp
+//
+//  Created by Song on 2021/02/19.
+//
+
+import Foundation
+
+struct ShowdownCards {
+    
+    let owner: Player
+    let setOfCards: [Card]
+}
