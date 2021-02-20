4e23b1c40992dcd74a2b69a11f923f312074cccc
zombietux <unknown>
2021-02-20 16:36:17 +0900

    StackView추가

--- PokerGameApp/PokerGameApp/Views/PlayerView.swift
+++ PokerGameApp/PokerGameApp/Views/PlayerView.swift
@@ -5,4 +5,22 @@
 //  Created by zombietux on 2021/02/18.
 //
 
-import Foundation
+import UIKit
+
+class PlayerView: UIStackView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+        configure()
+    }
+    
+    private func configure() {
+        self.translatesAutoresizingMaskIntoConstraints = false
+        self.axis = .vertical
+        self.spacing = 2
+    }
+}
