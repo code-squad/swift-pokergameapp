7d99ee76cb4ac4c70838ee2b6b3aa4cb15214ef3
zombietux <>
2021-02-20 16:20:45 +0900

    Players 클래스 추가

--- PokerGameApp/PokerGameApp/Models/Players.swift
+++ PokerGameApp/PokerGameApp/Models/Players.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+
+class Players {
+    private var dealer: Dealer
+    private var players: [Player]
+    
+    init(dealer: Dealer, players: [Player]) {
+        self.dealer = dealer
+        self.players = players
+    }
+    
+    func getDealerInfo() -> Dealer {
+        return self.dealer
+    }
+    
+    func getPlayersInfo() -> [Player] {
+        return self.players
+    }
+    
+    func appendPlayer(_ player: Player) {
+        players.append(player)
+    }
+}
