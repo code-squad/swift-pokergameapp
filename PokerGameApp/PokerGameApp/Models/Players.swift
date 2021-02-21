b7b86ec1dbaed69cdf4105e39c3273457c327a02
zombietux <>
2021-02-21 10:41:51 +0900

    [player]와 dealer를 playable로 합친 후 PokerGame에서 하던 일을 가져와서 get메서드 수정

--- PokerGameApp/PokerGameApp/Models/Players.swift
+++ PokerGameApp/PokerGameApp/Models/Players.swift
@@ -8,23 +8,53 @@
 import Foundation
 
 class Players {
-    private var dealer: Dealer
-    private var players: [Player]
+    private var players: [Playable]
+    private (set) var count: Int
     
-    init(dealer: Dealer, players: [Player]) {
-        self.dealer = dealer
+    init(players: [Playable]) {
         self.players = players
+        self.count = 0
     }
-    
-    func getDealerInfo() -> Dealer {
-        return self.dealer
-    }
-    
-    func getPlayersInfo() -> [Player] {
-        return self.players
-    }
-    
-    func appendPlayer(_ player: Player) {
+
+    func appendPlayer(_ player: Playable) {
         players.append(player)
+        updateCount()
+    }
+    
+    func forEachPlayers(behavior: (Playable) -> ()) {
+        players.forEach { player in
+            behavior(player)
+        }
+    }
+    
+    private func updateCount() {
+        self.count = players.count
+    }
+    
+    func registerPlayers(numberOfPlayers: Int, cardCount: Int) {
+        var dealer = Dealer(cardDeck: CardDeck())
+        
+        for index in 1...numberOfPlayers {
+            let hand = Hand(cards: dealer.handOut(cardCount))
+            let playerName = setPlayerName(index)
+            let player = Player(hand: hand, name: playerName)
+            appendPlayer(player)
+        }
+        
+        dealer = hasAHandToDealer(dealer: dealer, cardCount: cardCount)
+        appendPlayer(dealer)
+    }
+    
+    private func setPlayerName(_ playerNumber: Int) -> String {
+        return "참가자\(playerNumber)"
+    }
+    
+    private func hasAHandToDealer(dealer: Dealer, cardCount: Int) -> Dealer {
+        let dealer = dealer
+        return Dealer(cardDeck: dealer.takeRemainCards(), hand: Hand(cards: dealer.handOut(cardCount)))
+    }
+    
+    func resetPlayers() {
+        players = [Playable]()
     }
 }
