23ecd2e9a03562aa3ac96cf89334e5b17470bc93
Jun Ho JANG <junhojang@Junui-MacBookPro.local>
2021-02-21 20:33:36 +0900

    카드 배분에서 여분의 카드가 없는 경우와 그렇지 않은 경우를 나눴습니다. 플레이어로부터 핸드를 받는 함수를 추가했습니다.

--- PokerGameApp/PokerGameApp/Dealer.swift
+++ PokerGameApp/PokerGameApp/Dealer.swift
@@ -9,27 +9,41 @@ import Foundation
 
 class Dealer {
     
-    var cardDeck = CardDeck()
-    var players = Players()
-    
-    public var cardStud = 5
+    private var cardDeck = CardDeck()
+    private var players = Players()
+    public var hands = [[String]]()
     
     public func makeDeckForGame() {
         cardDeck.make()
     }
-
-    public func distributeCard() {
-        players.participatePlayer()
-        cardDeck.shuffle()
-        for _ in 1...cardStud {
-            for i in 0..<players.playerNum {
-                players.receiveCard(playerIndex: i, card: cardDeck.deck[0])
+    
+    public func distributeCard(playersNum: Int, cardStud: Int) {
+        players.participatePlayer(num: playersNum)
+        if cardDeck.deck.count > playersNum * cardStud {
+            cardDeck.shuffle()
+            for _ in 1...cardStud {
+                for i in 0..<playersNum {
+                    players.receiveCard(playerIndex: i, card: cardDeck.deck[0])
+                    cardDeck.removeOne()
+                }
+                players.receiveCard(playerIndex: players.dealerIndex(), card: cardDeck.deck[0])
                 cardDeck.removeOne()
             }
-            players.receiveCard(playerIndex: players.dealerIndex(), card: cardDeck.deck[0])
-            cardDeck.removeOne()
+            players.printHand()
+        } else {
+            cardDeck.reset()
         }
-        players.printHand()
+        
+        
+    }
+    
+    public func receiveHandFromPlayers() {
+        players.submitHandToDealer()
+        hands = players.hands
+    }
+    
+    public func resetDeck() {
+        cardDeck.reset()
     }
     
 }
