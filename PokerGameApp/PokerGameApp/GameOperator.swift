3096713578b328e26c84eb4db50fd993318ae9de
Jun Ho JANG <junhojang@Junui-MacBookPro.local>
2021-02-21 21:41:43 +0900

    게임 진행을 위한 함수를 구현했습니다.

--- PokerGameApp/PokerGameApp/GameOperator.swift
+++ PokerGameApp/PokerGameApp/GameOperator.swift
@@ -13,7 +13,11 @@ class GameOperator {
     var gameBoard = [[String]]()
     
 <<<<<<< HEAD
+<<<<<<< HEAD
     public func startGame() {
+=======
+    public func startGame(playersNum: Int, cardStud: Int) {
+>>>>>>> d8caa9f8... 게임 진행을 위한 함수를 구현했습니다.
 =======
     public func startGame(playersNum: Int, cardStud: Int) {
 >>>>>>> d8caa9f8... 게임 진행을 위한 함수를 구현했습니다.
