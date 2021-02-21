71fea47d127603aec4114639381997c216748214
zombietux <unknown>
2021-02-21 10:40:42 +0900

    Playable프로토콜 채택

--- PokerGameApp/PokerGameApp/Models/Player.swift
+++ PokerGameApp/PokerGameApp/Models/Player.swift
@@ -5,22 +5,18 @@
 //  Created by zombietux on 2021/02/16.
 //
 
-import Foundation
+import UIKit
 
-class Player {
-    private let hand: Hand
-    private let name: String
+class Player: Playable {
+    private var hand: Hand
+    let name: String
     
     init(hand: Hand, name: String) {
         self.hand = hand
         self.name = name
     }
     
-    func getHandInfo() -> Hand {
-        return self.hand
-    }
-    
-    func getNameInfo() -> String {
-        return self.name
+    func makeHand() -> [UIImageView] {
+        return hand.makeCardImage()
     }
 }
