ad03216c34ebe1940109bd8d3eb463e82e1713be
jhpark-steve <park9368@naver.com>
2021-02-21 23:23:16 +0900

    카드 위치 조정 및 카드 생성 방식 변경
    players 에서 uiimageview를 만들어 반환하던 것을 카드의 이름만 반환하여 mainView에 넘기고, mainview에서 uiimageview를 생성, 후 카드가 계속 누적하여 새로 그려져서 뷰를 하나 만들고 해당 뷰에 카드만 그린 후, 다시 그릴 때 해당 뷰의 서브뷰를 전부 삭제하고, 다시 그리도록 함

--- PokerGameApp/PokerGameApp/ViewController.swift
+++ PokerGameApp/PokerGameApp/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 class ViewController: UIViewController {
     private var gameStyle : Int = 0
     private var players : Int = 0
+    private var cardImages : [UIImageView] = []
+    private var mainCardView : UIView = UIView() //카드 뷰를 따로 생성하고 카드를 해당 뷰에 생성한 후, 다시 호출할 떄 removeFromSuperview로 카드만 다시 지우고 그리기 위함
     
     override var preferredStatusBarStyle: UIStatusBarStyle{
         .lightContent
@@ -17,6 +19,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        CreateCardView()
         initGameplay()
     }
     
@@ -60,7 +63,7 @@ class ViewController: UIViewController {
         }
     }
     
-    func initGamestyleSegcon(){
+    func initGamestyleSegcon(){ // 게임 스타일 세그먼트 컨트롤 초기화
         let gameStyle = ["7 Cards", "5 Cards"]
         let gamestyleSeg : UISegmentedControl = UISegmentedControl(items: gameStyle)
         gamestyleSeg.center = CGPoint(x: self.view.frame.width/2, y: 50)
@@ -72,7 +75,7 @@ class ViewController: UIViewController {
         view.addSubview(gamestyleSeg)
     }
     
-    func initPlayersSegcon(){
+    func initPlayersSegcon(){ // 게임 플레이어 수 세그먼트 컨트롤 버튼 초기화
         let players = ["2명","3명","4명"]
         let segmentPlayers : UISegmentedControl = UISegmentedControl(items: players)
         segmentPlayers.center = CGPoint(x: self.view.frame.width/2, y: 100)
@@ -84,11 +87,33 @@ class ViewController: UIViewController {
         self.view.addSubview(segmentPlayers)
     }
     
+    func CreateCardView(){ //카드를 놓을 뷰 생성
+        let cardView : UIView = UIView.init()
+        cardView.center = CGPoint(x: 0, y: 150)
+        cardView.frame.size = CGSize(width: self.view.frame.size.width, height: self.view.frame.size.height)
+        mainCardView = cardView
+        view.addSubview(mainCardView)
+    }
+    
     func drawCardinView(){
+        self.mainCardView.subviews.forEach{$0.removeFromSuperview()}
         let game = PokerGame.init(GameStyle: self.gameStyle, gamePlayers: self.players) //게임 스타일 결정
+        var cardname : String = ""
+        let CARDXPOS : CGFloat = self.view.frame.width/CGFloat(gameStyle)
+        let CARDYPOS : CGFloat = 150
+        let OFFSETCENTER : Int = gameStyle/2 + 1
+        let OFFSET : CGFloat = CGFloat(self.view.frame.size.width/2) - CGFloat(Int(self.view.frame.width)/gameStyle * OFFSETCENTER) + CGFloat((Int(self.view.frame.width)/(self.gameStyle+1))/2)
+        let CARDWIDTH : CGFloat = self.view.frame.size.width/(CGFloat(gameStyle) + 1)
+        let CARDHEIGHT : CGFloat = CARDWIDTH * 1.27
         game.gameStart()
-        for i in 0..<game.drawCard().count{
-            view.addSubview(game.drawCard()[i])
+        for i in 0..<(self.players){
+            for j in 0..<(self.gameStyle){
+                cardname = game.drawCard(playerindex: i, cardindex: j) // model에서 생성한 카드의 이름 순서대로 반환
+                let cardView : UIImageView = UIImageView(image: UIImage(named: cardname))
+                self.cardImages.append(cardView)
+                cardView.frame = CGRect(x: CARDXPOS * CGFloat(j) - OFFSET, y: CGFloat(i) * CGFloat(CGFloat(i)+CARDYPOS), width: CARDWIDTH, height: CARDHEIGHT)
+                mainCardView.addSubview(cardView)
+            }
         }
     }
 }
