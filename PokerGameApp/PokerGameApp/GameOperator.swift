bb7e5f81fd8ec7eddf3e20e8cabecddfe2fe3997
Jun Ho JANG <junhojang@Junui-MacBookPro.local>
2021-02-21 20:33:55 +0900

    게임 진행을 위한 함수를 구현했습니다.

--- PokerGameApp/PokerGameApp/GameOperator.swift
+++ PokerGameApp/PokerGameApp/GameOperator.swift
@@ -10,10 +10,20 @@ import Foundation
 class GameOperator {
     
     var gameOperator = Dealer()
+    var gameBoard = [[String]]()
     
-    public func startGame() {
+    public func startGame(playersNum: Int, cardStud: Int) {
         gameOperator.makeDeckForGame()
-        gameOperator.distributeCard()
+        gameOperator.distributeCard(playersNum: playersNum, cardStud: cardStud)
+        gameOperator.receiveHandFromPlayers()
+    }
+    
+    public func checkBoard() {
+        gameBoard = gameOperator.hands
+    }
+    
+    public func resetDeck() {
+        gameOperator.resetDeck()
     }
     
 }
