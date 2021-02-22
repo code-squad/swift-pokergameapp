23cee1f2a17d5abe293cef83b73a68d6144df978
Sonjh1306 <yale1306@gmail.com>
2021-02-22 20:24:10 +0900

    makeLabelStackView, makeVerticalStackView. makeCardStackView, makeCardImageView 생성

--- PokerGameApp/PokerGameApp/ViewController.swift
+++ PokerGameApp/PokerGameApp/ViewController.swift
@@ -11,14 +11,14 @@ class ViewController: UIViewController {
     @IBOutlet weak var cardStudSegment: UISegmentedControl!
     @IBOutlet weak var playerCountSegment: UISegmentedControl!
     
-    var cardStud: PokerGame.CardStud = .five
-    var playerCount: PokerGame.PlayerCount = .one
+    var cardStud: PokerGame.CardStud = .seven
+    var playerCount: PokerGame.PlayerCount = .four
     
     override func viewDidLoad() {
         super.viewDidLoad()
         makeBackGround()
-        makeStackView()
-        
+        makeVerticalStackView()
+
         let pokerGame = PokerGame(playerCount: .three, cardStud: .five)
         pokerGame.makeGamePlayer()
         pokerGame.distributeCard()
@@ -29,34 +29,69 @@ class ViewController: UIViewController {
         return .lightContent
     }
     
+    // 백그라운드 배경
     func makeBackGround() {
         if let bgImage = UIImage(named: "bg_pattern") {
             self.view.backgroundColor = .init(patternImage: bgImage)
         }
     }
     
+    // 카드 이미지 뷰
     func makeCardImageView() -> UIImageView {
         let cardImageView = UIImageView()
-        let image = UIImage(named: "card-back")
+        let image = UIImage(named: "cA")
         cardImageView.image = image
-        cardImageView.heightAnchor.constraint(equalTo: cardImageView.widthAnchor, multiplier: 1.27).isActive = true
+        cardImageView.widthAnchor.constraint(equalTo: cardImageView.heightAnchor, multiplier: 1.0/1.27).isActive = true
         return cardImageView
     }
     
-    func makeStackView() {
-        let stackView = UIStackView()
-        stackView.axis = .horizontal
-        stackView.distribution = .fillEqually
-        stackView.alignment = .fill
-        stackView.spacing = 5
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(stackView)
-        stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 100).isActive = true
-        stackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 10).isActive = true
-        stackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -10).isActive = true
-        for _ in 0..<7 {
-            stackView.addArrangedSubview(makeCardImageView())
+    // 카드 이미지 뷰를 가지고 있는 스택 뷰
+    func makeCardStackView() -> UIStackView {
+        let cardStackView = UIStackView()
+        cardStackView.axis = .horizontal
+        cardStackView.distribution = .fillEqually
+        cardStackView.alignment = .fill
+        
+        cardStackView.spacing = -10
+        cardStackView.translatesAutoresizingMaskIntoConstraints = false
+        for _ in 0..<cardStud.rawValue {
+            cardStackView.addArrangedSubview(makeCardImageView())
         }
+        return cardStackView
+    }
+    
+    // 플레이어 라벨을 가지고 있는 스택 뷰
+    func makeLabelStackView(text: String) -> UIStackView {
+        let nameStackView = UIStackView()
+        nameStackView.axis = .vertical
+        nameStackView.alignment = .fill
+        nameStackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        let nameLabel = UILabel()
+        nameLabel.text = text
+        nameLabel.textColor = .white
+        nameLabel.font = UIFont.boldSystemFont(ofSize: 20)
+        nameStackView.addArrangedSubview(nameLabel)
+        return nameStackView
+    }
+    
+    // 라벨 스택 뷰, 카드 스택 뷰를 가지고 있는 큰 스택 뷰
+    func makeVerticalStackView() {
+        let verticalStackView = UIStackView()
+        verticalStackView.axis = .vertical
+        verticalStackView.alignment = .top
+        verticalStackView.spacing = 5
+        verticalStackView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(verticalStackView)
+        verticalStackView.topAnchor.constraint(equalTo: playerCountSegment.bottomAnchor, constant: 20).isActive = true
+        verticalStackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 10).isActive = true
+        verticalStackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -10).isActive = true
+        for num in 0..<playerCount.rawValue {
+            verticalStackView.addArrangedSubview(makeLabelStackView(text: "Player\(num+1)"))
+            verticalStackView.addArrangedSubview(makeCardStackView())
+        }
+        verticalStackView.addArrangedSubview(makeLabelStackView(text: "Dealer"))
+        verticalStackView.addArrangedSubview(makeCardStackView())
     }
     
     @IBAction func changeCardStud(_ sender: Any) {
@@ -82,5 +117,4 @@ class ViewController: UIViewController {
             break
         }
     }
-    
 }
