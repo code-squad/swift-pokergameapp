96c39c20b4c083fb32db3791ca07dfce754cc925
zeke <yangsin1031@naver.com>
2021-02-22 18:34:50 +0900

    segment의 action 추가
    각 segment의 값이 변경될때마다 적용될 메소드 추가

--- PokerGameApp/PokerGameApp/Controller/ViewController.swift
+++ PokerGameApp/PokerGameApp/Controller/ViewController.swift
@@ -12,14 +12,19 @@ class ViewController: UIViewController {
     var stackView: UIStackView!
     let cardBackImage = UIImage(named: "card-back")
     var cards = CardDeck(cards: [])
+    var pokerGame: PokerGame?
+    var pokerGameParticipant: Participant?
+    var pokerGameType: GameType?
     var gameTypeSegment: UISegmentedControl {
-        let sc: UISegmentedControl = UISegmentedControl(items: ["7 Cards", "5 Cards"])
+        let sc: UISegmentedControl = UISegmentedControl(items: ["5 Cards", "7 Cards"])
         sc.center = CGPoint(x: self.view.frame.width/2, y: 80)
+        sc.addTarget(self, action: #selector(gameTypeChanged(sender:)), for: .valueChanged)
         return sc
     }
-    var playerCountSegment: UISegmentedControl {
+    var participantSegment: UISegmentedControl {
         let sc: UISegmentedControl = UISegmentedControl(items: ["2명", "3명", "4명"])
         sc.center = CGPoint(x: self.view.frame.width/2, y: 120)
+        sc.addTarget(self, action: #selector(participantChanged(sender:)), for: .valueChanged)
         return sc
     }
 
@@ -27,9 +32,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setBackground()
-        testPoker()
         self.view.addSubview(gameTypeSegment)
-        self.view.addSubview(playerCountSegment)
+        self.view.addSubview(participantSegment)
     }
    
     func setBackground() {
@@ -37,51 +41,60 @@ class ViewController: UIViewController {
         self.view.backgroundColor = UIColor(patternImage: bgImage)
     }
     
-    func setStackView() {
-        self.stackView = UIStackView()
-        self.stackView.axis = .horizontal
-        self.stackView.alignment = .fill
-        self.stackView.distribution = .equalSpacing
-        self.stackView.spacing = 5
-        self.stackView.translatesAutoresizingMaskIntoConstraints = false
-        
-        for _ in 0...6 {
-            self.stackView.addArrangedSubview(generateImage())
-        }
-        
-        self.view.addSubview(stackView)
-        stackView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
-        stackView.topAnchor.constraint(equalTo: view.topAnchor, constant: 100).isActive = true
-    }
+//    func setStackView() {
+//        self.stackView = UIStackView()
+//        self.stackView.axis = .horizontal
+//        self.stackView.alignment = .fill
+//        self.stackView.distribution = .equalSpacing
+//        self.stackView.spacing = 5
+//        self.stackView.translatesAutoresizingMaskIntoConstraints = false
+//
+//        for _ in 0...6 {
+//            self.stackView.addArrangedSubview(generateImage())
+//        }
+//
+//        self.view.addSubview(stackView)
+//        stackView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+//        stackView.topAnchor.constraint(equalTo: view.topAnchor, constant: 100).isActive = true
+//    }
     
-    func generateImage() -> UIImageView {
-        let width = self.view.bounds.width/8
-        let height = self.view.bounds.width/8 * 1.27
-        let imageView = UIImageView()
-        imageView.widthAnchor.constraint(equalToConstant: width).isActive = true
-        imageView.heightAnchor.constraint(equalToConstant: height).isActive = true
-        imageView.image = cardBackImage
-        return imageView
-    }
+//    func generateImage() -> UIImageView {
+//        let width = self.view.bounds.width/8
+//        let height = self.view.bounds.width/8 * 1.27
+//        let imageView = UIImageView()
+//        imageView.widthAnchor.constraint(equalToConstant: width).isActive = true
+//        imageView.heightAnchor.constraint(equalToConstant: height).isActive = true
+//        imageView.image = cardBackImage
+//        return imageView
+//    }
 
-    func testPoker() {
-        let pokergame = PokerGame(players: Players(participant: .four), dealer: Dealer(gameType: .seven))
-        pokergame.startGame()
-        pokergame.printParticipantCards()
+//    func testPoker() {
+//        let pokergame = PokerGame(players: Players(participant: .four), dealer: Dealer(gameType: .seven))
+//        pokergame.startGame()
+//        pokergame.printParticipantCards()
+//    }
+    @objc func gameTypeChanged(sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 0:
+            pokerGameType = .five
+        case 1:
+            pokerGameType = .seven
+        default:
+            return
+        }
+    }
+    @objc func participantChanged(sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 0:
+            pokerGameParticipant = .two
+        case 1:
+            pokerGameParticipant = .three
+        case 2:
+            pokerGameParticipant = .four
+        default:
+            return
+        }
     }
 
 }
 
-extension UIButton {
-    func setBackgroundColor(_ color: UIColor, for state: UIControl.State) {
-        UIGraphicsBeginImageContext(CGSize(width: 1.0, height: 1.0))
-        guard let context = UIGraphicsGetCurrentContext() else { return }
-        context.setFillColor(color.cgColor)
-        context.fill(CGRect(x: 0.0, y: 0.0, width: 1.0, height: 1.0))
-        
-        let backgroundImage = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
-         
-        self.setBackgroundImage(backgroundImage, for: state)
-    }
-}
