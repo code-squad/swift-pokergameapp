e81052dece2452c97799be1d708c7a1878819a05
1song2 <1song2918@gmail.com>
2021-02-23 14:13:48 +0900

    Remove method 'getOwnerName()' and 'tellName()'
    Add private(set) keyword to properties.

--- PokerGameApp/PokerGameApp/Models/ShowdownCards.swift
+++ PokerGameApp/PokerGameApp/Models/ShowdownCards.swift
@@ -9,16 +9,12 @@ import Foundation
 
 struct ShowdownCards {
     
-    private let owner: Player
+    private(set) var owner: Player
     private let setOfCards: [Card]
     
     init(owner: Player, setOfCards: [Card]) {
         self.owner = owner
         self.setOfCards = setOfCards
-    }
-    
-    func getOwnerName() -> String {
-        return owner.tellName()
     }
     
     func showCards(handler: (Card) -> ()) {
