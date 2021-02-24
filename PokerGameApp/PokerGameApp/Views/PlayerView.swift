085427d382301a85cc0b13887914a3accf798ae6
zombietux <unknown>
2021-02-24 19:34:49 +0900

    승자 이미지가 추가시킨 PlayerWithWinnerView추가

--- PokerGameApp/PokerGameApp/Views/PlayerView.swift
+++ PokerGameApp/PokerGameApp/Views/PlayerView.swift
@@ -7,6 +7,25 @@
 
 import UIKit
 
+class PlayerWithWinnerView: UIStackView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+        configure()
+    }
+    
+    private func configure() {
+        self.translatesAutoresizingMaskIntoConstraints = false
+        self.axis = .horizontal
+        self.spacing = 5
+        self.alignment = .bottom
+    }
+}
+
 class PlayerView: UIStackView {
     override init(frame: CGRect) {
         super.init(frame: frame)
