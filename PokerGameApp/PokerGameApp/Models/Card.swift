7fe4d79a8b1fbd3dfc19b9e38fb30051b04b0192
zombietux <unknown>
2021-02-24 19:28:06 +0900

    Card비교를 위한 프로토콜 추가

--- PokerGameApp/PokerGameApp/Models/Card.swift
+++ PokerGameApp/PokerGameApp/Models/Card.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct Card: CustomStringConvertible, Equatable {
+struct Card: CustomStringConvertible, Hashable {
     /*
      Suit 타입은 enum으로 생성.
      CardValue와 달리 다양한 초기화가 불필요하고 String에 맞게 유니코드만 반환해주면 된다고 판단.
@@ -26,8 +26,20 @@ struct Card: CustomStringConvertible, Equatable {
         self.rank = rank
         self.suit = suit
     }
-    
+  
     internal var description: String {
         return "\(suit.rawValue)\(rank.description)"
+    }
+}
+
+extension Card: Equatable {
+    static func ==(lhs: Card, rhs: Card) -> Bool {
+        return lhs.rank == rhs.rank && lhs.suit == rhs.suit
+    }
+}
+
+extension Card: Comparable {
+    static func <(lhs: Card, rhs: Card) -> Bool {
+        return lhs.rank < rhs.rank
     }
 }
