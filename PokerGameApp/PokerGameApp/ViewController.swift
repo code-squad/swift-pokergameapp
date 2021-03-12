a2ecc0c28d64ade459abc06934ef6e6546927a7d
JinseoPark <rarebook92@naver.com>
2021-03-12 15:26:55 +0900

    Create Card class for pockerGame
    Approaching OOP, make a card class and 4 type of suits by inheritance. Tempted to use enum to avoid magic number as well.

--- PokerGameApp/PokerGameApp/ViewController.swift
+++ PokerGameApp/PokerGameApp/ViewController.swift
@@ -7,40 +7,62 @@
 
 import UIKit
 
+/* 하드코딩을 대체하기 위한 상수 선언 */
+fileprivate let ZERO : Int = 0
+fileprivate let NUMBER_CARD : Int = 7
+fileprivate let STACKVIEW_SPACING : CGFloat = 10
+fileprivate let CARD_IMAGE_PATH : String = "bg_pattern.png"
+
+
 class ViewController: UIViewController {
 
     @IBOutlet weak var cardStackView: UIStackView!
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
     
         /*백그라운드 디자인 패턴 이미지*/
-        guard let patternImg = UIImage(named: "bg_pattern.png") else { return }
-        self.view.backgroundColor = UIColor(patternImage: patternImg)
         
-        /*참조 영상 : https://www.youtube.com/watch?v=zpb4-VnaaqQ */
-        /*카드 스택 뷰 옵션 설정. */
-        cardStackView.distribution = .fillEqually
-        cardStackView.spacing = 10
-        
-        
-        /*이미지 뷰 배열을 설정*/
-        var cardImgViewArr : [UIImageView] = []
+        func makeStackView() {
+            guard let patternImg = UIImage(named: CARD_IMAGE_PATH) else { return }
+            self.view.backgroundColor = UIColor(patternImage: patternImg)
             
-        /*7개의 카드 이미지 뷰를 생성과 동시에 해당 이미지를 스택에 추가한다.
-         또한 각 이미지뷰의 옵션도 설정함.
-         */
-        for index in 0 ..< 7 {
-            cardImgViewArr.append(UIImageView(image: UIImage(named: "card-back.png")))
-            
-            cardStackView.addArrangedSubview(cardImgViewArr[index])
-            
-            cardImgViewArr[index].contentMode = .scaleAspectFit
-            cardImgViewArr[index].clipsToBounds = true
-            
+            /*참조 영상 : https://www.youtube.com/watch?v=zpb4-VnaaqQ */
+            /*카드 스택 뷰 옵션 설정. */
+            cardStackView.distribution = .fillEqually
+            cardStackView.spacing = STACKVIEW_SPACING
+                    
             
         }
+       
+        /*이미지 뷰 배열 생성 및 설정*/
+        func arrangeImgViewToStackView() {
+            var cardImgViewArr : [UIImageView] = []
+            /*
+             7개의 카드 이미지 뷰를 생성과 동시에 해당 이미지를 스택에 추가한다.
+             또한 각 이미지뷰의 옵션도 설정함.
+             */
+            for index in ZERO ..< NUMBER_CARD {
+                cardImgViewArr.append(UIImageView(image: UIImage(named: "card-back.png")))
+                
+                cardStackView.addArrangedSubview(cardImgViewArr[index])
+                
+                cardImgViewArr[index].contentMode = .scaleAspectFit
+                cardImgViewArr[index].clipsToBounds = true
+
+            }
+        }
+        
+        makeStackView()
+        arrangeImgViewToStackView()
+        
+        /*Step2 콘솔 표현*/
+        
+        let spadeAce = Spade("1")
+        let heartJack = Heart("11")
+                
+        spadeAce.showCardInfo()
+        heartJack.showCardInfo()
         
     }
 }
